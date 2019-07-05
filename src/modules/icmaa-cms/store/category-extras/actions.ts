882504d77b033ca3414312ad2787869dc0b3772b
cewald <c.ewald@impericon.com>
2019-07-05 12:17:28 +0200

    Generate abstract class for cms actions

--- src/modules/icmaa-cms/store/category-extras/actions.ts
+++ src/modules/icmaa-cms/store/category-extras/actions.ts
@@ -1,83 +1,21 @@
-import Vue from 'vue'
 import { ActionTree } from 'vuex'
-import { KEY } from '../../'
+import { single as singleAbstract, MutationTypesInterface, SingleOptionsInterface } from '../abstract/actions'
+
+import { cmsCategoryExtrasStorageKey as storageKey } from './'
 import * as types from './mutation-types'
+import CategoryExtrasState, { CategoryExtrasStateItem } from '../../types/CategoryExtrasState'
 import RootState from '@vue-storefront/core/types/RootState';
-import CategoryExtrasState from '../../types/CategoryExtrasState'
-import { cmsCategoryExtrasStorageKey } from './'
 
-import Axios from 'axios'
-import config from 'config'
-
-import { currentStoreView } from '@vue-storefront/core/lib/multistore'
-import { processURLAddress } from '@vue-storefront/core/helpers'
-import { isServer } from '@vue-storefront/core/helpers'
-import { Logger } from '@vue-storefront/core/lib/logger'
+const documentType = 'category-extra';
+const mutationTypes: MutationTypesInterface = {
+  add: types.ICMAA_CMS_CATEGORY_EXRTAS_ADD,
+  upd: types.ICMAA_CMS_CATEGORY_EXRTAS_UPD,
+  rmv: types.ICMAA_CMS_CATEGORY_EXRTAS_RMV
+}
 
 const actions: ActionTree<CategoryExtrasState, RootState> = {
-  async single (context, { value, key = 'identifier' }): Promise<CategoryExtrasState> {
-    const state = context.state
-    const cache = Vue.prototype.$db[KEY]
-    const storeView = currentStoreView()
-
-    if (!state.items || state.items.length === 0 || !state.items.find(itm => itm[key] === value)) {
-      const cacheKey = cmsCategoryExtrasStorageKey + '/' + value
-      if (await cache.getItem(cacheKey).then(item => item !== null)) {
-        return cache.getItem(cacheKey).then(result => {
-          context.commit(types.ICMAA_CMS_CATEGORY_EXRTAS_UPD, result)
-          return result
-        })
-      }
-
-      if (!isServer) {
-        context.commit(types.ICMAA_CMS_CATEGORY_EXRTAS_ADD, { identifier: value })
-      }
-
-      let params = {
-        'type': 'category-extra',
-        'uid': encodeURIComponent(value),
-        'lang': null
-      }
-
-      if (storeView) {
-        params.lang = storeView.i18n.defaultLocale.toLowerCase()
-      }
-
-      return Axios.get(
-        processURLAddress(config.icmaa_cms.endpoint),
-        { responseType: 'json', params }
-      ).then(resp => {
-        let result = resp.data.result;
-        if (Object.keys(result).length === 0 || resp.status !== 200) {
-          throw new Error(resp.statusText)
-        }
-
-        result[key] = value;
-        context.commit(types.ICMAA_CMS_CATEGORY_EXRTAS_UPD, result)
-
-        cache.setItem(cacheKey, result)
-          .catch(error => Logger.error(error, 'icmaa-cms'))
-
-        return result
-      }).catch(err => {
-        context.commit(types.ICMAA_CMS_CATEGORY_EXRTAS_RMV, { identifier: value })
-        Logger.error(`Error while fetching identifier "${value}"`, 'icmaa-cms', err)()
-      })
-    } else {
-      return new Promise((resolve, reject) => {
-        if (state.items.length > 0) {
-          let block = state.items.find(itm => itm[key] === value)
-          if (block) {
-            resolve(block)
-          } else {
-            reject(new Error('Error while fetching state for ' + key + ' = ' + value))
-          }
-        } else {
-          resolve()
-        }
-      })
-    }
-  }
+  single: async (context, options: SingleOptionsInterface): Promise<CategoryExtrasStateItem|any> =>
+    singleAbstract({ documentType, mutationTypes, storageKey, context, options })
 }
 
 export default actions
