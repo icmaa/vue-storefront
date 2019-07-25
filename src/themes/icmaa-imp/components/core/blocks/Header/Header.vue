5f8a41d5d26809c2f0d0d8db8707473fe0a6b746
cewald <c.ewald@impericon.com>
2019-07-25 13:44:37 +0200

    Remove unnecessary components and its code

--- src/themes/icmaa-imp/components/core/blocks/Header/Header.vue
+++ src/themes/icmaa-imp/components/core/blocks/Header/Header.vue
@@ -21,62 +21,6 @@
           <logo width="174" height="43" class="logo t-flex-fix t--mr-4 xl:t-mr-0" />
         </div>
       </div>
-
-      <div class="container px15">
-        <div class="row between-xs middle-xs" v-if="!isCheckoutPage || isThankYouPage">
-          <div class="col-md-4 col-xs-2 middle-xs">
-            <div>
-              <hamburger-icon class="p15 icon bg-cl-secondary pointer" />
-            </div>
-          </div>
-          <div class="col-xs-2 visible-xs">
-            <search-icon class="p15 icon pointer" />
-          </div>
-          <div class="col-md-4 col-xs-4 center-xs pt5">
-            <div>
-              <logo width="auto" height="41px" />
-            </div>
-          </div>
-          <div class="col-xs-2 visible-xs">
-            <wishlist-icon class="p15 icon pointer" />
-          </div>
-          <div class="col-md-4 col-xs-2 end-xs">
-            <div class="inline-flex right-icons">
-              <search-icon class="p15 icon hidden-xs pointer" />
-              <wishlist-icon class="p15 icon hidden-xs pointer" />
-              <compare-icon class="p15 icon hidden-xs pointer" />
-              <microcart-icon class="p15 icon pointer" />
-              <account-icon class="p15 icon hidden-xs pointer" />
-            </div>
-          </div>
-        </div>
-        <div class="row between-xs middle-xs px15 py5" v-if="isCheckoutPage && !isThankYouPage">
-          <div class="col-xs-5 col-md-3 middle-xs">
-            <div>
-              <router-link
-                :to="localizedRoute('/')"
-                class="cl-tertiary links"
-              >
-                {{ $t('Return to shopping') }}
-              </router-link>
-            </div>
-          </div>
-          <div class="col-xs-2 col-md-6 center-xs">
-            <logo width="auto" height="41px" />
-          </div>
-          <div class="col-xs-5 col-md-3 end-xs">
-            <div>
-              <a
-                v-if="!currentUser"
-                href="#"
-                @click.prevent="gotoAccount"
-                class="cl-tertiary links"
-              >{{ $t('Login to your account') }}</a>
-              <span v-else>{{ $t('You are logged in as') }} {{ currentUser.firstname }}</span>
-            </div>
-          </div>
-        </div>
-      </div>
     </header>
     <div class="header-placeholder" />
   </div>
@@ -91,13 +35,6 @@ import ButtonCart from 'theme/components/core/blocks/Header/ButtonCart'
 import ButtonAccount from 'theme/components/core/blocks/Header/ButtonAccount'
 import SearchInput from 'theme/components/core/blocks/Header/SearchInput'
 
-import AccountIcon from 'theme/components/core/blocks/Header/AccountIcon'
-import CompareIcon from 'theme/components/core/blocks/Header/CompareIcon'
-import HamburgerIcon from 'theme/components/core/blocks/Header/HamburgerIcon'
-import MicrocartIcon from 'theme/components/core/blocks/Header/MicrocartIcon'
-import SearchIcon from 'theme/components/core/blocks/Header/SearchIcon'
-import WishlistIcon from 'theme/components/core/blocks/Header/WishlistIcon'
-
 export default {
   name: 'Header',
   components: {
@@ -105,14 +42,7 @@ export default {
     ButtonUiSidebar,
     ButtonCart,
     ButtonAccount,
-
-    SearchInput,
-    AccountIcon,
-    CompareIcon,
-    HamburgerIcon,
-    MicrocartIcon,
-    SearchIcon,
-    WishlistIcon
+    SearchInput
   },
   mixins: [CurrentPage],
   data () {
@@ -122,17 +52,6 @@ export default {
       scrollTop: 0,
       lastScrollTop: 0,
       navbarHeight: 110
-    }
-  },
-  computed: {
-    ...mapState({
-      isOpenLogin: state => state.ui.signUp,
-      currentUser: state => state.user.current
-    }),
-    isThankYouPage () {
-      return this.$store.state.checkout.isThankYouPage
-        ? this.$store.state.checkout.isThankYouPage
-        : false
     }
   },
   beforeMount () {
@@ -152,9 +71,6 @@ export default {
     }, 250)
   },
   methods: {
-    gotoAccount () {
-      this.$bus.$emit('modal-toggle', 'modal-signup')
-    },
     hasScrolled () {
       this.scrollTop = window.scrollY
       if (
@@ -194,43 +110,6 @@ header {
 
   .row-02 {
     height: 60px;
-  }
-}
-
-// Old header css below …
-
-.icon {
-  opacity: 0.6;
-  &:hover,
-  &:focus {
-    background-color: $color-icon-hover;
-    opacity: 1;
-  }
-}
-.right-icons {
-  //for edge
-  float: right;
-}
-.links {
-  text-decoration: underline;
-}
-@media (max-width: 767px) {
-  .row.middle-xs {
-    margin: 0 -15px;
-
-    &.py5 {
-      margin: 0;
-    }
-  }
-  .col-xs-2:first-of-type {
-    padding-left: 0;
-  }
-  .col-xs-2:last-of-type {
-    padding-right: 0;
-  }
-  a,
-  span {
-    font-size: 12px;
   }
 }
 </style>
