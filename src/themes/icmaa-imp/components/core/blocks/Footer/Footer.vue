d20587db4149120b6bb069a5c1662ce01e6ce1c6
cewald <c.ewald@impericon.com>
2019-07-30 13:30:45 +0200

    Add newsletter form to footer WIP

--- src/themes/icmaa-imp/components/core/blocks/Footer/Footer.vue
+++ src/themes/icmaa-imp/components/core/blocks/Footer/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer>
-    <div class="t-bg-white t-py-6 t-px-4">
+    <div class="social-media t-bg-white t-py-6 t-px-4">
       <div class="t-flex t-justify-between t-mb-6 ">
         <template v-for="(path, icon) in socialMediaIcons">
           <router-link :key="icon" :to="path" class="t-flex t-flex-fix t-items-center t-justify-center t-w-10 t-h-10 t-rounded-full t-bg-base-light t-text-2xl t-text-white">
@@ -8,35 +8,28 @@
           </router-link>
         </template>
       </div>
-      <div class="t-flex t-justify-between t-flex-wrap t-mb-6 t--mx-2 t--mb-4">
+      <div class="service-carrier t-flex t-justify-between t-flex-wrap t-mb-6 t--mx-2 t--mb-4">
         <template v-for="(name, path) in carrierLogos">
-          <div :key="path" class="t-flex t-w-1/3 sm:t-w-auto t-flex-initial t-justify-center t-px-2 t-pb-4">
+          <div :key="path" class="t-flex t-w-auto t-flex-initial t-justify-center t-px-2 t-pb-4">
             <div class="t-flex t-flex-initial t-w-24 t-h-12 t-items-center t-justify-center t-border t-border-base-lighter t-rounded-sm t-p-2 sm:t-p-4">
-              <retina-image :image="`/assets/logos/${path}.png`" class="t-flex t-max-w-full" />
+              <retina-image :image="`/assets/logos/${path}.png`" :alt="name" class="t-flex t-max-w-full" />
             </div>
           </div>
         </template>
       </div>
+      <newsletter />
     </div>
-    <div class="t-bg-black t-p-4">
-      <div class="t-flex t-flex-wrap t-items-center t-justify-center t-leading-loose">
-        <router-link to="" class="t-flex-initial t-px-3 t-text-white t-text-xs t-uppercase">
-          Test
-        </router-link>
-        <router-link to="" class="t-flex-initial t-px-3 t-text-white t-text-xs t-uppercase">
-          Test
-        </router-link>
-        <router-link to="" class="t-flex-initial t-px-3 t-text-white t-text-xs t-uppercase">
-          Test
-        </router-link>
-        <div class="t-flex-expand t-hidden sm:t-flex" />
-        <div class="t-hidden sm:t-flex t-text-white t-text-xs t-uppercase">
-          &copy; Impericon is a Division of IC Music and Apparel GmbH
-        </div>
+    <div class="footer-navigation t-bg-black t-p-4">
+      <div class="t-flex t-flex-wrap t-items-center t-justify-center t-leading-looser">
+        <template v-for="(link, index) in navigation">
+          <router-link :to="link.route" class="t-flex-initial t-px-4 t-text-white t-text-xs t-uppercase" :key="index">
+            {{ link.name }}
+          </router-link>
+        </template>
+        <div class="t-flex-expand t-hidden md:t-flex" />
+        <div class="copyright t-hidden md:t-flex t-text-white t-text-xs t-uppercase" :html="copyright" />
       </div>
     </div>
-
-    <newsletter class=" flex brdr-bottom-1 brdr-cl-secondary" v-if="!isCheckoutPage" />
     <back-to-top bottom="20px" right="20px" visibleoffset="200">
       <button type="button" class="btn-top button no-outline brdr-none cl-white bg-cl-mine-shaft :bg-cl-th-secondary py10 px10">
         <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd">
@@ -58,8 +51,15 @@ import RetinaImage from 'theme/components/core/blocks/RetinaImage'
 import config from 'config'
 
 export default {
-  mixins: [ CurrentPage ],
   name: 'MainFooter',
+  components: {
+    MaterialIcon,
+    RetinaImage,
+    Newsletter,
+    LanguageSwitcher,
+    BackToTop
+  },
+  mixins: [ CurrentPage ],
   computed: {
     multistoreEnabled () {
       return config.storeViews.multistore
@@ -82,14 +82,52 @@ export default {
         'payment/mastercard': 'MasterCard',
         'shipping/dhl': 'DHL'
       }
+    },
+    navigation () {
+      return [
+        {
+          name: 'Service',
+          route: '/service'
+        },
+        {
+          name: 'Impressum',
+          route: '/service'
+        },
+        {
+          name: 'AGB',
+          route: '/service'
+        },
+        {
+          name: 'Datenschutz',
+          route: '/service'
+        },
+        {
+          name: 'Model',
+          route: '/service'
+        },
+        {
+          name: 'Jobs',
+          route: '/service'
+        },
+        {
+          name: 'Partner werden',
+          route: '/service'
+        }
+      ]
+    },
+    copyright () {
+      return '&copy; Impericon is a Division of IC Music and Apparel GmbH'
     }
-  },
-  components: {
-    MaterialIcon,
-    RetinaImage,
-    Newsletter,
-    LanguageSwitcher,
-    BackToTop
   }
 }
 </script>
+
+<style lang="scss" scoped>
+
+@media (max-width: 360px) {
+  .service-carrier > div {
+    width: 33.333333%;
+  }
+}
+
+</style>
