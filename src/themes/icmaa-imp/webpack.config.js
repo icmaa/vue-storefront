f2a621fb967fde686188f64c9389da06470b8b2c
cewald <c.ewald@impericon.com>
2019-08-13 12:44:23 +0000

    Add first svg sprite config

--- src/themes/icmaa-imp/webpack.config.js
+++ src/themes/icmaa-imp/webpack.config.js
@@ -1,6 +1,8 @@
 const merge = require('webpack-merge')
 const path = require('path')
 
+const SVGSpritemapPlugin = require('svg-spritemap-webpack-plugin')
+
 /**
  * You can extend default webpack build here.
  * @see https://docs.vuestorefront.io/guide/core-themes/webpack.html
@@ -53,6 +55,25 @@ module.exports = function (config, { isClient, isDev }) {
 
   let rules = (isDev) ? config.default.module.rules : config.module.rules
   rules.map(rewriteMapping)
+
+  /**
+   * Create SVG sprites
+   */
+  const getSpriteConfig = (src = '', filename = 'default', targetPath = '', prefix = false, svg4everybody = true, svgo = true) => {
+    targetPath = isDev ? path.join(__dirname, targetPath) : targetPath
+    filename = `${targetPath}${filename}.svg`
+    return new SVGSpritemapPlugin(
+      path.join(__dirname, src),
+      { output: { filename, svg4everybody, svgo }, sprite: { prefix } }
+    )
+  }
+
+  config = merge(config, {
+    plugins: [
+      getSpriteConfig(path.join(__dirname, 'assets/flags/1x1/*.svg'), '1x1', 'assets/flags/'),
+      getSpriteConfig(path.join(__dirname, 'assets/flags/4x3/*.svg'), '4x3', 'assets/flags/')
+    ]
+  })
 
   return config
 }
