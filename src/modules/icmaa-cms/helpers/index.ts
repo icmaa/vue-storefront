3e8e0d4988a8bbc3bfbe527413a3db1369e0a60d
cewald <c.ewald@impericon.com>
2019-08-22 13:20:00 +0000

    Improve code

--- src/modules/icmaa-cms/helpers/index.ts
+++ src/modules/icmaa-cms/helpers/index.ts
@@ -1,8 +1,8 @@
-import { icmaa, defaultStoreCode } from 'config'
+import { defaultStoreCode } from 'config'
 import { localizedRoute, currentStoreView } from '@vue-storefront/core/lib/multistore';
 
 export const getCurrentStoreCode = (): string => {
-  return !currentStoreView() ? null : currentStoreView().storeCode
+  return !currentStoreView() && currentStoreView().storeCode !== defaultStoreCode ? null : currentStoreView().storeCode
 }
 
 export const localizeRouterLink = (text: string): string => {
