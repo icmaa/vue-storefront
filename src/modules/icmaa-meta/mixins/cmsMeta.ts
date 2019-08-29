0cbbdf201ee35791a5b0ed29a659dd340cd0afac
Dierk Landmann <d.landmann@impericon.com>
2019-08-29 12:37:43 +0200

    wip

--- src/modules/icmaa-meta/mixins/cmsMeta.ts
+++ src/modules/icmaa-meta/mixins/cmsMeta.ts
@@ -1,14 +1,6 @@
 
 const defaults: any = {
-  metaInfo: {
-    title: 'Placeholder - cmsPage',
-    meta: [
-      { vmid: 'description', name: 'description', content: 'Placeholder - cmspageDescrition' },
-      { vmid: 'keywords', name: 'keywords', content: 'Placeholder - cmspageKeywords' },
-      { vmid: 'og:title', property: 'og:title', content: 'Placeholder - cmspageTitle' },
-      { vmid: 'og:url', property: 'og:url', content: 'Placeholder - cmspageUrl' }
-    ]
-  }
+
 }
 
 export default defaults
