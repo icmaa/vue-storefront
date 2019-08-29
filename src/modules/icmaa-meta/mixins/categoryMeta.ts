d47688173777db22341b2ae28470ad1fb6ecfc72
Dierk Landmann <d.landmann@impericon.com>
2019-08-29 12:37:43 +0200

    wip

--- src/modules/icmaa-meta/mixins/categoryMeta.ts
+++ src/modules/icmaa-meta/mixins/categoryMeta.ts
@@ -1,14 +1,54 @@
 // title and description --> icmaa-cms/mixins/categoryExtras.ts
-const defaults: any = {
-  metaInfo: {
-    meta: [
-      { vmid: 'keywords', name: 'keywords', content: 'Placeholder - categorypageKeywords' },
-      { vmid: 'og:image', property: 'og:image', content: 'Placeholder - categorypageImage(s)' }, // multiple ..
-      { vmid: 'og:title', property: 'og:title', content: 'Placeholder - categorypageTitle' },
-      { vmid: 'og:url', property: 'og:url', content: 'Placeholder - categorypageUrl' },
-      { vmid: 'og:type', property: 'og:type', content: 'product.group' }
-    ]
-  }
-}
 
-export default defaults
+import { mapGetters } from 'vuex';
+import { htmlDecode } from '@vue-storefront/core/filters';
+
+export default {
+  computed: {
+    ...mapGetters('category', [
+      'getCurrentCategory',
+      'getCurrentCategoryProductQuery',
+      'getAllCategoryFilters',
+      'getCategoryBreadcrumbs',
+      'getCurrentCategoryPath'
+    ]),
+
+    productName () {
+      return this.product ? this.product.name : '';
+    },
+    productPrice () {
+      return this.product ? this.product.price : '';
+    },
+    productBrandBand () {
+      return this.product ? this.product.brand : this.product.band;
+    }
+  },
+  metaInfo () {
+    return {
+      title: 'Placeholder - categorypage',
+      meta: [
+        {
+          vmid: 'keywords',
+          name: 'keywords',
+          content: 'Placeholder - categorypageKeywords'
+        },
+        {
+          vmid: 'og:image',
+          property: 'og:image',
+          content: 'bild ...'
+        }, // TODO multiple images..
+        {
+          vmid: 'og:title',
+          property: 'og:title',
+          content: htmlDecode(this.getCurrentCategory.name)
+        },
+        {
+          vmid: 'og:url',
+          property: 'og:url',
+          content: htmlDecode(this.getCurrentCategory.url_path)
+        },
+        { vmid: 'og:type', property: 'og:type', content: 'product.group' }
+      ]
+    };
+  }
+};
--- src/modules/icmaa-meta/mixins/productMeta.ts
+++ src/modules/icmaa-meta/mixins/productMeta.ts
@@ -1,30 +1,20 @@
-import { storeCode, storeLang, store } from 'icmaa-meta/helper';
+import { storeCode, storeLang, store, storeCurrency } from 'icmaa-meta/helper';
 import { mapGetters } from 'vuex';
-import { htmlDecode } from '@vue-storefront/core/filters'
-import { currentStoreView, localizedRoute } from '@vue-storefront/core/lib/multistore'
-
-const storeView = currentStoreView();
+import { htmlDecode } from '@vue-storefront/core/filters';
 
 export default {
-
   computed: {
     ...mapGetters({
-      product: 'product/productCurrent',
-      originalProduct: 'product/productOriginal',
-      parentProduct: 'product/productParent',
-      attributesByCode: 'attribute/attributeListByCode',
-      attributesById: 'attribute/attributeListById',
-      breadcrumbs: 'product/breadcrumbs',
-      configuration: 'product/currentConfiguration',
-      options: 'product/currentOptions',
-      category: 'category/getCurrentCategory',
-      gallery: 'product/productGallery'
+      product: 'product/productCurrent'
     }),
     productName () {
       return this.product ? this.product.name : ''
     },
     productPrice () {
       return this.product ? this.product.price : ''
+    },
+    productBrandBand () {
+      return this.product ? this.product.brand : this.product.band
     }
   },
   metaInfo () {
@@ -33,19 +23,19 @@ export default {
       meta: [
         { vmid: 'description', name: 'description', content: 'Placeholder - productpageDescrition' },
         { vmid: 'keywords', name: 'keywords', content: 'Placeholder - productpageKeywords' },
-        { vmid: 'og:image', property: 'og:image', content: 'Placeholder - productpageImage(s)' }, // multiple ..
+        { vmid: 'og:image', property: 'og:image', content: htmlDecode(this.product.image) }, // TODO multiple images..
         { vmid: 'og:title', property: 'og:title', content: htmlDecode(this.productName) },
-        { vmid: 'og:url', property: 'og:url', content: storeCode() },
+        { vmid: 'og:url', property: 'og:url', content: htmlDecode(this.product.url_path) },
         { vmid: 'og:type', property: 'og:type', content: 'product' },
-        // { name: 'product.final_price', content: 'Placeholder - productpageFinalprice' }, // ??
-        // { name: 'product.price', content: 'Placeholder - productpagePrice' }, // ??
-        // { name: 'product.name', content: 'Placeholder - productpageName' }, // ??
+        { name: 'product.final_price', content: this.productPrice }, // ??
+        { name: 'product.price', content: this.productPrice }, // ??
+        { name: 'product.name', content: htmlDecode(this.productName) }, // ??
         { name: 'product:condition', content: 'new' },
-        { name: 'product:availability', content: 'test' },
-        { name: 'product:price:currency', content: 'test' },
-        { name: 'product:price:amount', content: htmlDecode(this.productPrice) },
-        { name: 'product:brand', content: 'Placeholder - productpageBrand' }
+        { name: 'product:availability', content: this.product.stock.is_in_stock }, // TODO available for order, in stock, out fo stock, preorder
+        { name: 'product:price:currency', content: storeCurrency() },
+        { name: 'product:price:amount', content: this.productPrice },
+        { name: 'product:brand', content: this.productBrandBand } // TODO mapping values
       ]
     }
   }
-};
+}
