1284e4f0afb2964acafb5e595e975613555d2f31
Dierk Landmann <d.landmann@impericon.com>
2019-09-04 15:56:12 +0200

    correct Urls

--- src/modules/icmaa-meta/mixins/productMeta.ts
+++ src/modules/icmaa-meta/mixins/productMeta.ts
@@ -1,6 +1,7 @@
-import { mapGetters } from 'vuex';
+import { storeCode } from 'icmaa-meta/helper';
 import { htmlDecode } from '@vue-storefront/core/filters';
 import { currentStoreView } from '@vue-storefront/core/lib/multistore';
+import config from 'config';
 
 const storeView = currentStoreView()
 const currencyCode = storeView.i18n.currencyCode
@@ -16,11 +17,14 @@ export default {
     productBrand () {
       return this.product.brand ? this.product.brand : this.product.band
     },
+    productUrl () {
+      return config.icmaa_meta.base_url + '/' + storeCode() + '/' + this.product.url_path; // TODO storecode "default"
+    },
     productFbImages () {
       let facebookImageTags = []
       this.product.media_gallery.forEach(image => {
         facebookImageTags.push(
-          { vmid: 'og:image', property: 'og:image', content: image.image }
+          { vmid: 'og:image', property: 'og:image', content: (config.icmaa_meta.base_url + '/media/catalog/product/') + image.image }
         )
       })
       // console.log(facebookImageTags);
@@ -34,13 +38,13 @@ export default {
         { vmid: 'description', name: 'description', content: 'Placeholder - productpageDescrition' },
         { vmid: 'keywords', name: 'keywords', content: 'Placeholder - productpageKeywords' },
         { vmid: 'og:title', property: 'og:title', content: htmlDecode(this.productName) },
-        { vmid: 'og:url', property: 'og:url', content: htmlDecode(this.product.url_path) },
+        { vmid: 'og:url', property: 'og:url', content: htmlDecode(this.productUrl) },
         { vmid: 'og:type', property: 'og:type', content: 'product' },
         { name: 'product.final_price', content: this.productPrice }, // ??
         { name: 'product.price', content: this.productPrice }, // ??
         { name: 'product.name', content: htmlDecode(this.productName) }, // ??
         { name: 'product:condition', content: 'new' },
-        { name: 'product:availability', content: this.product.stock.is_in_stock }, // TODO available for order, in stock, out fo stock, preorder
+        { name: 'product:availability', content: this.product.stock.is_in_stock }, // TODO mapping values - available for order, in stock, out fo stock, preorder
         { name: 'product:price:currency', content: currencyCode },
         { name: 'product:price:amount', content: this.productPrice },
         { name: 'product:brand', content: this.productBrand }, // TODO mapping values
