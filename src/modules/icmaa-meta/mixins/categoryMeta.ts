9b478c5d30ca1b386c0c5e4918f03fceb5a4d26d
Dierk Landmann <d.landmann@impericon.com>
2019-09-17 17:11:36 +0200

    wip - categoryMeta, cmsMeta

--- src/modules/icmaa-meta/mixins/categoryMeta.ts
+++ src/modules/icmaa-meta/mixins/categoryMeta.ts
@@ -1,5 +1,5 @@
 // title and description --> icmaa-cms/mixins/categoryExtras.ts
-import { storeCode } from 'icmaa-meta/helper'
+import { storeCode } from 'icmaa-meta/helper';
 import { htmlDecode } from '@vue-storefront/core/filters'
 import config from 'config'
 import { mapGetters } from 'vuex';
@@ -7,21 +7,36 @@ import { getThumbnailPath } from '@vue-storefront/core/helpers';
 
 export default {
   computed: {
-    ...mapGetters('catalog-next', ['getCategoryProducts']),
+    // ...mapGetters('catalog-next', ['getCategoryProducts']),
+    ...mapGetters({
+      getCurrentSearchQuery: 'category-next/getCurrentSearchQuery',
+      getCategoryProducts: 'category-next/getCategoryProducts',
+      getCurrentCategory: 'category-next/getCurrentCategory',
+      getCategoryProductsTotal: 'category-next/getCategoryProductsTotal',
+      getAvailableFilters: 'category-next/getAvailableFilters'
+    }),
     categoryUrl () {
       return (
-        config.icmaa_meta.base_url + '/' + storeCode() + '/' + this.getCurrentCategory.url_path
+        config.icmaa_meta.base_url +
+        '/' +
+        storeCode() +
+        '/' +
+        this.getCurrentCategory.url_path
       ); // TODO storecode "default"
     },
-    categoryImages () {
+    categoryFBImages () {
       let categoryImagesTags = [];
-      let limit = config.facebook.opengraph.imagesListInCategoryView
+      // let limit = config.facebook.opengraph.imagesListInCategoryView
 
-      this.getCategoryProducts.slice(0, limit).forEach(image => {
+      this.getCategoryProducts.slice(0, 4).forEach(image => {
         categoryImagesTags.push({
           property: 'og:image',
-          content:
-            getThumbnailPath('/catalog/product' + image.image, this.width, this.height, 'media')
+          content: getThumbnailPath(
+            '/catalog/product' + image.image,
+            this.width,
+            this.height,
+            'media'
+          )
         });
       });
 
@@ -31,10 +46,18 @@ export default {
   metaInfo () {
     return {
       meta: [
-        { vmid: 'og:title', property: 'og:title', content: htmlDecode(this.getCurrentCategory.name) },
-        { vmid: 'og:url', property: 'og:url', content: htmlDecode(this.categoryUrl) },
+        {
+          vmid: 'og:title',
+          property: 'og:title',
+          content: htmlDecode(this.getCurrentCategory.name)
+        },
+        {
+          vmid: 'og:url',
+          property: 'og:url',
+          content: htmlDecode(this.categoryUrl)
+        },
         { vmid: 'og:type', property: 'og:type', content: 'product.group' },
-        ...this.categoryImages
+        ...this.categoryFBImages
       ]
     };
   }
--- src/modules/icmaa-meta/mixins/cmsMeta.ts
+++ src/modules/icmaa-meta/mixins/cmsMeta.ts
@@ -7,22 +7,22 @@ export default {
   computed: {
     ...mapGetters('icmaaCmsPage', ['getPages']),
     cmsTitle () {
-      return this.getPages.identifier ? this.getPages.identifier : '';
+      return this.getPages.metaTitle ? this.getPages.metaTitle : '';
     },
     cmsUrl () {
       return config.icmaa_meta.base_url + '/' + storeCode() + '/' + this.getPages.identifier; // TODO storecode "default"
     },
     cmsDescription () {
-      return this.getPages.identifier ? this.getPages.identifier : '';
+      return this.getPages.metaDescription ? this.getPages.metaDescription : '';
     }
   },
   metaInfo () {
     return {
-      title: 'htmlDecode(this.cmsTitle)',
+      // title: htmlDecode(this.cmsTitle),
       meta: [
-        { vmid: 'description', name: 'description', content: 'htmlDecode(this.cmsDescription)' },
-        { vmid: 'og:url', property: 'og:url', content: 'test' },
-        { vmid: 'og:title', property: 'og:title', content: 'htmlDecode(this.cmsTitle)' }
+        { vmid: 'description', name: 'description', content: htmlDecode(this.cmsDescription) },
+        { vmid: 'og:url', property: 'og:url', content: htmlDecode(this.cmsUrl) },
+        { vmid: 'og:title', property: 'og:title', content: htmlDecode(this.cmsTitle) }
       ]
     }
   }
