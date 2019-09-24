03ec26d5c72768ff1499ead246ec0c5a06e7793a
Dierk Landmann <d.landmann@impericon.com>
2019-09-24 16:33:46 +0200

    cmsMeta with Storyblok-Data and Inheritance from config

--- src/modules/icmaa-cms/components/Page.ts
+++ src/modules/icmaa-cms/components/Page.ts
@@ -39,30 +39,5 @@ export default {
     await store.dispatch(
       'icmaaCmsPage/single', { value: route.params.identifier }
     )
-  },
-  metaInfo () {
-    let meta: any = {}
-    const metaKeys = ['title', 'tags', 'description']
-
-    metaKeys.forEach((value) => {
-      const key = camelCase('meta-' + value)
-      if (this.page.hasOwnProperty(key) && this.page[key] !== null) {
-        if (value === 'title') {
-          meta[value] = this.page[key]
-        } else {
-          if (!meta.hasOwnProperty('meta')) {
-            meta['meta'] = []
-          }
-
-          meta.meta.push({
-            vmid: value,
-            name: value,
-            content: this.page[key]
-          })
-        }
-      }
-    })
-
-    return meta
   }
 }
--- src/modules/icmaa-meta/mixins/cmsMeta.ts
+++ src/modules/icmaa-meta/mixins/cmsMeta.ts
@@ -1,35 +1,30 @@
-import { storeCode } from 'icmaa-meta/helper'
-import { htmlDecode } from '@vue-storefront/core/filters'
-import config from 'config'
-import { mapGetters } from 'vuex'
+import camelCase from 'lodash-es/camelCase';
 
 export default {
-  computed: {
-    ...mapGetters('icmaaCmsPage', ['getPages']),
-
-    cmsTitle () {
-      return this.getPages.metaTitle ? this.getPages.metaTitle : '';
-    },
-
-    cmsUrl () {
-      return config.icmaa_meta.base_url + '/' + storeCode() + '/' + this.getPages.identifier; // TODO storecode "default"
-    },
-    cmsDescription () {
-      return this.getPages.metaDescription ? this.getPages.metaDescription : '';
-    },
-    cmsOgImage () {
-      return this.getPages.metaOgImage ? this.getPages.metaOgImage : '';
-    }
-  },
   metaInfo () {
-    return {
-      title: htmlDecode(this.cmsTitle),
-      meta: [
-        { vmid: 'description', name: 'description', content: htmlDecode(this.cmsDescription) },
-        { vmid: 'og:url', property: 'og:url', content: 'url' },
-        { vmid: 'og:title', property: 'og:title', content: htmlDecode(this.cmsTitle) },
-        { vmid: 'og:image', property: 'og:image', content: htmlDecode(this.cmsOgImage) }
-      ]
-    }
+    let meta: any = {}
+    const metaKeys = ['title', 'tags', 'description', 'og:image', 'og:title', 'og:url']
+
+    metaKeys.forEach((value) => {
+      const key = camelCase('meta-' + value)
+      if (this.page.hasOwnProperty(key) && this.page[key] !== '') {
+        if (value === 'title') {
+          meta[value] = this.page[key]
+        } else {
+          if (!meta.hasOwnProperty('meta')) {
+            meta['meta'] = []
+          }
+
+          meta.meta.push({
+            vmid: value,
+            name: value,
+            content: this.page[key]
+          })
+        }
+      }
+    })
+    console.log(meta);
+
+    return meta
   }
 }
