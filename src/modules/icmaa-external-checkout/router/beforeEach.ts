86352460d84667b5f880622317690289ffb55f66
cewald <c.ewald@impericon.com>
2019-09-27 10:46:33 +0000

    Improve code and create helper classes

--- src/modules/icmaa-external-checkout/router/beforeEach.ts
+++ src/modules/icmaa-external-checkout/router/beforeEach.ts
@@ -1,33 +1,11 @@
-import {Route} from 'vue-router'
-import rootStore from '@vue-storefront/core/store'
-import config from 'config'
-import {currentStoreView} from '@vue-storefront/core/lib/multistore'
-import { isServer } from '@vue-storefront/core/helpers'
-
-export const shopControllerName = (userToken: string, cartToken: string): string => {
-  let shopUrl: string = config.externalCheckout.shopUrl
-  if (config.storeViews.multistore) {
-    const store = currentStoreView()
-    shopUrl = shopUrl.endsWith('/') ? shopUrl.slice(-1) : shopUrl
-    shopUrl = store.url.startsWith('/') ? shopUrl + store.url : store.url
-  }
-
-  return shopUrl + '/vue/cart/sync/token/' + userToken + '/cart/' + cartToken
-}
+import { Route } from 'vue-router'
+import { getRedirectToExternalCheckoutUrl } from '../helper'
 
 export function beforeEachGuard (to: Route, from: Route, next) {
-  const cartToken: string = rootStore.state.cart.cartServerToken
-  const userToken: string = rootStore.state.user.token
-
-  const storeCode = currentStoreView().storeCode
-  const multistoreEnabled: boolean = config.storeViews.multistore
-
-  if (to.name === 'checkout' || (multistoreEnabled && to.name === storeCode + '-checkout')) {
-    const redirectUrl = shopControllerName(userToken, cartToken)
-    if (!isServer) {
-      window.location.replace(redirectUrl)
-    }
+  const getCheckoutUrl = getRedirectToExternalCheckoutUrl(to.name)
+  if (!getCheckoutUrl) {
+    return next()
   }
 
-  next()
+  location.href = getCheckoutUrl as string
 }
