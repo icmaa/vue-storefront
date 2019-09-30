95ddf6f7253c2f28815c3879fb7d3a4997487ae6
cewald <c.ewald@impericon.com>
2019-09-30 17:55:58 +0000

    Simplify productMeta.ts because we have some helper classes now

--- src/modules/icmaa-meta/mixins/productMeta.ts
+++ src/modules/icmaa-meta/mixins/productMeta.ts
@@ -1,23 +1,15 @@
+import { mapGetters } from 'vuex'
 import { htmlDecode } from '@vue-storefront/core/filters'
 import { currentStoreView } from '@vue-storefront/core/lib/multistore'
-import { optionLabel } from '@vue-storefront/core/modules/catalog/helpers/optionLabel'
 import { getThumbnailPath } from '@vue-storefront/core/helpers'
 
 const storeView = currentStoreView()
 const currencyCode = storeView.i18n.currencyCode
 
 export default {
-  async asyncData ({ store }) {
-    /**
-     * Load the attribute options and labels we need for metaInfo()
-     * !!! It's important to know that attributes are removed from initial ssr state by default and put into
-     * localstorage on client-side using the "useInitialStateFilter" config to save some DOM output size (up to 50%).
-     * But this way we can't load the attribute values because they are already empty in state management when we try
-     * to load them in metaInfo(). So we removed "attributes" from "initialStateFilter" config value.
-     * @see https://github.com/DivanteLtd/vue-storefront/issues/3520#issuecomment-530336629
-     */
-    return store.dispatch('attribute/list', { filterValues: [ 'band', 'brand' ] })
-  },
+  ...mapGetters({
+    getOptionLabel: 'attribute/getOptionLabel'
+  }),
   computed: {
     productName () {
       return this.product ? this.product.name : ''
@@ -48,11 +40,6 @@ export default {
       return facebookImageTags
     }
   },
-  methods: {
-    getAttributeLabel (attributeKey: string = '', optionId: number) {
-      return optionLabel(this.$store.state.attribute, { attributeKey, optionId })
-    }
-  },
   metaInfo () {
     return {
       title: 'Placeholder - productpage', // TODO
@@ -67,7 +54,7 @@ export default {
         { name: 'product:availability', content: this.product.stock.is_in_stock }, // TODO mapping values - available for order, in stock, out fo stock, preorder
         { name: 'product:price:currency', content: currencyCode },
         { name: 'product:price:amount', content: this.productPrice },
-        { name: 'product:brand', content: this.getAttributeLabel(this.productBandOrBrandCode, this.productBandOrBrand) },
+        { name: 'product:brand', content: this.getOptionLabel(this.productBandOrBrandCode, this.productBandOrBrand) },
         ...this.productFbImages
       ]
     }
