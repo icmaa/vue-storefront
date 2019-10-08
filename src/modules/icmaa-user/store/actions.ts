4c7030b9448728ebf19e66f33262f3e9363658e4
cewald <c.ewald@impericon.com>
2019-10-08 09:32:02 +0000

    Set cluster on PDP view and click on department-logo

--- src/modules/icmaa-user/store/actions.ts
+++ src/modules/icmaa-user/store/actions.ts
@@ -3,10 +3,13 @@ import { StorageManager } from '@vue-storefront/core/lib/storage-manager'
 import RootState from '@vue-storefront/core/types/RootState'
 import UserState from '../types/UserState'
 import * as types from './mutation-types'
+import isEmpty from 'lodash-es/isEmpty'
 
 const actions: ActionTree<UserState, RootState> = {
   setCluster ({ commit }, cluster) {
-    commit(types.USER_ADD_SESSION_DATA, { key: 'cluster', value: cluster })
+    if (!isEmpty(cluster)) {
+      commit(types.USER_ADD_SESSION_DATA, { key: 'cluster', value: cluster })
+    }
   },
   async loadSessionData ({ commit }) {
     const usersCollection = StorageManager.get('user')
--- src/modules/icmaa-user/store/getters.ts
+++ src/modules/icmaa-user/store/getters.ts
@@ -8,6 +8,13 @@ const getters: GetterTree<UserState, RootState> = {
   getCluster (state): string|false {
     return (!isEmpty(state.sessionData) && state.sessionData.cluster)
       ? state.sessionData.cluster.toString() : false
+  },
+  getClusterString (state, getters, RootState, RootGetters): string|false {
+    if (getters.getCluster) {
+      return RootGetters['attribute/getOptionLabel']({ attributeKey: 'customercluster', optionId: getters.getCluster })
+    }
+
+    return false
   }
 }
 
