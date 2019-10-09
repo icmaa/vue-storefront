609abef97e76a16521a4dfce7d3b1446e5c62a98
cewald <c.ewald@impericon.com>
2019-10-09 05:13:17 +0000

    Fix type constraints to improve our mutation factory in `icmaa-cms`

--- src/modules/icmaa-teaser/types/TeaserState.ts
+++ src/modules/icmaa-teaser/types/TeaserState.ts
@@ -1,7 +1,22 @@
-export interface TeaserStateItem {
+import { AbstractStateItem } from 'icmaa-cms/types/AbstractState'
+
+export interface TeaserStateItem extends AbstractStateItem {
+  size: string,
+  active: boolean,
   showFrom: string,
   showTo: string,
-  active: boolean,
+  displayOrder: number,
+  tag: string[],
+  cluster: string[],
+  text1: string,
+  text2: string,
+  text3: string,
+  buttonText: string,
+  link: string,
+  imageUrl1X1: string,
+  imageUrl2X1: string,
+  textColor: string,
+  backgroundColor: string,
   [key: string]: any
 }
 
