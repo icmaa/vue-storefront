26e2b2748b050826ba66f16d969417b43a2392e5
cewald <c.ewald@impericon.com>
2019-10-14 17:55:54 +0000

    Add small improvements and translations

--- src/themes/icmaa-imp/components/core/ProductListing.vue
+++ src/themes/icmaa-imp/components/core/ProductListing.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="product-listing t-flex t-flex-wrap t-justify-start t-px-3 lg:t-px-4 lg:t--mx-2">
-    <product-tile v-for="(product, key) in products" :key="`${key}-${product.id}`" :product="product" class="t-px-1 lg:t-px-2 t-mb-8" :class="[ viewport === 'sm' ? 't-w-1/2' : 't-w-1/' + columns ]" />
+    <product-tile v-for="(product, key) in products" :key="`${key}-${product.id}`" :product="product" class="t-px-1 lg:t-px-2 t-mb-8" :class="[ 't-w-1/2 lg:t-w-1/' + columns ]" />
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
 import ProductTile from 'theme/components/core/ProductTile'
 
 export default {
@@ -22,9 +21,6 @@ export default {
       type: [Number, String],
       default: 4
     }
-  },
-  computed: {
-    ...mapGetters({ viewport: 'ui/getViewport' })
   }
 }
 </script>
