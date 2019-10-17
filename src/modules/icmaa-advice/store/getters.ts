1e4d0ff0a1523abca434a4cddc0bd2d803eb53b0
schill <s.chill@impericon.com>
2019-10-17 11:24:38 +0200

    New getter for cluster

--- src/modules/icmaa-advice/store/getters.ts
+++ src/modules/icmaa-advice/store/getters.ts
@@ -1,13 +1,36 @@
 import { GetterTree } from 'vuex';
 import AdviceState, { AdviceStateItem } from '../types/AdviceState';
 import RootState from '@vue-storefront/core/types/RootState';
+import itemActions from '@vue-storefront/core/modules/cart/store/actions/itemActions';
 
 import config from 'config';
+import head from 'lodash-es/head';
 
 const getters: GetterTree<AdviceState, RootState> = {
   getAdvice: (state): AdviceStateItem[] => state.items,
-  getSingleAdvice: state => (identifier): AdviceStateItem => {
-    return state.items.find(item => item.active === true);
+  getSingleAdvice: (state): AdviceStateItem => {
+    return state.items.length > 0 ? state.items.length[0] : [];
+  },
+  getClusterAdvice: (state, getters, RootState, rootGetters): AdviceStateItem => {
+    let items = state.items;
+    const cluster = rootGetters['user/getCluster'];
+
+    items = items.filter(i => {
+      if (i.cluster.length === 0) {
+        return true;
+      }
+
+      if (!cluster && i.cluster.includes(config.icmaa_cluster.noClusterValue)) {
+        return true;
+      }
+
+      return i.cluster.includes(cluster);
+    });
+    if (items.length === 0) {
+      return getters.getSingleAdvice();
+    }
+
+    return head(items);
   }
 };
 
