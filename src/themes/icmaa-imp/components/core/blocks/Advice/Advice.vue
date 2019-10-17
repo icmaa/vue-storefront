6f6dd931ca08b6d5379cfaa5859cb5e774809758
schill <s.chill@impericon.com>
2019-10-17 14:11:55 +0200

    Redirect to link

--- src/themes/icmaa-imp/components/core/blocks/Advice/Advice.vue
+++ src/themes/icmaa-imp/components/core/blocks/Advice/Advice.vue
@@ -1,13 +1,13 @@
 <template>
   <transition name="fade">
-    <div class="t-w-full row-01 t-bg-alt-1" id="advice" v-if="advice && isOpen">
+    <div class="t-w-full row-01 t-bg-alt-1 t-cursor-pointer" id="advice" v-if="advice && isOpen" @click="redirect">
       <div class="t-container t-flex t-items-center t-justify-between t-h-full t-text-white">
         <div class="" />
         <div>{{ advice.text }}</div>
-        <button-component class="t-text-xs t-uppercase t-text-white" :type="'ghost'">
+        <button-component class="t-text-xs t-uppercase t-text-white" :type="'ghost-custom'" :custom-color="'white'">
           {{ advice.buttonText }}
         </button-component>
-        <button-component class="t-justify-end" type="transparent-white" icon="close" :icon-only="true" @click.native="close">
+        <button-component class="t-justify-end" type="transparent-white" icon="close" :icon-only="true" @click.stop.native="close">
           {{ $t('Close') }}
         </button-component>
       </div>
@@ -41,15 +41,19 @@ export default {
   methods: {
     close () {
       this.isOpen = false
+    },
+    redirect () {
+      this.$router.push(this.localizedRoute(this.advice.link))
+      this.isOpen = false
     }
   },
   computed: {
-    ...mapGetters('icmaaAdvice', ['getSingleAdvice', 'getClusterAdvice']),
-    advice () {
-      return this.getSingleAdvice
+    ...mapGetters('icmaaAdvice', ['getAdvice', 'getSingleAdvice']),
+    allAdvices () {
+      return this.getAdvice
     },
-    clusterAdvice () {
-      return this.getClusterAdvice
+    advice () {
+      return this.getSingleAdvice(this.tags)
     }
   },
   mounted () {
