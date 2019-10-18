5ed306c01811f571a9ad9ec858d58f4654253758
schill <s.chill@impericon.com>
2019-10-18 13:54:09 +0200

    code format

--- src/modules/icmaa-advice/store/actions.ts
+++ src/modules/icmaa-advice/store/actions.ts
@@ -1,22 +1,19 @@
-import { ActionTree } from 'vuex';
-import {
-  list as listAbstract,
-  MutationTypesInterface
-} from 'icmaa-cms/store/abstract/actions';
+import { ActionTree } from 'vuex'
+import { list as listAbstract, MutationTypesInterface } from 'icmaa-cms/store/abstract/actions'
 
-import { adviceStorageKey as storageKey } from './';
-import * as types from './mutation-types';
-import AdviceState, { AdviceStateItem } from '../types/AdviceState';
-import RootState from '@vue-storefront/core/types/RootState';
+import { adviceStorageKey as storageKey } from './'
+import * as types from './mutation-types'
+import AdviceState, { AdviceStateItem } from '../types/AdviceState'
+import RootState from '@vue-storefront/core/types/RootState'
 
-import { getCurrentStoreviewDatetime } from '../../icmaa-teaser/helper/date'
+import { getCurrentStoreviewDatetime } from 'icmaa-teaser/helper/date'
 
-const documentType = 'advice';
+const documentType = 'advice'
 const mutationTypes: MutationTypesInterface = {
   add: types.ICMAA_ADVICE_ADD,
   upd: types.ICMAA_ADVICE_UPD,
   rmv: types.ICMAA_ADVICE_RMV
-};
+}
 
 const actions: ActionTree<AdviceState, RootState> = {
   list: async (context): Promise<AdviceStateItem[]> => {
@@ -24,7 +21,7 @@ const actions: ActionTree<AdviceState, RootState> = {
       active: { in: true },
       show_from: { 'lt-date': getCurrentStoreviewDatetime() },
       show_to: { 'gt-date': getCurrentStoreviewDatetime() }
-    };
+    }
 
     return listAbstract<AdviceStateItem>({
       documentType,
@@ -33,8 +30,8 @@ const actions: ActionTree<AdviceState, RootState> = {
       context,
       options,
       identifier: 'uuid'
-    });
+    })
   }
-};
+}
 
-export default actions;
+export default actions
