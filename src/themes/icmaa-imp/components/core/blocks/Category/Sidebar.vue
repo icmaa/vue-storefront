a856a4a81a8375bb1be71d4d20a067b49a8eb81a
cewald <c.ewald@impericon.com>
2019-10-20 14:06:56 +0000

    Fix z-index bug for clear filters button

--- src/themes/icmaa-imp/components/core/blocks/Category/Sidebar.vue
+++ src/themes/icmaa-imp/components/core/blocks/Category/Sidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <sidebar :title="$t('Filter')" :close-on-click="false" :class="[ hasActiveFilters ? 't-pb-40' : 't-pb-24' ]">
-    <div v-if="hasActiveFilters" class="t-fixed t-bottom-0 t-left-0 t-p-4 t-bg-white t-shadow t-w-full">
+    <div v-if="hasActiveFilters" class="t-fixed t-bottom-0 t-left-0 t-z-10 t-p-4 t-bg-white t-shadow t-w-full">
       <button-component @click="resetAllFilters" v-text="$t('Clear filters')" class="t-w-full" />
     </div>
     <div v-for="(group, groupKey) in groupedFilters" :key="groupKey">
