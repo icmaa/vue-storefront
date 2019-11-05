45072bef1523c145d94bb250ca14fcd7774d3149
schill <s.chill@impericon.com>
2019-11-05 10:28:54 +0100

    Changed styling

--- src/themes/icmaa-imp/components/core/blocks/Switcher/Language.vue
+++ src/themes/icmaa-imp/components/core/blocks/Switcher/Language.vue
@@ -5,26 +5,25 @@
     </p>
 
     <div class="t-flex t-flex-wrap">
-      <div class=" t-w-1/4 sm:t-w-1/6 t-p-1">
-        <div class="t-border t-border-base-lighter t-rounded-sm t-pt-1" :class="{ 't-bg-base-lightest': config.i18n.defaultCountry === currentStoreView.i18n.defaultCountry }">
-          <a href="/" class="t-flex t-flex-col t-items-center">
-            <flag-icon :iso="config.i18n.defaultCountry" class="t-border" format="4x3" width="60" height="45" />
-            <div class="t-text-sm">{{ config.i18n.defaultCountry }}</div>
-          </a>
-        </div>
+      <div class=" t-w-full sm:t-w-1/2 t-p-1">
+        <a href="/" class="t-flex t-items-center t-border t-border-base-lighter t-rounded-sm t-p-1" :class="{ 't-bg-base-lightest': config.i18n.defaultCountry === currentStoreView.i18n.defaultCountry }">
+          <flag-icon :iso="config.i18n.defaultCountry" class="t-border t-border-base-lightest t-mx-2" format="4x3" width="60" height="45" />
+          <div class="t-text-sm">
+            {{ config.i18n.fullCountryName }}
+          </div>
+        </a>
       </div>
 
-      <div class="t-w-1/4 sm:t-w-1/6 t-p-1" v-for="(storeView, storeCode) in storeViews" :key="storeCode" v-if="!storeView.disabled && typeof storeView === 'object' && storeView.i18n">
-        <div class="t-border t-border-base-lighter t-rounded-sm t-pt-1" :class="{ 't-bg-base-lightest': storeView.storeId === currentStoreView.storeId }">
-          <a :href="storeView.url" class="t-flex t-flex-col t-items-center">
-            <flag-icon :iso="storeView.i18n.defaultCountry" class="t-border t-border-base-lightest" format="4x3" width="60" height="45" />
-            <div class="t-text-sm">{{ storeView.i18n.defaultCountry }}</div>
-          </a>
-        </div>
+      <div class="t-w-full sm:t-w-1/2 t-p-1" v-for="(storeView, storeCode) in storeViews" :key="storeCode" v-if="!storeView.disabled && typeof storeView === 'object' && storeView.i18n">
+        <a :href="storeView.url" class="t-flex t-items-center t-border t-border-base-lighter t-rounded-sm t-p-1" :class="{ 't-bg-base-lightest': storeView.storeId === currentStoreView.storeId }">
+          <flag-icon :iso="storeView.i18n.defaultCountry" class="t-border t-border-base-lightest t-mx-2" format="4x3" width="60" height="45" />
+          <div class="t-text-sm">{{ storeView.i18n.fullCountryName }}</div>
+        </a>
       </div>
     </div>
   </modal>
 </template>
+
 <script>
 import config from 'config'
 import Modal from 'theme/components/core/Modal.vue'
