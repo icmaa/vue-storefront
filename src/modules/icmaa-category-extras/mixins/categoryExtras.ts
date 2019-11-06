7f7d6087fcdd286168cd8f832b28880385484724
cewald <c.ewald@impericon.com>
2019-11-06 14:16:15 +0000

    Fix category-extras vs. category-meta mixin

--- src/modules/icmaa-category-extras/mixins/categoryExtras.ts
+++ src/modules/icmaa-category-extras/mixins/categoryExtras.ts
@@ -1,7 +1,3 @@
-import { mapGetters } from 'vuex'
-
-import { htmlDecode } from '@vue-storefront/core/filters/html-decode'
-
 export default {
   async asyncData ({ store, route }) {
     /**
@@ -40,35 +36,6 @@ export default {
     getCategoryExtrasValueOrCategoryValue (key: string, catKey: string = 'name'): any {
       return this.categoryExtras && this.categoryExtras[key]
         ? this.categoryExtras[key] : this.getCurrentCategory[catKey]
-    }
-  },
-  computed: {
-    ...mapGetters('category-next', ['getCurrentCategory']),
-    ...mapGetters({ categoryExtras: 'icmaaCategoryExtras/getCategoryExtrasByCurrentCategory' }),
-    title (): string {
-      return this.getCategoryExtrasValueOrCategoryValue('title')
-    },
-    description (): string {
-      return this.getCategoryExtrasValueOrCategoryValue('description')
-    },
-    metaTitle (): string {
-      return this.getCategoryExtrasValueOrCategoryValue('metaTitle')
-    },
-    metaDescription (): string|boolean {
-      return this.categoryExtras && this.categoryExtras.metaDescription
-        ? this.categoryExtras.metaDescription : false
-    }
-  },
-  metaInfo () {
-    let meta = []
-
-    if (this.metaDescription) {
-      meta.push({ vmid: 'description', name: 'description', content: htmlDecode(this.metaDescription) })
-    }
-
-    return {
-      title: htmlDecode(this.metaTitle),
-      meta
     }
   }
 }
