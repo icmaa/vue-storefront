5c9ccfbfb017588282aa6c5f27575e7726375f34
schill <s.chill@impericon.com>
2019-11-06 12:45:32 +0100

    Icon size

--- src/themes/icmaa-imp/components/core/blocks/Switcher/Language.vue
+++ src/themes/icmaa-imp/components/core/blocks/Switcher/Language.vue
@@ -7,16 +7,16 @@
     <div class="t-flex t-flex-wrap">
       <div class=" t-w-full sm:t-w-1/2 t-p-1">
         <a href="/" class="t-flex t-items-center t-border t-border-base-lighter t-rounded-sm t-p-1" :class="{ 't-bg-base-lightest': config.i18n.defaultCountry === currentStoreView.i18n.defaultCountry }">
-          <flag-icon :iso="config.i18n.defaultCountry" class="t-border t-border-base-lightest t-mx-2" format="4x3" width="60" height="45" />
+          <flag-icon :iso="config.i18n.defaultCountry" class="t-border t-border-base-lightest t-mx-2" format="4x3" width="40" height="30" />
           <div class="t-text-sm">
             {{ config.i18n.fullCountryName }}
           </div>
         </a>
       </div>
 
-      <div class="t-w-full sm:t-w-1/2 t-p-1" v-for="(storeView, storeCode) in storeViews" :key="storeCode" v-if="!storeView.disabled && typeof storeView === 'object' && storeView.i18n">
+      <div class="t-w-full sm:t-w-1/2 t-p-1" v-for="(storeView, storeCode) in config.storeViews" :key="storeCode" v-if="!storeView.disabled && typeof storeView === 'object' && storeView.i18n">
         <a :href="storeView.url" class="t-flex t-items-center t-border t-border-base-lighter t-rounded-sm t-p-1" :class="{ 't-bg-base-lightest': storeView.storeId === currentStoreView.storeId }">
-          <flag-icon :iso="storeView.i18n.defaultCountry" class="t-border t-border-base-lightest t-mx-2" format="4x3" width="60" height="45" />
+          <flag-icon :iso="storeView.i18n.defaultCountry" class="t-border t-border-base-lightest t-mx-2" format="4x3" width="40" height="30" />
           <div class="t-text-sm">{{ storeView.i18n.fullCountryName }}</div>
         </a>
       </div>
@@ -43,9 +43,6 @@ export default {
   computed: {
     currentStoreView () {
       return currentStoreView()
-    },
-    storeViews () {
-      return config.storeViews
     },
     config () {
       return config
