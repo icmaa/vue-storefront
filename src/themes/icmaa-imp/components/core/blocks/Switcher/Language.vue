fa6e559d727de6d411f32305cc5af74d443e53ad
cewald <c.ewald@impericon.com>
2019-11-08 10:45:55 +0000

    Revert changes on language-switcher

--- src/themes/icmaa-imp/components/core/blocks/Switcher/Language.vue
+++ src/themes/icmaa-imp/components/core/blocks/Switcher/Language.vue
@@ -3,49 +3,50 @@
     <p slot="header">
       {{ $t('Choose your country') }}
     </p>
-
-    <div class="t-flex t-flex-wrap">
-      <div class=" t-w-full sm:t-w-1/2 t-p-1">
-        <a href="/" class="t-flex t-items-center t-border t-border-base-lighter t-rounded-sm t-p-1" :class="{ 't-bg-base-lightest': config.i18n.defaultCountry === currentStoreView.i18n.defaultCountry }">
-          <flag-icon :iso="config.i18n.defaultCountry" class="t-border t-border-base-lightest t-mx-2" format="4x3" width="40" height="30" />
-          <div class="t-text-sm">
-            {{ config.i18n.fullCountryName }}
-          </div>
-        </a>
-      </div>
-
-      <div class="t-w-full sm:t-w-1/2 t-p-1" v-for="(storeView, storeCode) in config.storeViews" :key="storeCode" v-if="!storeView.disabled && typeof storeView === 'object' && storeView.i18n">
-        <a :href="storeView.url" class="t-flex t-items-center t-border t-border-base-lighter t-rounded-sm t-p-1" :class="{ 't-bg-base-lightest': storeView.storeId === currentStoreView.storeId }">
-          <flag-icon :iso="storeView.i18n.defaultCountry" class="t-border t-border-base-lightest t-mx-2" format="4x3" width="40" height="30" />
-          <div class="t-text-sm">{{ storeView.i18n.fullCountryName }}</div>
-        </a>
+    <div slot="content">
+      <div :class="{ 'columns': enableColumns }">
+        <div class="country country-current">
+          <h3>{{ $t(config.i18n.fullCountryName) }}</h3>
+          <ul>
+            <li><a href="/">{{ $t(config.i18n.fullLanguageName) }}</a></li>
+          </ul>
+        </div>
+        <div class="country country-available" v-for="(storeView, storeCode) in storeViews" :key="storeCode" v-if="!storeView.disabled && typeof storeView === 'object' && storeView.i18n">
+          <h3>{{ $t(storeView.i18n.fullCountryName) }}</h3>
+          <ul>
+            <li><a :href="storeView.url">{{ $t(storeView.i18n.fullLanguageName) }}</a></li>
+          </ul>
+        </div>
       </div>
     </div>
   </modal>
 </template>
-
 <script>
-import config from 'config'
 import Modal from 'theme/components/core/Modal.vue'
-import FlagIcon from 'theme/components/core/blocks/FlagIcon'
-import { currentStoreView } from '@vue-storefront/core/lib/multistore'
-
+import config from 'config'
 export default {
   components: {
-    Modal,
-    FlagIcon
+    Modal
   },
   data () {
     return {
+      minCountryPerColumn: 3,
       componentLoaded: false
     }
   },
   computed: {
-    currentStoreView () {
-      return currentStoreView()
+    storeViews () {
+      return config.storeViews
     },
     config () {
       return config
+    },
+    enableColumns () {
+      var enableStoreViews = Object.keys(config.storeViews).filter((key) => {
+        var value = config.storeViews[key]
+        return (typeof value === 'object' && value.disabled === false)
+      })
+      return enableStoreViews.length > this.minCountryPerColumn
     }
   },
   mounted () {
@@ -61,3 +62,36 @@ export default {
   }
 }
 </script>
+<style lang="scss" scoped>
+  h3 {
+    margin-top: 0;
+    margin-bottom: 0.5em;
+  }
+  .columns {
+    -moz-column-count: 2;
+    column-count: 2;
+    column-gap: 15px;
+    .country {
+      -webkit-column-break-inside: avoid;
+      page-break-inside: avoid;
+      break-inside: avoid;
+    }
+  }
+  .country {
+    margin-bottom: 2em;
+    color: #4f4f4f;
+  }
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+    margin-left: -1em;
+    li {
+      display: inline-block;
+      margin-left: 1em;
+      a {
+        font-size: 0.9em;
+      }
+    }
+  }
+</style>
