20f31d63a63eb34b921438ad0a91570872f4750a
cewald <c.ewald@impericon.com>
2019-11-08 17:22:02 +0000

    Initial commit for DIY login button

--- src/themes/icmaa-imp/components/core/blocks/Auth/FacebookLoginButton.vue
+++ src/themes/icmaa-imp/components/core/blocks/Auth/FacebookLoginButton.vue
@@ -1,6 +1,11 @@
 <template>
-  <button-component type="facebook" icon="facebook" icon-set="icmaa" icon-position="left">
-    {{ $t('Login with Facebook') }}
+  <button-component @click="toggleLogin" type="facebook" icon="facebook" icon-set="icmaa" icon-position="left" :disabled="disabled">
+    <template v-if="!connected">
+      {{ $t('Login with Facebook') }}
+    </template>
+    <template v-else>
+      {{ $t('Logout from Facebook') }}
+    </template>
   </button-component>
 </template>
 
@@ -14,23 +19,105 @@ export default {
   },
   data () {
     return {
-      appId: 103608836440301,
-      loginOptions: { scope: 'email', return_scopes: true }
+      appId: '103608836440301',
+      version: 'v5.0',
+      loginOptions: { scope: 'email', return_scopes: true },
+      options: {},
+      working: false,
+      connected: false
+    }
+  },
+  created () {
+    const created = new Promise(async resolve => {
+      const { appId, version, options } = this
+      const sdk = await this.getFbSdk({ appId, version, options })
+      const fbLoginStatus = await this.getFbLoginStatus()
+      if (fbLoginStatus.status === 'connected') {
+        this.connected = true
+      }
+      this.$emit('sdk-init', { FB: sdk })
+      resolve()
+    })
+    this.doAsync(created)
+  },
+  computed: {
+    disabled () {
+      return this.working === true
     }
   },
   methods: {
-    fbSdkInit (response) {
-      console.log(response)
+    toggleLogin () {
+      this.$emit('click')
+      if (this.connected) {
+        this.logout()
+      } else {
+        this.login()
+      }
     },
-    fbLogin (response, test) {
-      console.log(response, test)
+    async login () {
+      const login = this.fbLogin(this.loginOptions)
+      const response = await this.doAsync(login)
+      if (response.status === 'connected') {
+        this.connected = true
+      } else {
+        this.connected = false
+      }
+      this.$emit('login', response)
+      return login
     },
-    fbConnect (response) {
-      console.log(response)
-      this.$store.dispatch('notification/spawnNotification', {
-        type: 'success',
-        message: this.$t('You are logged in!'),
-        action1: { label: this.$t('OK') }
+    async logout () {
+      const logout = this.fbLogout()
+      const response = await this.doAsync(logout)
+      this.connected = false
+      this.$emit('logout', response)
+      return logout
+    },
+    async doAsync (promise) {
+      this.working = true
+      await promise
+      this.working = false
+      return promise
+    },
+    async initFbSdk (options) {
+      return new Promise(resolve => {
+        window.fbAsyncInit = function () {
+          const defaults = { cookie: true, xfbml: true }
+          options = { ...defaults, ...options }
+          window.FB.init(options)
+          resolve()
+        };
+        (function (d, s, id) {
+          const fjs = d.getElementsByTagName(s)[0]
+          if (d.getElementById(id)) { return; }
+          const js = d.createElement(s); js.id = id
+          js.src = '//connect.facebook.net/en_US/sdk.js'
+          fjs.parentNode.insertBefore(js, fjs)
+        }(document, 'script', 'facebook-jssdk'))
+      })
+    },
+    async getFbSdk (options) {
+      return new Promise(async resolve => {
+        if (window.FB) {
+          resolve(window.FB)
+        } else {
+          await this.initFbSdk(options)
+          resolve(window.FB)
+        }
+      })
+    },
+    async fbLogin (options) {
+      return new Promise(resolve => {
+        window.FB.login(response => resolve(response), options)
+      })
+    },
+    async getFbLoginStatus () {
+      return new Promise(resolve => {
+        window.FB.getLoginStatus(response => resolve(response))
+      })
+    },
+    async fbLogout () {
+      return new Promise(resolve => {
+        window.FB.logout(response => resolve(response))
       })
     }
   }
