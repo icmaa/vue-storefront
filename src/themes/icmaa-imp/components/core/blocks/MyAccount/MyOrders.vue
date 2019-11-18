4ad4e0635c94d68541e9ab7d554f9d9ae23b3848
schill <s.chill@impericon.com>
2019-11-18 13:05:30 +0100

    Status icon and styling

--- src/themes/icmaa-imp/components/core/blocks/MyAccount/MyOrders.vue
+++ src/themes/icmaa-imp/components/core/blocks/MyAccount/MyOrders.vue
@@ -6,56 +6,32 @@
     <!-- My orders body -->
     <div class="t-text-sm">
       <div class="" v-show="!isHistoryEmpty">
-        <table class="t-w-full t-table-auto t-text-left">
-          <thead>
-            <tr>
-              <th v-show="viewport !== 'xs'" class="">
-                {{ $t('Order ID') }}
-              </th>
-              <th class="">
-                {{ $t('Date') }}
-              </th>
-              <th class="">
-                {{ $t('Value') }}
-              </th>
-              <th class="">
-                {{ $t('Status') }}
-              </th>
-              <th class="">
-                &nbsp;
-              </th>
-            </tr>
-          </thead class="">
-          <tbody>
-            <template v-for="order in ordersHistory">
-              <tr :order="order" :key="order.entity_id" class="t-cursor-pointer t-border-t t-border-base-lightest t-px-2 t-py-3" @click="redirect(order.entity_id)">
-                <td v-show="viewport !== 'xs'" class="t-py-2">
-                  #{{ order.increment_id }}
-                </td>
-                <td class="t-py-2">
-                  {{ order.created_at | date }}
-                </td>
-                <td class="t-py-2">
-                  {{ order.grand_total | price }}
-                </td>
-                <td class="t-py-2 t-flex t-items-center">
-                  <!-- {{ order.status | capitalize }}-->
-                  <span class="status" />&nbsp;{{ order.status | capitalize }}
-                </td>
-                <td class="">
-                  <material-icon icon="chevron_right" size="md" class="t-align-middle" />
-                </td>
-              </tr>
-            </template>
-          </tbody>
-        </table>
+        <template v-for="order in ordersHistory">
+          <div :order="order" :key="order.entity_id" class="t-flex t-flex-wrap t-items-center t-cursor-pointer t-rounded-sm t-bg-base-lightest t-mb-4 t-px-2 t-py-3" @click="redirect(order.entity_id)">
+            <div class="t-flex t-flex-wrap t-w-2/5">
+              <div v-show="viewport !== 'xs'" class="t-w-full sm:t-w-1/2 t-p-2">#{{ order.increment_id }}</div>
+              <div class="t-w-full sm:t-w-1/2 t-p-2">
+                {{ order.created_at | date }}
+              </div>
+            </div>
+            <div class="t-flex t-flex-wrap t-w-2/5">
+              <div class="t-w-full sm:t-w-1/2 t-p-2">
+                {{ order.grand_total | price }}
+              </div>
+
+              <div class="t-w-full sm:t-w-1/2 t-p-2 t-flex t-items-center"><status-icon :status="order.status" /><span v-show="viewport !== 'xs'" class="status" />&nbsp;{{ order.status | capitalize }}</div>
+            </div>
+            <div class="t-w-1/5 t-flex t-justify-center">
+              <material-icon icon="chevron_right" size="md" class="t-align-middle" />
+            </div>
+          </div>
+        </template>
       </div>
       <div class="" v-show="isHistoryEmpty">
         <p>{{ $t('No orders yet') }}</p>
       </div>
       <!-- Custom design -->
       <div />
-
       <div />
     </div>
   </div>
@@ -66,30 +42,22 @@ import { mapGetters } from 'vuex'
 import Headline from 'theme/components/core/blocks/MyAccount/Headline'
 import UserOrder from '@vue-storefront/core/modules/order/components/UserOrdersHistory'
 import MaterialIcon from 'theme/components/core/blocks/MaterialIcon'
+import StatusIcon from 'theme/components/core/blocks/MyAccount/StatusIcon.vue'
 
 export default {
   mixins: [UserOrder],
   components: {
     Headline,
-    MaterialIcon
+    MaterialIcon,
+    StatusIcon
   },
   computed: {
     ...mapGetters({ viewport: 'ui/getViewport' })
   },
   methods: {
-    redirect (orderId) {
+    redirect(orderId) {
       this.$router.push(this.localizedRoute(`/my-account/orders/${orderId}`))
     }
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.status {
-  height: 20px;
-  width: 20px;
-  background-color: greenyellow;
-  border-radius: 50%;
-  display: inline-block;
-}
-</style>
