df651d14dee86d6b26f88b79698811aeee41b027
schill <s.chill@impericon.com>
2019-11-19 16:16:23 +0100

    Styling improvements

--- src/themes/icmaa-imp/components/core/blocks/MyAccount/MyOrder.vue
+++ src/themes/icmaa-imp/components/core/blocks/MyAccount/MyOrder.vue
@@ -2,16 +2,23 @@
   <div class="t-p-4 t-bg-white" v-if="order">
     <!-- My order header -->
     <headline icon="local_mall">
-{{ $t('Order #') }}{{ order.increment_id }}
-</headline>
+      {{ $t('Order #') }}{{ order.increment_id }}
+    </headline>
 
     <!-- <a href="#" class="t-align-right t-text-base-light" @click.prevent="remakeOrder(singleOrderItems)">{{ $t('Remake order') }}</a>-->
-    <div class="t-flex t-flex-wrap t-justify-between t-bg-base-lightest t-text-sm t-p-2 t-mb-4">
+    <div class="t-flex t-flex-wrap t-justify-between t-bg-base-lightest t-rounded-sm t-text-sm t-p-2 t-mb-4">
       <div class="t-flex">
-        <div class="t-w-full t-flex t-items-center"><div><strong>{{ $t('Status') }}</strong></div><status-icon :status="order.status" /></div>
-        <div class="t-w-full">{{ order.status | capitalize }}</div>
+        <div class="t-w-full t-flex t-items-center t-py-1">
+          <div>
+            <strong>{{ $t('Status') }}</strong>
+          </div>
+          <status-icon :status="order.status" />
+          <div>{{ order.status | capitalize }}</div>
+        </div>
       </div>
-      <div><strong>{{ $t('Order date') }}</strong> {{ order.created_at | date('LLL') }}</div>
+      <div class="t-py-2">
+        <strong>{{ $t('Order date') }}</strong> {{ order.created_at | date('LLL') }}
+      </div>
     </div>
 
     <!-- Order information -->
@@ -24,23 +31,19 @@
           <div class="t-border-b">
             <strong>{{ $t('Billing address') }}</strong>
           </div>
-          <address>
-            <p>{{ billingAddress.firstname }} {{ billingAddress.lastname }}</p>
-            <p>{{ billingAddress.street[0] }} {{ billingAddress.street[1] }}</p>
-            <p>{{ billingAddress.postcode }} {{ billingAddress.city }}</p>
-            <p>{{ billingAddress.country }}</p>
-          </address>
+          <p>{{ billingAddress.firstname }} {{ billingAddress.lastname }}</p>
+          <p>{{ billingAddress.street[0] }} {{ billingAddress.street[1] }}</p>
+          <p>{{ billingAddress.postcode }} {{ billingAddress.city }}</p>
+          <p>{{ billingAddress.country }}</p>
         </div>
         <div class="t-w-full sm:t-w-1/2 t-mb-4 sm:t-pr-4">
           <div class="t-border-b">
             <strong>{{ $t('Shipping address') }}</strong>
           </div>
-          <address>
-            <p>{{ shippingAddress.firstname }} {{ shippingAddress.lastname }}</p>
-            <p>{{ shippingAddress.street[0] }} {{ shippingAddress.street[1] }}</p>
-            <p>{{ shippingAddress.postcode }} {{ shippingAddress.city }}</p>
-            <p>{{ shippingAddress.country }}</p>
-          </address>
+          <p>{{ shippingAddress.firstname }} {{ shippingAddress.lastname }}</p>
+          <p>{{ shippingAddress.street[0] }} {{ shippingAddress.street[1] }}</p>
+          <p>{{ shippingAddress.postcode }} {{ shippingAddress.city }}</p>
+          <p>{{ shippingAddress.country }}</p>
         </div>
         <div class="t-w-full sm:t-w-1/2 t-mb-4 sm:t-pr-4">
           <div class="t-border-b">
@@ -57,12 +60,12 @@
       </div>
     </div>
 
-    <!-- Ordered products -->
+    <!-- products -->
     <div class="t-text-lg t-py-2 t-mb-4">
       {{ $t('Items ordered') }}
     </div>
-    <div class="t-w-full t-flex t-mb-2 t-pb-2 t-border-b t-border-base-lightest" v-for="item in singleOrderItems" :key="item.item_id">
-      <div class="t-w-1/3 sm:t-w-1/4 xl:t-w-1/6 t-mr-4">
+    <div class="t-w-full t-flex t-mb-2 t-pb-2 t-border-b t-rounded-sm t-border-base-lightest" v-for="item in singleOrderItems" :key="item.item_id">
+      <div class="t-w-1/3 sm:t-w-1/6 t-mr-4">
         <product-image :image="{ src: getThumbnail(item.thumbnail, 20, 20), loading: getThumbnail(item.thumbnail, 20, 20) }" />
       </div>
       <div class="t-w-2/3 t-py-2">
@@ -75,28 +78,28 @@
       </div>
     </div>
 
-    <!-- price -->
-    <div class="t-bg-base-lightest t-p-4 t-text-sm">
+    <!-- order price -->
+    <div class="t-bg-base-lightest t-p-4 t-text-sm t-rounded-sm">
       <div class="t-w-full t-text-right">
-        <div class="t-flex t-justify-between">
-          <span class="t-w-1/2">{{ $t('Subtotal') }}</span>
-          <span class="t-w-1/2">{{ order.subtotal | price }}</span>
+        <div class="t-flex t-justify-between sm:t-justify-end">
+          <span class="t-w-1/2 sm:t-w-1/3">{{ $t('Subtotal') }}</span>
+          <span class="t-w-1/2 sm:t-w-1/3">{{ order.subtotal | price }}</span>
         </div>
-        <div class="t-flex t-justify-between">
-          <span class="t-w-1/2">{{ $t('Shipping') }}</span>
-          <span class="t-w-1/2">{{ order.shipping_amount | price }}</span>
+        <div class="t-flex t-justify-between sm:t-justify-end">
+          <span class="t-w-1/2 sm:t-w-1/3">{{ $t('Shipping') }}</span>
+          <span class="t-w-1/2 sm:t-w-1/3">{{ order.shipping_amount | price }}</span>
         </div>
-        <div class="t-flex t-justify-between">
-          <span class="t-w-1/2">{{ $t('Tax') }}</span>
-          <span class="t-w-1/2">{{ (order.tax_amount + order.discount_tax_compensation_amount) | price }}</span>
+        <div class="t-flex t-justify-between sm:t-justify-end">
+          <span class="t-w-1/2 sm:t-w-1/3">{{ $t('Tax') }}</span>
+          <span class="t-w-1/2 sm:t-w-1/3">{{ (order.tax_amount + order.discount_tax_compensation_amount) | price }}</span>
         </div>
-        <div v-if="order.discount_amount" class="t-flex t-justify-between">
-          <span class="t-w-1/2">{{ $t('Discount') }}</span>
-          <span class="t-w-1/2">{{ order.discount_amount | price }}</span>
+        <div v-if="order.discount_amount" class="t-flex t-justify-between sm:t-justify-end">
+          <span class="t-w-1/2 sm:t-w-1/3">{{ $t('Discount') }}</span>
+          <span class="t-w-1/2 sm:t-w-1/3">{{ order.discount_amount | price }}</span>
         </div>
-        <div class="t-flex t-justify-between t-font-bold">
-          <span class="t-w-1/2">{{ $t('Grand total') }}</span>
-          <span class="t-w-1/2">{{ order.grand_total | price }}</span>
+        <div class="t-flex t-justify-between sm:t-justify-end t-font-bold">
+          <span class="t-w-1/2 sm:t-w-1/3">{{ $t('Grand total') }}</span>
+          <span class="t-w-1/2 sm:t-w-1/3">{{ order.grand_total | price }}</span>
         </div>
       </div>
     </div>
--- src/themes/icmaa-imp/components/core/blocks/MyAccount/MyOrders.vue
+++ src/themes/icmaa-imp/components/core/blocks/MyAccount/MyOrders.vue
@@ -7,22 +7,26 @@
     <div class="t-text-sm">
       <div class="" v-show="!isHistoryEmpty">
         <template v-for="order in ordersHistory">
-          <div :order="order" :key="order.entity_id" class="t-flex t-flex-wrap t-items-center t-cursor-pointer t-rounded-sm t-bg-base-lightest t-mb-4 t-px-2 t-py-3" @click="redirect(order.entity_id)">
-            <div class="t-flex t-flex-wrap ">
+          <div :order="order" :key="order.entity_id" class="t-flex t-items-center t-cursor-pointer t-rounded-sm t-bg-base-lightest t-mb-4 t-px-2 t-py-3" @click="redirect(order.entity_id)">
+            <div class="t-flex t-flex-wrap t-w-3/5">
               <div class="t-w-full t-flex t-justify-between">
-                <div>{{ $t('Order #') }}: {{ order.increment_id }}</div>
-                <strong><div>{{ order.grand_total | price }}</div></strong>
+                <div>
+                  <strong>{{ $t('Order #') }}</strong>{{ order.increment_id }}
+                </div>
               </div>
               <div class="t-w-full">
-                {{ $t('Order date') }}: {{ order.created_at | date }}
+                <strong>{{ $t('Order date') }}</strong> {{ order.created_at | date }}
+              </div>
+              <div class="t-w-full t-flex t-items-center">
+                <strong>Status</strong> <status-icon :status="order.status" /><span class="status">&nbsp;{{ order.status | capitalize }}</span>
               </div>
             </div>
-            <div class="t-flex t-flex-wrap ">
-              <div class="t-w-full sm:t-w-1/2 t-p-2 t-flex t-items-center">
-                <status-icon :status="order.status" /><span v-show="viewport !== 'xs'" class="status">&nbsp;{{ order.status | capitalize }}</span>
-              </div>
+
+            <div class="t-flex t-flex-wrap t-flex-grow">
+              <strong><div>{{ order.grand_total | price }}</div></strong>
             </div>
-            <div class="t-w-1/5 t-flex t-justify-center">
+
+            <div class="t-flex t-justify-end">
               <material-icon icon="chevron_right" size="md" class="t-align-middle" />
             </div>
           </div>
