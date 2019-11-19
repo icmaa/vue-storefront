313e5ea6d3b5cadbec7f8dc220ebb4504beeb9b2
schill <s.chill@impericon.com>
2019-11-19 12:29:30 +0100

    Styling changes

--- src/themes/icmaa-imp/components/core/blocks/MyAccount/MyOrders.vue
+++ src/themes/icmaa-imp/components/core/blocks/MyAccount/MyOrders.vue
@@ -8,18 +8,19 @@
       <div class="" v-show="!isHistoryEmpty">
         <template v-for="order in ordersHistory">
           <div :order="order" :key="order.entity_id" class="t-flex t-flex-wrap t-items-center t-cursor-pointer t-rounded-sm t-bg-base-lightest t-mb-4 t-px-2 t-py-3" @click="redirect(order.entity_id)">
-            <div class="t-flex t-flex-wrap t-w-2/5">
-              <div v-show="viewport !== 'xs'" class="t-w-full sm:t-w-1/2 t-p-2">#{{ order.increment_id }}</div>
-              <div class="t-w-full sm:t-w-1/2 t-p-2">
-                {{ order.created_at | date }}
+            <div class="t-flex t-flex-wrap ">
+              <div class="t-w-full t-flex t-justify-between">
+                <div>{{ $t('Order #') }}: {{ order.increment_id }}</div>
+                <strong><div>{{ order.grand_total | price }}</div></strong>
+              </div>
+              <div class="t-w-full">
+                {{ $t('Order date') }}: {{ order.created_at | date }}
               </div>
             </div>
-            <div class="t-flex t-flex-wrap t-w-2/5">
-              <div class="t-w-full sm:t-w-1/2 t-p-2">
-                {{ order.grand_total | price }}
+            <div class="t-flex t-flex-wrap ">
+              <div class="t-w-full sm:t-w-1/2 t-p-2 t-flex t-items-center">
+                <status-icon :status="order.status" /><span v-show="viewport !== 'xs'" class="status">&nbsp;{{ order.status | capitalize }}</span>
               </div>
-
-              <div class="t-w-full sm:t-w-1/2 t-p-2 t-flex t-items-center"><status-icon :status="order.status" /><span v-show="viewport !== 'xs'" class="status" />&nbsp;{{ order.status | capitalize }}</div>
             </div>
             <div class="t-w-1/5 t-flex t-justify-center">
               <material-icon icon="chevron_right" size="md" class="t-align-middle" />
@@ -55,7 +56,7 @@ export default {
     ...mapGetters({ viewport: 'ui/getViewport' })
   },
   methods: {
-    redirect(orderId) {
+    redirect (orderId) {
       this.$router.push(this.localizedRoute(`/my-account/orders/${orderId}`))
     }
   }
