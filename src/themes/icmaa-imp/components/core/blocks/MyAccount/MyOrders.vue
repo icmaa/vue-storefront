804f9b0cd70445ce18cc982d9a83d2c7654d94ec
schill <s.chill@impericon.com>
2019-11-25 12:32:56 +0100

    Styling

--- src/themes/icmaa-imp/components/core/blocks/MyAccount/MyOrders.vue
+++ src/themes/icmaa-imp/components/core/blocks/MyAccount/MyOrders.vue
@@ -8,25 +8,25 @@
       <div class="" v-show="!isHistoryEmpty">
         <template v-for="order in ordersHistory">
           <div :order="order" :key="order.entity_id" class="t-flex t-items-center t-cursor-pointer t-rounded-sm t-bg-base-lightest t-mb-4 t-px-2 t-py-3" @click="redirect(order.entity_id)">
-            <div class="t-flex t-flex-wrap t-w-3/5">
-              <div class="t-w-full t-flex t-justify-between">
+            <div class="t-flex t-w-3/5">
+              <div class="t-w-full t-flex t-flex-wrap">
                 <div>
-                  <strong>{{ $t('Order #') }}</strong>{{ order.increment_id }}
+                  <span class="t-font-bold">{{ $t('Order #') }}</span>{{ order.increment_id }}
+                </div>
+                <div class="t-w-full">
+                  <span class="t-font-bold">{{ $t('Order date') }}</span>{{ order.created_at | date }}
                 </div>
               </div>
-              <div class="t-w-full">
-                <strong>{{ $t('Order date') }}</strong> {{ order.created_at | date }}
-              </div>
-              <div class="t-w-full t-flex t-items-center">
-                <strong>Status</strong> <status-icon :status="order.status" /><span class="status">&nbsp;{{ order.status | capitalize }}</span>
-              </div>
             </div>
 
-            <div class="t-flex t-flex-wrap t-flex-grow">
-              <strong><div>{{ order.grand_total | price }}</div></strong>
+            <div class="t-flex t-w-2/5 t-flex-wrap">
+              <div class="t-font-bold">Warenwert</div>
+              <div>{{ order.grand_total | price }}</div>
+              <div class="t-font-bold t-w-full">Status</div>
+              <status-icon :status="order.status" /><span class="status">&nbsp;{{ order.status | capitalize }}</span>
             </div>
 
-            <div class="t-flex t-justify-end">
+            <div class="t-flex t-w-1/5 t-justify-end">
               <material-icon icon="chevron_right" size="md" class="t-align-middle" />
             </div>
           </div>
@@ -60,7 +60,7 @@ export default {
     ...mapGetters({ viewport: 'ui/getViewport' })
   },
   methods: {
-    redirect (orderId) {
+    redirect(orderId) {
       this.$router.push(this.localizedRoute(`/my-account/orders/${orderId}`))
     }
   }
