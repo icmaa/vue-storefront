10e84935a7789b55c507ff2c206302b4cccbb77a
schill <s.chill@impericon.com>
2019-11-26 14:14:09 +0100

    Removed unnecessary div elements

--- src/themes/icmaa-imp/components/core/blocks/MyAccount/MyOrders.vue
+++ src/themes/icmaa-imp/components/core/blocks/MyAccount/MyOrders.vue
@@ -3,55 +3,51 @@
     <headline icon="local_mall">
       {{ $t('My orders') }}
     </headline>
+
     <!-- My orders body -->
-    <div class="t-text-sm">
-      <div class="" v-show="!isHistoryEmpty">
-        <template v-for="order in ordersHistory">
-          <div :order="order" :key="order.entity_id" class="t-flex  t-items-center t-cursor-pointer  t-border-t t-mb-2 t-px-2 t-py-3" @click="redirect(order.entity_id)">
-            <div class="t-flex t-w-3/5 t-mr-2">
-              <div class="t-w-full t-flex t-flex-wrap">
-                <div class="t-mb-4">
-                  <div class="t-font-bold">
-                    {{ $t('Order number') }}
-                  </div>
-                  <div>#{{ order.increment_id }}</div>
-                </div>
-                <div class="t-w-full">
-                  <div class="t-font-bold">
-                    {{ $t('Order date') }}
-                  </div>
-                  <div>{{ order.created_at | date }}</div>
-                </div>
+    <div class="t-text-sm" v-show="!isHistoryEmpty">
+      <template v-for="order in ordersHistory">
+        <div :order="order" :key="order.entity_id" class="t-flex t-flex-wrap t-items-center t-justify-between t-cursor-pointer t-border-t t-mb-2 t-px-2 t-py-3" @click="redirect(order.entity_id)">
+          <div class="t-mr-2">
+            <div class="t-mb-4">
+              <div class="t-font-bold">
+                {{ $t('Order number') }}
               </div>
+              <div>#{{ order.increment_id }}</div>
             </div>
-
-            <div class="t-flex t-w-2/5 t-flex-wrap t-mr-2">
-              <div class="t-mb-4">
-                <div class="t-font-bold">
-                  {{ $t('Grand total') }}
-                </div>
-                <div>{{ order.grand_total | price }}</div>
+            <div class="t-mb-4">
+              <div class="t-font-bold">
+                {{ $t('Order date') }}
               </div>
-              <div class="t-w-full t-font-bold">
-                Status
-              </div>
-              <div class="t-w-full t-flex t-items-center">
-                <status-icon :status="order.status" /><span class="status">&nbsp;{{ order.status | capitalize }}</span>
-              </div>
-            </div>
-
-            <div class="t-flex t-w-1/5 t-justify-end">
-              <material-icon icon="chevron_right" size="md" class="t-align-middle" />
+              <div>{{ order.created_at | date }}</div>
             </div>
           </div>
-        </template>
-      </div>
-      <div class="" v-show="isHistoryEmpty">
-        <p>{{ $t('No orders yet') }}</p>
-      </div>
-      <!-- Custom design -->
-      <div />
-      <div />
+
+          <div class="t-mr-2">
+            <div class="t-mb-4">
+              <div class="t-font-bold">
+                {{ $t('Grand total') }}
+              </div>
+              <div>{{ order.grand_total | price }}</div>
+            </div>
+            <div class="t-mb-4">
+              <div class="t-font-bold">
+                Status
+              </div>
+              <div class="t-flex t-items-center">
+                <status-icon :status="order.status" /><span> {{ order.status | capitalize }}</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="t-flex t-justify-end">
+            <material-icon icon="chevron_right" size="md" class="t-align-middle" />
+          </div>
+        </div>
+      </template>
+    </div>
+    <div class="t-text-sm" v-show="isHistoryEmpty">
+      <span class="t-py-2">{{ $t('No orders yet') }}</span>
     </div>
   </div>
 </template>
