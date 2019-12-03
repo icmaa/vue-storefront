23d6434f80decaae7f9fccdbb3770c016d769fbf
schill <s.chill@impericon.com>
2019-12-03 16:03:36 +0100

    load & show product alerts

--- src/themes/icmaa-imp/components/core/blocks/MyAccount/MyProductAlerts.vue
+++ src/themes/icmaa-imp/components/core/blocks/MyAccount/MyProductAlerts.vue
@@ -3,18 +3,48 @@
     <headline icon="alarm_on">
       {{ $t('My product-alerts') }}
     </headline>
+    <div>
+      <template v-if="products.length > 0">
+        <product v-for="(product, i) in products" :key="product.childId" :product="product.product[0]" :product-id="product.childId" :class="{ 't-border-b': stockItems.length !== i + 1 }" />
+      </template>
+      <template v-else>
+        {{ $t('You have no product alerts') }}
+      </template>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 import Headline from 'theme/components/core/blocks/MyAccount/Headline'
 import i18n from '@vue-storefront/i18n'
+import Product from 'theme/components/core/blocks/MyAccount/Product'
 
 export default {
   name: 'MyProductAlerts',
   components: {
-    Headline
+    Headline,
+    Product
+  },
+  data () {
+    return {
+      productIds: []
+    }
+  },
+  computed: {
+    ...mapGetters({
+      products: 'icmaaProductAlert/getProducts',
+      stockItems: 'icmaaProductAlert/getStockItems'
+    })
+  },
+  async mounted () {
+    this.productIds = await this.$store.dispatch('icmaaProductAlert/fetchProductStockAlerts')
+
+    this.productIds.forEach(async element => {
+      await this.$store.dispatch('icmaaProductAlert/fetchProductsByProductId', {
+        productId: element
+      })
+    })
   }
 }
 </script>
