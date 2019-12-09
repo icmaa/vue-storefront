1e0fe4533fb349c067bbfef4891b82024f4dbdaa
cewald <c.ewald@impericon.com>
2019-12-09 12:55:12 +0000

    Design improvements [WIP]

--- src/themes/icmaa-imp/components/core/ProductListingTicket.vue
+++ src/themes/icmaa-imp/components/core/ProductListingTicket.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="product-listing t-px-3 lg:t-px-4 lg:t--mx-2">
+  <div class="product-listing t-px-4 t-mb-8">
     <ul>
       <template v-for="(product, i) in products">
-        <product-tile-ticket :product="product" :key="`product-${i}`" />
-        <list-banner v-if="i === 7 || i%28 === 27" :key="`listbanner-${i}`" class="t-w-full t-px-1 lg:t-px-2 t-mb-8" />
+        <product-tile-ticket :product="product" :key="`product-${i}`" class="t-mb-1" />
+        <list-banner v-if="i === 7 || i%28 === 27" :key="`listbanner-${i}`" class="t-w-full t-mb-1" />
       </template>
     </ul>
   </div>
--- src/themes/icmaa-imp/components/core/ProductTileTicket.vue
+++ src/themes/icmaa-imp/components/core/ProductTileTicket.vue
@@ -1,30 +1,30 @@
 <template>
-  <router-link :to="productLink" tag="li" class="t-w-full t-my-5 t-py-5 t-border-b t-border-t-0 t-border-l-0 t-border-r-0 t-border-black t-clearfix t-cursor-pointer">
-    <div class="t-w-full md:t-w-5/12 t-float-left md:t-pl-5 t-mb-5 md:t-mb-5 t-text-primary">
+  <router-link :to="productLink" tag="li" class="t-flex t-flex-wrap t-w-full t-bg-white t-py-4 t-cursor-pointer">
+    <div class="t-w-full md:t-w-5/12 md:t-pl-5 t-mb-5 md:t-mb-5 t-text-primary">
       {{ translatedProductName | htmlDecode }} <span class="t-float-right t-clearfix">{{ ticketStockStatus }}</span>
     </div>
-    <div class="t-w-6/12  md:t-w-3/12 t-float-left md:t-pl-5">
+    <div class="t-w-6/12  md:t-w-3/12 md:t-pl-5">
       {{ product.ticket_city }}<br><span class="t-uppercase t-text-gray-500">{{ product.ticket_venue }}</span>
     </div>
-    <div class="t-w-3/12 md:t-w-2/12 t-float-left t-pl-5">
+    <div class="t-w-3/12 md:t-w-2/12 t-pl-5">
       {{ ticketEventdate }}<br>{{ product.ticket_start }}
     </div>
-    <div class="t-w-2/12 md:t-w-1/12 t-float-left md:t-pr-5">
+    <div class="t-w-2/12 md:t-w-1/12 md:t-pr-5">
       <p>
         <span class="price-original t-text-base-light t-line-through t-mr-2" v-if="product.special_price && parseFloat(product.original_price_incl_tax) > 0">
-          {{ product.original_price_incl_tax | price }}
+          {{ price(product.original_price_incl_tax) }}
         </span>
         <span class="price-special t-text-sale t-font-bold" v-if="product.special_price && parseFloat(product.special_price) > 0">
           <span v-if="hasMultiplePrices" v-text="$t('as low as')" />
-          {{ product.price_incl_tax | price }}
+          {{ price(product.price_incl_tax) }}
         </span>
         <span class="price t-text-base-dark t-font-bold" v-if="!product.special_price && parseFloat(product.price_incl_tax) > 0">
           <span v-if="hasMultiplePrices" v-text="$t('as low as')" />
-          {{ product.price_incl_tax | price }}
+          {{ price(product.price_incl_tax) }}
         </span>
       </p>
     </div>
-    <div class="t-w-1/12 md:t-w-1/12 t-float-right t-pr-5 t-text-right">
+    <div class="t-w-1/12 md:t-w-1/12 t-pr-5 t-self-center t-text-right">
       <button type="button" class="t-items-center t-rounded-sm t-cursor-pointer t-webkit-tap-transparent disabled:t-opacity-75 t-relative t-uppercase t-h-10 t-px-2 md:t-px-4 t-text-xs t-bg-primary t-text-white t-justify-center">
         <i class="t-flex t-flex-fix material-icons t-text-2xl">keyboard_arrow_right</i>
       </button>
@@ -35,7 +35,6 @@
 <script>
 import config from 'config'
 import rootStore from '@vue-storefront/core/store'
-import PromoBanner from 'theme/components/core/blocks/Category/PromoBanner'
 import ProductNameMixin from 'icmaa-catalog/mixins/ProductNameMixin'
 import ProductPriceMixin from 'theme/mixins/product/priceMixin'
 import { ProductTile } from '@vue-storefront/core/modules/catalog/components/ProductTile'
@@ -43,9 +42,6 @@ import { toDate } from 'icmaa-config/helpers/datetime'
 
 export default {
   mixins: [ProductTile, ProductNameMixin, ProductPriceMixin],
-  components: {
-    PromoBanner
-  },
   props: {
     labelsActive: {
       type: Boolean,
