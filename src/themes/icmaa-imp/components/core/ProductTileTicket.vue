81be47a32befb12089fb0283bf556331126743d3
cewald <c.ewald@impericon.com>
2019-12-09 16:24:58 +0000

    Small css class improvements (and use button-component)

--- src/themes/icmaa-imp/components/core/ProductTileTicket.vue
+++ src/themes/icmaa-imp/components/core/ProductTileTicket.vue
@@ -1,18 +1,19 @@
 <template>
-  <router-link :to="productLink" tag="li" class="t-flex t-flex-wrap t-w-full t-bg-white t-py-4 t-cursor-pointer">
-    <div class="t-w-full md:t-w-5/12 t-px-5 t-mb-3 md:t-mb-3 t-text-primary">
+  <router-link :to="productLink" tag="li" class="t-flex t-flex-wrap t-px-2 t-bg-white t-py-4 t-cursor-pointer">
+    <div class="t-w-full md:t-w-5/12 t-px-2 t-mb-3 md:t-mb-3 t-flex-grow t-text-primary">
       {{ translatedProductName | htmlDecode }} <span class="t-float-right t-clearfix">{{ ticketStockStatus }}</span>
     </div>
-    <div class="t-w-full   md:t-w-3/12 t-pl-5">
-      {{ product.ticket_city }}<br><span class="t-uppercase t-text-gray-500">{{ product.ticket_venue }}</span>
+    <div class="t-w-full md:t-w-3/12 t-px-2">
+      <div class="" v-text="product.ticket_city" />
+      <div class="t-uppercase t-text-base-light" v-text="product.ticket_venue" />
     </div>
-    <div class="t-w-full  md:t-w-2/12 t-pl-5">
+    <div class="t-w-full  md:t-w-2/12 t-px-2">
       {{ ticketEventdate }}
       <div class="t-inline md:t-block">
         {{ product.ticket_start }}
       </div>
     </div>
-    <div class="t-w-10/12 md:t-w-1/12 t-pr-5 t-text-right">
+    <div class="t-w-10/12 md:t-w-1/12 t-px-2 t-text-right">
       <p>
         <span class="price-original t-text-base-light t-line-through t-mr-2" v-if="product.special_price && parseFloat(product.original_price_incl_tax) > 0">
           {{ price(product.original_price_incl_tax) }}
@@ -27,10 +28,10 @@
         </span>
       </p>
     </div>
-    <div class="t-w-2/12 md:t-w-1/12 t-pr-5 t-self-center t-text-right">
-      <button type="button" class="t-items-center t-rounded-sm t-cursor-pointer t-webkit-tap-transparent disabled:t-opacity-75 t-relative t-uppercase t-h-10 t-px-2 md:t-px-4 t-text-xs t-bg-primary t-text-white t-justify-center">
-        <i class="t-flex t-flex-fix material-icons t-text-2xl">keyboard_arrow_right</i>
-      </button>
+    <div class="t-w-2/12 md:t-w-1/12 t-px-2 t-flex-1 t-self-center">
+      <button-component type="transparent" icon="keyboard_arrow_right" :icon-only="true">
+        {{ $t('Show product') }}
+      </button-component>
     </div>
   </router-link>
 </template>
@@ -38,6 +39,7 @@
 <script>
 import config from 'config'
 import rootStore from '@vue-storefront/core/store'
+import ButtonComponent from 'theme/components/core/blocks/Button'
 import ProductNameMixin from 'icmaa-catalog/mixins/ProductNameMixin'
 import ProductPriceMixin from 'theme/mixins/product/priceMixin'
 import { ProductTile } from '@vue-storefront/core/modules/catalog/components/ProductTile'
@@ -45,6 +47,9 @@ import { toDate } from 'icmaa-config/helpers/datetime'
 
 export default {
   mixins: [ProductTile, ProductNameMixin, ProductPriceMixin],
+  components: {
+    ButtonComponent
+  },
   props: {
     labelsActive: {
       type: Boolean,
