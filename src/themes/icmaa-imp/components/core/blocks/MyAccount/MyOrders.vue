9460bfc1a1879c335bc54e8a3d30568ccf0083b3
cewald <c.ewald@impericon.com>
2019-12-11 14:41:18 +0000

    Improve styling of list

--- src/themes/icmaa-imp/components/core/blocks/MyAccount/MyOrders.vue
+++ src/themes/icmaa-imp/components/core/blocks/MyAccount/MyOrders.vue
@@ -1,53 +1,47 @@
 <template>
-  <div class="t-p-4 t-bg-white">
-    <headline icon="local_mall">
-      {{ $t('My orders') }}
-    </headline>
-
-    <!-- My orders body -->
-    <div class="t-text-sm" v-show="!isHistoryEmpty">
-      <template v-for="order in ordersHistory">
-        <div :order="order" :key="order.entity_id" class="t-flex t-items-center t-justify-between t-cursor-pointer t-border-t t-mb-2 t-px-2 t-py-3" @click="redirect(order.entity_id)">
-          <!-- order date -->
-          <div class="t-flex t-flex-grow t-flex-wrap t-mr-8">
-            <div class="t-w-full t-flex t-flex-wrap t-items-center">
-              <div class="t-w-1/2 t-mb-4 t-mr-4">
-                <div>{{ order.created_at | date }}</div>
-              </div>
-              <!-- total -->
-              <div class="t-text-2xl t-mb-4">
-                <div>{{ order.grand_total | price }}</div>
-              </div>
-            </div>
-
-            <div class="t-w-full t-flex t-flex-wrap t-items-center">
-              <div class="t-w-1/2 t-mb-4 t-mr-4">
-                <div class="t-font-bold">
-                  {{ $t('Order number') }}
-                </div>
-                <div>#{{ order.increment_id }}</div>
-              </div>
-
-              <div class="t-mb-4">
-                <div class="t-font-bold">
-                  Status
-                </div>
-                <div class="t-flex t-items-center">
-                  <status-icon :status="order.status" /><span> {{ order.status | capitalize }}</span>
-                </div>
-              </div>
-            </div>
+  <div>
+    <div class="t-p-4 t-bg-white">
+      <headline icon="local_mall">
+        {{ $t('My orders') }}
+      </headline>
+      <p v-if="!isHistoryEmpty" class="t-text-sm">
+        {{ $t('These are your recent orders. Click for more details and options.') }}
+      </p>
+      <p v-else class="t-text-sm">
+        {{ $t('Sorry, but you don\'t have any orders yet.') }}
+      </p>
+    </div>
+    <template v-if="!isHistoryEmpty">
+      <router-link tag="div" v-for="order in ordersHistory" :key="order.entity_id" class="t-flex t-items-center t-p-4 t-mt-4 t-bg-white t-text-sm t-cursor-pointer" :to="localizedRoute(`/my-account/orders/${order.id}`)">
+        <div class="t-flex t-flex-grow t-flex-wrap t-items-center t-justify-between">
+          <div class="t-w-1/2 t-mb-2">
+            {{ order.created_at | date }}
           </div>
-
-          <div class="">
-            <material-icon icon="chevron_right" size="md" class="t-align-middle" />
+          <div class="t-w-1/2 t-text-2xl t-mb-2">
+            {{ order.grand_total | price }}
+          </div>
+          <div class="t-w-1/2">
+            <div class="t-font-bold">
+              {{ $t('Order number') }}
+            </div>
+            <div>#{{ order.increment_id }}</div>
+          </div>
+          <div class="t-w-1/2">
+            <div class="t-font-bold">
+              {{ $t('Status') }}
+            </div>
+            <div class="t-flex t-items-center">
+              <status-icon :status="order.status">
+                {{ $t(`orderStatus_${order.status}`) }}
+              </status-icon>
+            </div>
           </div>
         </div>
-      </template>
-    </div>
-    <div class="t-text-sm" v-show="isHistoryEmpty">
-      <span class="t-py-2">{{ $t('No orders yet') }}</span>
-    </div>
+        <router-link :to="localizedRoute(`/my-account/orders/${order.id}`)" class="t-hidden sm:t-block t-flex-fix">
+          <material-icon icon="chevron_right" size="lg" class="t-align-middle" />
+        </router-link>
+      </router-link>
+    </template>
   </div>
 </template>
 
@@ -56,7 +50,7 @@ import { mapGetters } from 'vuex'
 import Headline from 'theme/components/core/blocks/MyAccount/Headline'
 import UserOrder from '@vue-storefront/core/modules/order/components/UserOrdersHistory'
 import MaterialIcon from 'theme/components/core/blocks/MaterialIcon'
-import StatusIcon from 'theme/components/core/blocks/MyAccount/StatusIcon.vue'
+import StatusIcon from 'theme/components/core/blocks/MyAccount/MyOrders/StatusIcon'
 
 export default {
   mixins: [UserOrder],
@@ -64,11 +58,6 @@ export default {
     Headline,
     MaterialIcon,
     StatusIcon
-  },
-  methods: {
-    redirect (orderId) {
-      this.$router.push(this.localizedRoute(`/my-account/orders/${orderId}`))
-    }
   }
 }
 </script>
---
+++ src/themes/icmaa-imp/components/core/blocks/MyAccount/MyOrders/StatusIcon.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="t-flex t-items-center" :class="status">
+    <material-icon icon="lens" size="sm" class="t-mr-1" />
+    <slot />
+  </div>
+</template>
+
+<script>
+import MaterialIcon from 'theme/components/core/blocks/MaterialIcon'
+
+export default {
+  name: 'StatusIcon',
+  components: {
+    MaterialIcon
+  },
+  props: {
+    status: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+.canceled {
+  color: gray;
+}
+
+.pending {
+  color: #b20a0e;
+}
+
+.processing {
+  color: #f7c603;
+}
+
+.complete {
+  color: #297825;
+}
+
+.preorder {
+  background-color: #006ea1;
+}
+</style>
