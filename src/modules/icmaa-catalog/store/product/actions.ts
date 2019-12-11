a84cf559eb461948cf00da93df0eaf294fc6d67f
cewald <c.ewald@impericon.com>
2019-12-11 11:15:49 +0000

    Load parent categories for product by priority of path ids

--- src/modules/icmaa-catalog/store/product/actions.ts
+++ src/modules/icmaa-catalog/store/product/actions.ts
@@ -10,14 +10,26 @@ const actions: ActionTree<ProductState, RootState> = {
    * Clone of originial `product/loadProductBreadcrumbs`
    *
    * Changes:
-   * * Add category whitelist support to hide unimportant categories
+   * * Load only parent categories containing `icmaa.breadcrumbs.path` items in path
+   * * This way we can prior categories of products to be used as parent category
    */
   async loadProductBreadcrumbs ({ dispatch, rootGetters }, { product } = {}) {
     if (product && product.category_ids) {
       let currentCategory = rootGetters['category-next/getCurrentCategory'] // use current category, if set
       if (!currentCategory || !currentCategory.id || !product.category_ids.includes(currentCategory.id.toString())) {
-        const categoryFilters = Object.assign({ 'id': [...product.category_ids] }, cloneDeep(config.entities.category.breadcrumbFilterFields))
-        const categories = await dispatch('category-next/loadCategories', {filters: categoryFilters}, { root: true })
+        const { path } = icmaa.breadcrumbs
+        const categoryFilters = Object.assign(
+          { 'id': [...product.category_ids], path },
+          cloneDeep(config.entities.category.breadcrumbFilterFields)
+        )
+
+        const onlyActive = false
+        const categories = await dispatch(
+          'category-next/loadCategories',
+          { filters: categoryFilters, onlyActive },
+          { root: true }
+        )
+
         currentCategory = categories.sort((a, b) => (a.level > b.level) ? -1 : 1)[0] // sort starting by deepest level
       }
       await dispatch('category-next/loadCategoryBreadcrumbs', { category: currentCategory, currentRouteName: product.name }, { root: true })
