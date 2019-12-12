ff521f17b703d4c78ee5e1436df45ed629dd3c0d
cewald <c.ewald@impericon.com>
2019-12-12 07:08:36 +0000

    Improve order details

--- src/themes/icmaa-imp/components/core/blocks/MyAccount/MyOrder.vue
+++ src/themes/icmaa-imp/components/core/blocks/MyAccount/MyOrder.vue
@@ -19,7 +19,7 @@
           <status-icon :status="order.status" />
         </div>
         <div class="t-w-full lg:t-w-2/4 t-flex t-flex-wrap t-items-center t-justify-between lg:t-justify-end">
-          <router-link :to="localizedRoute(service)" class="t-w-full t-mb-2 lg:t-w-auto lg:t-mb-0 lg:t-mr-4 t-font-light t-text-normal">
+          <router-link :to="localizedRoute('/service')" class="t-w-full t-mb-2 lg:t-w-auto lg:t-mb-0 lg:t-mr-4 t-font-light t-text-normal">
             {{ $t('Are there any questions left?') }}
           </router-link>
           <button-component type="ghost" @click="$router.push(localizedRoute(`/my-account/order-review/${order.id}`))">
@@ -37,7 +37,7 @@
     </div>
     <div class="t-p-4 t-bg-white t-mb-4">
       <div class="t-flex t-flex-wrap t--mx-2 t-text-sm t-text-base-tone">
-        <div class="t-w-full sm:t-w-1/2 t-order-1 sm:t-order-0 t-px-2 t-mb-4">
+        <div class="t-w-full sm:t-w-1/2 t-order-1 sm:t-order-none t-px-2 t-mb-4">
           <div class="t-font-bold t-mb-1 t-text-base-lighter t-text-xxs t-uppercase">
             {{ $t('Billing address') }}
           </div>
@@ -46,7 +46,7 @@
           <p>{{ billingAddress.postcode }} {{ billingAddress.city }}</p>
           <p>{{ billingAddress.country }}</p>
         </div>
-        <div class="t-w-full sm:t-w-1/2 t-order-3 sm:t-order-0 t-px-2 t-mb-4">
+        <div class="t-w-full sm:t-w-1/2 t-order-3 sm:t-order-none t-px-2 t-mb-4">
           <div class="t-font-bold t-mb-1 t-text-base-lighter t-text-xxs t-uppercase">
             {{ $t('Shipping address') }}
           </div>
@@ -55,13 +55,13 @@
           <p>{{ shippingAddress.postcode }} {{ shippingAddress.city }}</p>
           <p>{{ shippingAddress.country }}</p>
         </div>
-        <div class="t-w-full sm:t-w-1/2 t-order-2 sm:t-order-0 t-px-2 t-mb-4 sm:t-mb-0">
+        <div class="t-w-full sm:t-w-1/2 t-order-2 sm:t-order-none t-px-2 t-mb-4 sm:t-mb-0">
           <div class="t-font-bold t-mb-1 t-text-base-lighter t-text-xxs t-uppercase">
             {{ $t('Payment method') }}
           </div>
           <p>{{ paymentMethod }}</p>
         </div>
-        <div class="t-w-full sm:t-w-1/2 t-order-4 sm:t-order-0 t-px-2">
+        <div class="t-w-full sm:t-w-1/2 t-order-4 sm:t-order-none t-px-2">
           <div class="t-font-bold t-mb-1 t-text-base-lighter t-text-xxs t-uppercase">
             {{ $t('Shipping method') }}
           </div>
@@ -70,23 +70,37 @@
       </div>
     </div>
 
-    <!-- product list -->
-    <div class="t-block t-text-2xl t-font-thin t-leading-relaxed t-mb-2">
-      {{ $t('Items ordered') }}
-    </div>
-    <div class="t-w-full t-flex t-mb-2 t-pb-2 t-border-b t-rounded-sm t-border-base-lightest" v-for="item in singleOrderItems" :key="item.item_id">
-      <div class="t-w-1/3 sm:t-w-1/6 t-mr-4">
-        <product-image :image="{ src: getThumbnail(item.thumbnail, 20, 20), loading: getThumbnail(item.thumbnail, 20, 20) }" />
+    <div>
+      <div class="t-text-base-tone t-text-xl t-font-thin t-mb-4 t-pl-4">
+        {{ $t('Items ordered') }}
       </div>
-      <div class="t-w-2/3 t-py-2">
-        <div class="t-block t-text-primary t-w-full t-text-sm t-leading-tight t-mb-2" :data-div="$t('Product Name')">
-          {{ item.qty_ordered }} x {{ item.name }}
+      <div class="t-flex t-items-center t-bg-white t-p-4 t-pb-0">
+        <div class="t-w-8/12 t-font-bold t-text-base-lighter t-text-xxs t-uppercase">
+          {{ $t('Product name') }}
         </div>
-        <button-component v-for="(option, key) in extractProductOptionValues(item.product_options)" :key="key" class="t-mr-2 t-mb-2" type="tag" size="sm" :cursor-pointer="false">
-          {{ option }}
-        </button-component>
-        <div class="t-font-bold" :data-div="$t('Subtotal')">
-          {{ item.row_total_incl_tax | price }}
+        <div class="t-w-2/12 t-text-right t-font-bold t-text-base-lighter t-text-xxs t-uppercase">
+          {{ $t('Qty') }}
+        </div>
+        <div class="t-w-2/12 t-text-right t-font-bold t-text-base-lighter t-text-xxs t-uppercase">
+          {{ $t('Subtotal') }}
+        </div>
+      </div>
+      <div class="t-mb-1 t-bg-white t-p-4 t-text-sm t-text-base-tone" v-for="item in singleOrderItems" :key="item.item_id">
+        <div class="t-flex t-items-end">
+          <div class="t-w-8/12 t-text-sm" :data-div="$t('Product Name')">
+            <div class="t-text-primary">
+              {{ item.name }}
+            </div>
+            <div class="">
+              {{ item.sku }}
+            </div>
+          </div>
+          <div class="t-w-2/12 t-text-right">
+            {{ item.qty_ordered }}
+          </div>
+          <div class="t-w-2/12 t-text-right" :data-div="$t('Subtotal')">
+            {{ item.row_total_incl_tax | price }}
+          </div>
         </div>
       </div>
     </div>
@@ -110,43 +124,24 @@
 
 <script>
 import Vue from 'vue'
-import { mapGetters, mapActions } from 'vuex'
 import Headline from 'theme/components/core/blocks/MyAccount/Headline'
 import MyOrder from '@vue-storefront/core/compatibility/components/blocks/MyAccount/MyOrder'
 import TrackingLink from 'icmaa-tracking/components/TrackingLink'
-import ProductImage from 'theme/components/core/ProductImage'
 import StatusIcon from 'theme/components/core/blocks/MyAccount/MyOrders/StatusIcon'
 import ButtonComponent from 'theme/components/core/blocks/Button'
-import { getThumbnailPath, productThumbnailPath } from '@vue-storefront/core/helpers'
 import { formatProductLink } from '@vue-storefront/core/modules/url/helpers'
 import { price } from 'icmaa-config/helpers/price'
 
 export default {
   mixins: [MyOrder],
   components: {
-    ProductImage,
     Headline,
     StatusIcon,
     TrackingLink,
     ButtonComponent
   },
-  computed: {
-    ...mapGetters('icmaaCmsBlock', ['getJsonBlockByIdentifier']),
-    service () {
-      const metaNavigation = this.getJsonBlockByIdentifier('navigation-meta').find(el => el.name === 'Service') || { route: '/service' }
-      return `${metaNavigation.route}`
-    }
-  },
   methods: {
-    price,
-    extractProductOptionValues (options) {
-      if (options) {
-        const regex = /(?<="value";s:\d+:")[^"]*/
-        const match = regex.exec(options)
-        return match === null ? [] : match
-      }
-      return []
-    }
+    price
   }
 }
 </script>
