c2f25814951bb21ffda951c015881f17ca6dcd7b
cewald <c.ewald@impericon.com>
2019-12-16 07:14:56 +0000

    Improve redirect

--- src/modules/icmaa-url/server.ts
+++ src/modules/icmaa-url/server.ts
@@ -2,21 +2,29 @@ import appConfig from 'config'
 import { serverHooks } from '@vue-storefront/core/server/hooks'
 
 if (appConfig.storeViews.multistore) {
-  serverHooks.afterApplicationInitialized(({ app, config }) => {
-    const blacklist = ['/__webpack_hmr']
+  serverHooks.afterApplicationInitialized(({ app }) => {
+    const blacklist = ['__webpack_hmr']
     const storeCodes = appConfig.storeViews.mapStoreUrlsFor
-    const hasStoreCode = new RegExp(`^(?!\\/(${storeCodes.join('|')})\\/).+((?<=\\.html)|(?<!\\.[a-zA-Z0-9]*))$`)
 
-    app.use(hasStoreCode, (req, res, next) => {
-      const { baseUrl } = req
-      if (!blacklist.includes(baseUrl)) {
-        const newUrl = '/' + storeCodes[0] + baseUrl
-        console.log('Redirect to default store:', newUrl)
-        res.redirect(newUrl)
-        return
+    const blacklistStr = blacklist.join('|')
+    const storeCodesStr = storeCodes.join('|')
+    const hasStoreCode = new RegExp(
+      `^((?!\\/(${storeCodesStr})(\\/|$))(?!\\/(${blacklistStr})$))\\/?.*((?<=\\.html)|(?<!\\.[a-zA-Z0-9]*))$`
+    )
+
+    app.get(hasStoreCode, (req, res) => {
+      const { path } = req
+      const newUrl = '/' + storeCodes[0] + path
+
+      let query = ''
+      if (Object.values(req.query).length > 0) {
+        const params = new URLSearchParams(req.query)
+        query += '?' + params.toString()
       }
 
-      next()
+      res.redirect(newUrl + query)
+
+      console.log('Redirect to default:', newUrl)
     })
   })
 }
