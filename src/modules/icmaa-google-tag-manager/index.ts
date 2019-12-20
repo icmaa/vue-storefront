4635667c1ec4a417fd1ad06776f21df1ed95a14c
cewald <c.ewald@impericon.com>
2019-12-20 11:29:21 +0000

    Add custom event and only load GTM if accepted

--- src/modules/icmaa-google-tag-manager/index.ts
+++ src/modules/icmaa-google-tag-manager/index.ts
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import VueGtm from 'vue-gtm'
 
-import { once, isServer } from '@vue-storefront/core/helpers'
+import EventBus from '@vue-storefront/core/compatibility/plugins/event-bus'
+import { once } from '@vue-storefront/core/helpers'
 import { StorefrontModule } from '@vue-storefront/core/lib/modules'
 import { Logger } from '@vue-storefront/core/lib/logger'
 
@@ -9,21 +10,34 @@ import { icmaaGoogleTagManagerModule } from './store'
 import { afterRegistration, isEnabled } from './hooks/afterRegistration'
 export const KEY = 'icmaa-google-tag-manager'
 
-export const IcmaaGoogleTagManagerModule: StorefrontModule = function ({store, router, appConfig}) {
-  if (isEnabled(appConfig.googleTagManager.id)) {
+const initGTM = async ({ appConfig, router }) => {
+  const { id, debug } = appConfig.googleTagManager
+  const enabled = await isEnabled(id)
+  if (enabled) {
     once('__VUE_EXTEND_GTM__', () => {
-      Vue.use(VueGtm, {
-        enabled: true,
-        id: appConfig.googleTagManager.id,
-        debug: appConfig.googleTagManager.debug,
-        vueRouter: router
+      Vue.use(VueGtm, { enabled, id, debug, vueRouter: router })
+    })
+
+    router.afterEach((to, from) => {
+      const name = to.meta.gtm || to.name
+      let dataLayer = (window['dataLayer'] = window['dataLayer'] || [])
+      dataLayer.push({
+        event: 'icmaa-content-view',
+        'content-name': to.fullPath,
+        'content-view-name': name
       })
     })
   } else {
-    Logger.warn('Google Tag Manager extensions is not working. Ensure Google Tag Manager container ID is defined in config', 'GTM')()
+    Logger.log('Google Tag Manager extensions is not enabled', 'icmaa-gtm')()
   }
+}
+
+export const IcmaaGoogleTagManagerModule: StorefrontModule = async ({store, router, appConfig}) => {
+  initGTM({ appConfig, router })
+  EventBus.$on('cookiesAccepted', () => {
+    initGTM({ appConfig, router })
+  })
 
   store.registerModule(KEY, icmaaGoogleTagManagerModule)
-
   afterRegistration(appConfig, store)
 }
