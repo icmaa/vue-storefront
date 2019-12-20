b60407c50af05235c8ea408449d2119e6926ba06
cewald <c.ewald@impericon.com>
2019-12-20 07:12:08 +0000

    Set cluster on products and category page using mixin

---
+++ src/modules/icmaa-user/mixins/cluster.ts
@@ -0,0 +1,26 @@
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters({
+      categoryExtras: 'icmaaCategoryExtras/getCategoryExtrasByCurrentCategory',
+      category: 'category-next/getCurrentCategory'
+    })
+  },
+  methods: {
+    setCluster () {
+      console.log('SETCLUSTERREERER', this.categoryExtras.cluster)
+      if (this.categoryExtras && this.categoryExtras.cluster) {
+        this.$store.dispatch('user/setCluster', this.categoryExtras.cluster)
+      }
+    }
+  },
+  mounted () {
+    this.setCluster()
+  },
+  watch: {
+    category () {
+      this.setCluster()
+    }
+  }
+}
