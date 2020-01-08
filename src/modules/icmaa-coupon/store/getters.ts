06b80d9774796130fd0773a69255fc6fb442d995
schill <s.chill@impericon.com>
2020-01-08 15:19:03 +0100

    Added currency

--- src/modules/icmaa-coupon/store/getters.ts
+++ src/modules/icmaa-coupon/store/getters.ts
@@ -5,8 +5,9 @@ import CouponState from '../types/CouponState'
 const getters: GetterTree<CouponState, RootState> = {
   getCouponBalance: (state) => state.balance,
   getCouponExpires: (state) => state.expires,
-  getCoupon: (state) => state.coupon,
-  getCouponPin: (state) => state.pin
+  getCoupon: (state) => state.number,
+  getCouponPin: (state) => state.pin,
+  getCouponCurrency: (state) => state.currency
 }
 
 export default getters
