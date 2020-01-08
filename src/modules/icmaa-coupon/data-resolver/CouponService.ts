ae30f73385295977aa5bce30d22e7b557d8ef0ba
schill <s.chill@impericon.com>
2020-01-08 15:18:41 +0100

    Added CouponResult interface

--- src/modules/icmaa-coupon/data-resolver/CouponService.ts
+++ src/modules/icmaa-coupon/data-resolver/CouponService.ts
@@ -1,18 +1,18 @@
 import config from 'config';
 import { processURLAddress } from '@vue-storefront/core/helpers';
 import { TaskQueue } from '@vue-storefront/core/lib/sync';
-import CouponState from '../types/CouponState'
+import { CouponResult } from '../types/CouponState'
 
-const loadCoupon = (coupon: string, pin: string): Promise<CouponState> =>
+const loadCoupon = (number: string, pin: string): Promise<CouponResult> =>
   TaskQueue.execute({
-    url: processURLAddress(config.icmaa_coupon.endpoint) + '/coupon?token={{token}}',
+    url: processURLAddress(config.icmaa_coupon.endpoint) + '/index?token={{token}}',
     payload: {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       mode: 'cors',
-      body: JSON.stringify({ coupon: coupon, pin: pin })
+      body: JSON.stringify({ number: number, pin: pin })
     }
-  }).then(resp => resp.payload);
+  }).then(resp => resp.result);
 
 export default {
   loadCoupon
--- src/modules/icmaa-coupon/types/CouponState.ts
+++ src/modules/icmaa-coupon/types/CouponState.ts
@@ -1,6 +1,23 @@
 export default interface Coupon {
-  coupon: string,
+  number: string,
   pin: string,
   expires: string,
-  balance: number
+  balance: number,
+  currency: string
+}
+
+export interface CouponResult {
+  cert_id: number,
+  cert_number: string,
+  balance: number,
+  currency_code: string,
+  pin: number,
+  status: string,
+  expire_at: string|null,
+  recipient_name: string|null,
+  recipient_email: string|null,
+  recipient_address: string|null,
+  recipient_message: string|null,
+  store_id: number,
+  sender_name: string
 }
