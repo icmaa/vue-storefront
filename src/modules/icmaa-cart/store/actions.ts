617b4574246add6e64ea06c855573a91f59b7e6c
cewald <c.ewald@impericon.com>
2020-01-08 11:28:28 +0000

    Sync totals and cart after coupon is added

---
+++ src/modules/icmaa-cart/store/actions.ts
@@ -0,0 +1,58 @@
+import { ActionTree } from 'vuex'
+import RootState from '@vue-storefront/core/types/RootState'
+import CartState from '@vue-storefront/core/modules/cart/types/CartState'
+import { CartService } from '@vue-storefront/core/data-resolver'
+import { cartHooksExecutors } from '@vue-storefront/core/modules/cart/hooks'
+import * as types from '@vue-storefront/core/modules/cart/store/mutation-types'
+
+const actions: ActionTree<CartState, RootState> = {
+  async removeCoupon ({ getters, dispatch }) {
+    if (getters.canSyncTotals) {
+      const { result } = await CartService.removeCoupon()
+      if (result) {
+        await dispatch('afterCoupon')
+        return result
+      }
+    }
+  },
+  async applyCoupon ({ getters, dispatch }, couponCode) {
+    if (couponCode && getters.canSyncTotals) {
+      const { result } = await CartService.applyCoupon(couponCode)
+      if (result) {
+        await dispatch('afterCoupon')
+      }
+      return result
+    }
+  },
+  /**
+   * We need to update/sync the cart after the coupon is applied to update cart for insentive products.
+   * There is already an up-to-date representation of the cart in `cart/shipping-information` request
+   * of `syncTotals` but this isn't returned so we can't use it without extending the core excessivly.
+   * @param param
+   */
+  async afterCoupon ({ getters, dispatch, commit }) {
+    const { getCartItems, isTotalsSyncRequired } = getters
+    const { result, resultCode } = await CartService.getItems()
+    const { serverItems, clientItems } = cartHooksExecutors.beforeSync({ clientItems: getCartItems, serverItems: result })
+
+    if (resultCode === 200) {
+      const diffLog = await dispatch('merge', {
+        dryRun: false,
+        serverItems,
+        clientItems,
+        forceClientState: false
+      })
+
+      // Force server sync of totals
+      if (!isTotalsSyncRequired && clientItems.length > 0) {
+        await dispatch('syncTotals')
+      }
+
+      cartHooksExecutors.afterSync(diffLog)
+
+      return diffLog
+    }
+  }
+}
+
+export default actions
--- src/modules/icmaa-cart/store/index.ts
+++ src/modules/icmaa-cart/store/index.ts
@@ -1,7 +1,9 @@
 import { Module } from 'vuex'
 import getters from './getters'
+import actions from './actions'
 import CartState from '@vue-storefront/core/modules/cart/types/CartState'
 
 export const IcmaaExtendedCartStore: Module<CartState, any> = {
+  actions,
   getters
 }
