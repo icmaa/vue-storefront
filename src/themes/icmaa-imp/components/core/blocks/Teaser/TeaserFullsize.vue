b639d3713c3045f213ed57049d3a68b37a20aa67
cewald <c.ewald@impericon.com>
2020-01-22 16:53:10 +0000

    Improvements

--- src/themes/icmaa-imp/components/core/blocks/Teaser/TeaserFullsize.vue
+++ src/themes/icmaa-imp/components/core/blocks/Teaser/TeaserFullsize.vue
@@ -2,10 +2,10 @@
   <div id="teaser-large" class="t-mx-0 sm:t-mx-4 t-cursor-pointer t-webkit-tap-transparent" @click="redirect">
     <div class="t-relative">
       <retina-image :alt="teaser.text1 | htmlDecode" :image="imageUrl" :width="1248" :height="730" :placeholder="true" ratio="133:78" class="t-w-full" />
-      <div class="t-flex t-justify-center t-items-center t-absolute t-bottom-0 t-inset-x-0 t-mb-6 t-text-sm t-text-small t-uppercase" :class="{ 't-text-white': !textColor }" :style="{ color: textColor }">
+      <h2 class="t-flex t-justify-center t-items-center t-absolute t-bottom-0 t-inset-x-0 t-mb-6 t-text-sm t-text-small t-uppercase" :class="{ 't-text-white': !textColor }" :style="{ color: textColor }">
         {{ teaser.text1 }}
         <material-icon icon="arrow_forward" size="sm" class="t-ml-2" />
-      </div>
+      </h2>
     </div>
   </div>
 </template>
