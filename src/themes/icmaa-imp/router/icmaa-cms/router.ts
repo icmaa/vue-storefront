10e5266da0f97f35ee5f74c8934be63a8a0ceb7f
schill <s.chill@impericon.com>
2020-01-22 16:59:53 +0100

    Changed route name

--- src/themes/icmaa-imp/router/icmaa-cms/router.ts
+++ src/themes/icmaa-imp/router/icmaa-cms/router.ts
@@ -1,11 +1,11 @@
 // Route components
 const ServiceComponent = () => import(/* webpackChunkName: "vsf-icmaa-cms-page-custom-service" */ 'theme/components/core/blocks/ICMAA/Cms/Pages/Service.vue')
 const ServiceRTEComponent = () => import(/* webpackChunkName: "vsf-icmaa-cms-page-custom-service-rte" */ 'theme/components/core/blocks/ICMAA/Cms/Pages/ServiceRTE.vue')
-const ServiceSizeComponent = () => import(/* webpackChunkName: "vsf-icmaa-cms-page-custom-size" */ 'theme/components/core/blocks/ICMAA/Cms/Pages/ServiceSize.vue')
+const ServiceSizeComponent = () => import(/* webpackChunkName: "vsf-icmaa-cms-page-custom-service-size" */ 'theme/components/core/blocks/ICMAA/Cms/Pages/ServiceSize.vue')
 
 export const routes: any = [
   // Custom cms routes, like /service or /festivals
   { name: 'service', path: '/:identifier', component: ServiceComponent },
   { name: 'service-rte', path: '/:identifier', component: ServiceRTEComponent },
-  { name: 'size', path: '/:identifier', component: ServiceSizeComponent }
+  { name: 'service-size', path: '/:identifier', component: ServiceSizeComponent }
 ]
