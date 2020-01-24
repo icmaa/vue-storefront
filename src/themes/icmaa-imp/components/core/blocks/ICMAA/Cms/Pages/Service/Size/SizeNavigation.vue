6c5632d08394fbbe6b3418efa83b7631e433d30f
schill <s.chill@impericon.com>
2020-01-24 17:01:31 +0100

    Toggle navigation

---
+++ src/themes/icmaa-imp/components/core/blocks/ICMAA/Cms/Pages/Service/Size/SizeNavigation.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="t-flex t-items-center t-justify-between t-flex-wrap t-cursor-pointer t-font-bold" @click="toggleView">
+    <div>{{ headline }}</div>
+    <material-icon :icon="icon" size="lg" class="t-text-base-lighter t-ml-2" />
+  </div>
+</template>
+
+<script>
+import MaterialIcon from 'theme/components/core/blocks/MaterialIcon'
+
+export default {
+  name: 'ServiceSizeNavigation',
+  components: {
+    MaterialIcon
+  },
+  props: {
+    headline: {
+      required: true,
+      type: String
+    }
+  },
+  data () {
+    return {
+      show: false
+    }
+  },
+  computed: {
+    icon () {
+      return this.show ? 'expand_less' : 'expand_more'
+    }
+  },
+  methods: {
+    toggleView () {
+      this.show = !this.show
+      this.$emit('toggle', this.show)
+    }
+  }
+}
+</script>
