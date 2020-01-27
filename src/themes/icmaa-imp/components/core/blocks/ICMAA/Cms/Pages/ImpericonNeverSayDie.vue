67128feafb5a389d35129838d9468338a3867be7
Dierk Landmann <d.landmann@impericon.com>
2020-01-27 10:20:54 +0100

    initial INSD CMS Page

---
+++ src/themes/icmaa-imp/components/core/blocks/ICMAA/Cms/Pages/ImpericonNeverSayDie.vue
@@ -0,0 +1,21 @@
+<template>
+  <layout>
+    <h2 v-html="content.test" />
+  </layout>
+</template>
+
+<script>
+import Page from 'icmaa-cms/components/Page'
+import ButtonComponent from 'theme/components/core/blocks/Button'
+
+export default {
+  name: 'ImpericonNeverSayDie',
+  mixins: [ Page ],
+
+  data () {
+    return {
+      dataType: 'yaml'
+    }
+  }
+}
+</script>
