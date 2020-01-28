513eca842cff36fcc1552638a6979e041baeb11b
schill <s.chill@impericon.com>
2020-01-28 17:10:49 +0100

    Renamed component

--- src/themes/icmaa-imp/router/icmaa-cms/router.ts
+++ src/themes/icmaa-imp/router/icmaa-cms/router.ts
@@ -3,7 +3,7 @@ const ServiceComponent = () => import(/* webpackChunkName: "vsf-icmaa-cms-page-c
 const ServiceRTEComponent = () => import(/* webpackChunkName: "vsf-icmaa-cms-page-custom-service-rte" */ 'theme/components/core/blocks/ICMAA/Cms/Pages/ServiceRTE.vue')
 const TicketsComponent = () => import(/* webpackChunkName: "vsf-icmaa-cms-page-custom-tickets" */ 'theme/pages/Tickets.vue')
 const FestivalComponent = () => import(/* webpackChunkName: "vsf-icmaa-cms-page-custom-festival" */ 'theme/components/core/blocks/ICMAA/Cms/Pages/Festival.vue')
-const AffiliatesComponent = () => import(/* webpackChunkName: "vsf-icmaa-cms-page-custom-affiliates" */ 'theme/components/core/blocks/ICMAA/Cms/Pages/Affiliates.vue')
+const AffiliateComponent = () => import(/* webpackChunkName: "vsf-icmaa-cms-page-custom-affiliate" */ 'theme/components/core/blocks/ICMAA/Cms/Pages/Affiliate.vue')
 
 export const routes: any = [
   // Custom cms routes, like /service or /festivals
@@ -11,5 +11,5 @@ export const routes: any = [
   { name: 'service-rte', path: '/:identifier', component: ServiceRTEComponent },
   { name: 'tickets', path: '/:identifier', component: TicketsComponent },
   { name: 'festival', path: '/:identifier', component: FestivalComponent },
-  { name: 'affiliates', path: '/:identifier', component: AffiliatesComponent }
+  { name: 'affiliate', path: '/:identifier', component: AffiliateComponent }
 ]
