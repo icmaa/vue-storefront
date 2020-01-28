1c4919d5e01a2ffc93be12064563e3185fe20396
Dierk Landmann <d.landmann@impericon.com>
2020-01-28 13:22:39 +0100

    smooth mobile service-navigation

--- src/themes/icmaa-imp/components/core/blocks/ICMAA/Cms/Pages/Service/Layout.vue
+++ src/themes/icmaa-imp/components/core/blocks/ICMAA/Cms/Pages/Service/Layout.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="t-container">
     <div class="t-flex t-flex-wrap t-py-4">
-      <navigation class="t-w-full lg:t-w-2/12 t-px-4" />
+      <navigation class="t-w-full lg:t-w-2/12 t-px-4 t-mb-8" />
       <div class="t-w-full lg:t-w-10/12 t-px-4">
         <h1 v-html="headline" v-if="headline" />
         <slot />
--- src/themes/icmaa-imp/components/core/blocks/ICMAA/Cms/Pages/Service/Navigation.vue
+++ src/themes/icmaa-imp/components/core/blocks/ICMAA/Cms/Pages/Service/Navigation.vue
@@ -1,10 +1,17 @@
 <template>
   <nav role="service-navigation">
     <ul>
-      <li v-for="(item, index) in navigation" :key="index" class="t-border-b t-border-base-lighter">
-        <router-link :to="localizedRoute(item.path)" class="t-flex t-p-2 t-text-sm">
-          {{ item.label }}
-        </router-link>
+      <li class="t-w-full t-relative">
+        <a class="t-block lg:t-hidden t-bg-white t-border-black t-border t-p-2 t-appearance-none focus:outline-none focus:shadow-outline" href="#">Service
+          <div class="t-pointer-events-none t-absolute t-inset-y-0 t-right-0 t-flex t-items-center t-px-2"><i class="material-icons t-text-2xl">keyboard_arrow_down</i></div>
+        </a>
+        <ul class="t-hidden lg:t-block t-bg-white lg:t-bg-transparent t-absolute lg:t-relative t-w-full t-z-10">
+          <li v-for="(item, index) in navigation" :key="index" class="t-border-b t-border-base-lighter">
+            <router-link :to="localizedRoute(item.path)" class="t-flex t-p-2 t-text-sm">
+              {{ item.label }}
+            </router-link>
+          </li>
+        </ul>
       </li>
     </ul>
   </nav>
@@ -26,3 +33,10 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+/* Display the ServiceNav on hover and tap*/
+ul li a:hover + .t-hidden, .t-hidden:hover {
+    display: block;
+}
+</style>
