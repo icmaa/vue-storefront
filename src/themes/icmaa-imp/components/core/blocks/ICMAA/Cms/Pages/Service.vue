57ba9ed0c7cf0088d131dde5b37d72d40f5a689b
cewald <c.ewald@impericon.com>
2020-01-30 18:07:44 +0000

    Style service landing-page

--- src/themes/icmaa-imp/components/core/blocks/ICMAA/Cms/Pages/Service.vue
+++ src/themes/icmaa-imp/components/core/blocks/ICMAA/Cms/Pages/Service.vue
@@ -1,11 +1,10 @@
 <template>
   <layout id="cms-page" :headline="content.headline">
     <h2 v-html="content.quickHelp.headline" />
-    <ul>
+    <ul class="t-list-disc t-ml-5">
       <li v-for="question in content.quickHelp.questions" :key="question.text">
-        <router-link :to="question.link" class="t-flex t-items-stretch t-mb-3 group">
-          <i v-text="question.icon" class="material-icons t-flex-none t-mr-2 t-text-gray-400 group-hover:t-text-primary" />
-          <span v-html="question.text" class="t-flex-auto group-hover:t-text-primary" />
+        <router-link :to="localizedRoute(question.link)" class="t-flex t-mb-3 t-text-base-tone">
+          {{ question.text }}
         </router-link>
       </li>
     </ul>
@@ -13,8 +12,8 @@
     <div class="t-flex t-flex-wrap t--mx-2">
       <div v-for="member in content.serviceTeam.team" :key="member.name" class="t-w-full sm:t-w-1/2 md:t-w-1/3 xl:t-w-1/4 t-px-2 t-pb-4">
         <div class="t-relative">
-          <div v-html="member.name" class="t-absolute t-top-0 t-left-0 t-p-3 sm:t-p-4 t-font-serif t-text-gray-800 t-text-2xl md:t-text-xl" />
-          <img :src="getMediaThumbnail(member.img, 0, 0)" :alt="member.name" class="t-border-gray-200 t-border-4">
+          <div v-html="member.name" class="t-absolute t-top-0 t-left-0 t-p-3 sm:t-p-4 t-font-serif t-text-base-tone t-text-2xl md:t-text-xl" />
+          <img :src="getMediaThumbnail(member.img, 0, 0)" :alt="member.name">
         </div>
       </div>
     </div>
