d7c62b950821936754b190057bb483f0df8e0111
cewald <c.ewald@impericon.com>
2020-02-01 11:17:05 +0000

    Improve `/service-contact` dialog

--- src/themes/icmaa-imp/components/core/blocks/ICMAA/Cms/Pages/ServiceContact.vue
+++ src/themes/icmaa-imp/components/core/blocks/ICMAA/Cms/Pages/ServiceContact.vue
@@ -1,25 +1,28 @@
 <template>
   <layout id="cms-page" :headline="content.headline">
-    <p class="t-mb-4" v-html="content.context" />
-    <div v-for="(s, i) in subjects" :key="i">
-      <div class="t-flex t-justify-between t-cursor-pointer">
-        <div @click="selectedSubject = s.name">
-          {{ s.name }}
-        </div>
-        <material-icon icon="keyboard_arrow_right" v-if="selectedSubject === s.name" @click.native="clearSelection()" />
-      </div>
-      <div v-if="s.children && s.children.length > 0 && selectedSubject === s.name">
-        <div v-for="(c, j) in s.children" :key="j" class="t-flex t-justify-between t-cursor-pointer">
-          <div @click="selectedChild = c.name">
-            {{ c.name }}
+    <p class="t-mb-2 t-font-bold t-text-sm" v-html="content.context" />
+    <div :class="{ 't-mb-6': formVisible }">
+      <div v-for="(s, i) in subjects" :key="i">
+        <div class="t-flex t-items-center t-justify-between t-cursor-pointer t-border-t t-border-base-lightest t-p-2 t-pl-0" :class="{ 't-opacity-50': !isSelectedSubject(s.name) && selectedSubject}" @click="toggleSubject(s)">
+          <material-icon icon="indeterminate_check_box" v-if="isSelectedSubject(s.name)" />
+          <material-icon icon="check_box_outline_blank" v-else class=" t-text-base-lighter" />
+          <div class="t-flex-1 t-ml-1">
+            {{ s.name }}
           </div>
-          <material-icon icon="keyboard_arrow_right" v-if="selectedChild === c.name" @click.native="clearSelection(false)" />
+        </div>
+        <div v-if="s.children && s.children.length > 0 && selectedSubject === s.name">
+          <p class="t-border-base-lightest t-border-t t-py-2 t-font-bold t-text-sm" v-html="content.context_more" />
+          <div v-for="(c, j) in s.children" :key="j" class="t-flex t-items-center t-justify-between t-cursor-pointer t-border-t t-border-base-lightest t-p-2 t-pl-0" :class="{ 't-opacity-50': !isSelectedChildSubject(c.name) && selectedChildSubject}" @click="toggleSubject(c, true)">
+            <material-icon icon="indeterminate_check_box" v-if="isSelectedChildSubject(c.name)" />
+            <material-icon icon="check_box_outline_blank" v-else class="t-text-base-lighter" />
+            <div class="t-flex-1 t-ml-1">
+              {{ c.name }}
+            </div>
+          </div>
         </div>
       </div>
     </div>
-    <template v-if="formVisible">
-      <form-component form-identifier="service-contact" v-model="formData" @submit="sendMail()" />
-    </template>
+    <form-component form-identifier="service-contact" v-model="formData" @submit="sendMail()" v-if="formVisible" />
   </layout>
 </template>
 
@@ -42,30 +45,46 @@ export default {
       formData: { },
       dataType: 'yaml',
       selectedSubject: false,
-      selectedChild: false
+      selectedChildSubject: false
     }
   },
   computed: {
     formVisible () {
-      return (this.selectedSubject && this.selectedChild) || (this.selectedSubject && this.selectedSubject.hasOwnProperty('children'))
+      return ((this.selectedSubject && this.selectedChildSubject) || (this.selectedSubject && !this.selectedSubjectHasChildren))
     },
     subjects () {
-      return this.content.subjects.filter(s => !this.selectedSubject || this.selectedSubject === s.name)
+      return this.content.subjects.filter(s => !this.selectedSubject || !this.selectedSubjectHasChildren || this.selectedSubject === s.name)
     },
     subject () {
-      const sub = this.selectedChild ? ` - ${this.selectedChild}` : ''
-      return `${this.selectedSubject || ''}${this.selectedChild}`
+      const sub = this.selectedChildSubject ? ` - ${this.selectedChildSubject}` : ''
+      return `${this.selectedSubject || ''}${sub}`
+    },
+    selectedSubjectHasChildren () {
+      const subject = this.content.subjects.find(s => s.name === this.selectedSubject)
+      return (subject && subject.hasOwnProperty('children'))
     }
   },
   methods: {
-    clearSelection (all = true) {
-      this.selectedSubject = false
-      if (all) {
-        this.selectedChild = false
+    toggleSubject (subject, isChild = false) {
+      if (!isChild) {
+        this.selectedSubject = !this.selectedSubject || this.selectedSubject !== subject.name ? subject.name : false
+        this.selectedChildSubject = false
+      } else {
+        this.selectedChildSubject = !this.selectedChildSubject || this.selectedChildSubject !== subject.name ? subject.name : false
       }
+    },
+    clearSelection (clearChild = true) {
+      this.selectedSubject = false
+      this.selectedChildSubject = false
     },
     sendMail () {
       return this.$store.dispatch('icmaaForms/mail', { subject: this.subject, data: this.formData })
+    },
+    isSelectedSubject (subject) {
+      return subject === this.selectedSubject
+    },
+    isSelectedChildSubject (subject) {
+      return subject === this.selectedChildSubject
     }
   },
   asyncData ({ store }) {
