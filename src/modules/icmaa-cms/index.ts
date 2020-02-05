9df57c9c8e5ff774c22a396a7423d3c4ea4bc7ef
cewald <c.ewald@impericon.com>
2020-02-05 09:55:54 +0000

    Refactor some code

--- src/modules/icmaa-cms/index.ts
+++ src/modules/icmaa-cms/index.ts
@@ -1,13 +1,16 @@
 import { StorefrontModule } from '@vue-storefront/core/lib/modules'
 import { StorageManager } from '@vue-storefront/core/lib/storage-manager'
+import { setupMultistoreRoutes } from '@vue-storefront/core/lib/multistore'
 
 import { BlockStore, cmsBlockStateKey } from './store/block'
 import { PageStore, cmsPageStateKey } from './store/page'
+import moduleRoutes from './routes'
 
 export const KEY = 'icmaa-cms'
 export const cacheStorage = StorageManager.init(KEY)
 
-export const IcmaaCmsModule: StorefrontModule = function ({ store }) {
+export const IcmaaCmsModule: StorefrontModule = function ({ store, appConfig, router }) {
   store.registerModule(cmsBlockStateKey, BlockStore)
   store.registerModule(cmsPageStateKey, PageStore)
+  setupMultistoreRoutes(appConfig, router, moduleRoutes, 10)
 }
---
+++ src/modules/icmaa-cms/pages/LandingPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="cms-page" class="t-container t-p-4">
+    Hallo welt
+  </div>
+</template>
+
+<script>
+import CmsMetaMixin from 'icmaa-meta/mixins/cmsMeta'
+
+export default {
+  name: 'LandingPage',
+  mixins: [ CmsMetaMixin ],
+  components: {
+
+  },
+  props: {
+
+  }
+}
+</script>
