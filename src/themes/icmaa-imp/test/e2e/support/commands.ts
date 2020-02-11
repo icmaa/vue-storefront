372da196474ccc087310758b5bc2b0d7f9d3ad5e
cewald <c.ewald@impericon.com>
2020-02-11 13:48:46 +0000

    Add `faker` js lib for sample users

--- src/themes/icmaa-imp/test/e2e/support/commands.ts
+++ src/themes/icmaa-imp/test/e2e/support/commands.ts
@@ -24,7 +24,7 @@
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
-import Settings from './utils/settings'
+import Settings from './utils/Settings'
 
 const { _ } = Cypress
 
---
+++ src/themes/icmaa-imp/test/e2e/support/utils/Faker.ts
@@ -0,0 +1,34 @@
+import faker from 'faker'
+import Settings from './Settings'
+
+/**
+ * Example usage:
+ *
+ * import { Faker, getRandomIcmaaEmail } from '../../support/utils/Faker'
+ *
+ * const faker = Faker()
+ * const name = faker.name
+ * const password = faker.internet.password()
+ *
+ * const email = getRandomIcmaaEmail()
+ */
+
+/**
+ * Import faker.js using the current storeview or a specific one
+ * @param country
+ */
+export const Faker = (country?: string) => {
+  faker.locale = country || Settings.currentStoreView
+  return faker
+}
+
+/**
+ * Return a random ICMAA-like email address
+ */
+export const getRandomIcmaaEmail = () => {
+  return [
+    'a9.tests+',
+    new Date().getTime(),
+    '@impericon.com'
+  ].join('')
+}
