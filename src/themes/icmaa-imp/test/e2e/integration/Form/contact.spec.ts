341f4b15d4d628de6da04bbf26d80a096e3709ad
Dierk Landmann <d.landmann@impericon.com>
2020-02-12 17:16:26 +0100

    contact-form test

--- src/themes/icmaa-imp/test/e2e/integration/Form/contact.spec.ts
+++ src/themes/icmaa-imp/test/e2e/integration/Form/contact.spec.ts
@@ -1,5 +1,22 @@
+import { Faker, getRandomIcmaaEmail } from '../../support/utils/Faker'
+
+const faker = Faker()
+const email = getRandomIcmaaEmail()
+
 describe('Contact', () => {
   it('Check Form', () => {
     cy.visitAsRecurringUser('/service-contact')
+    cy.get('div[data-test-id="ServiceContactSelector"]').random().click().then(($selector) => {
+      if ($selector.find('div[data-test-id="ServiceContactChildrenSelector"]').length > 0) {
+        cy.get('div[data-test-id="ServiceContactChildrenSelector"]').random().click()
+      }
+    })
+    cy.get('input#name').click().type(faker.name.findName())
+    cy.get('input#phone').click().type(faker.phone.phoneNumber())
+    cy.get('input#email').click().type(email)
+    cy.get('input#order_number').click().type('123456789')
+    cy.get('textarea#message').click().type(faker.random.words())
+    cy.get('#cms-page').find('button').click()
+    cy.get('[data-test-id="NotificationItem"].t-bg-alt-3')
   })
 })
