fd7f28130969d76813de91596ca5cbdf7c1fa890
cewald <c.ewald@impericon.com>
2020-02-13 10:36:13 +0100

    Add login/logout

--- src/themes/icmaa-imp/test/e2e/integration/Customer/createAccount.spec.ts
+++ src/themes/icmaa-imp/test/e2e/integration/Customer/createAccount.spec.ts
@@ -1,5 +1,30 @@
 describe('Customer', () => {
-  it('creates an account', () => {
+  it('register account, logout and login again', () => {
     cy.registerCustomer()
+
+    cy.getByTestId('HeaderButtonAccount')
+      .as('accountButton')
+
+    cy.get('@accountButton').should('have.class', 'logged-in')
+
+    cy.openNavigationSidebar('[data-test-id="HeaderButtonAccount"]')
+    cy.get('@sidebar').findByTestId('logoutButton').click()
+
+    cy.get('@accountButton')
+      .should('have.class', 'logged-out')
+
+    cy.openNavigationSidebar('[data-test-id="HeaderButtonAccount"]', '[data-test-id="Modal"]')
+
+    cy.getCustomer().then(customer => {
+      cy.get('@sidebar').find('input[name="email"]').type(customer.email)
+      cy.get('@sidebar').find('input[name="password"]').type(customer.password)
+      cy.get('@sidebar').findByTestId('loginSubmit').click()
+    })
+
+    cy.getByTestId('Loader').should('be.visible')
+    cy.getByTestId('NotificationItem').should('be.visible')
+
+    cy.get('@accountButton')
+      .should('have.class', 'logged-in')
   })
 })
