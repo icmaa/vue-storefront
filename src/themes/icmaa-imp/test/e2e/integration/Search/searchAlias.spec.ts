8f46e3b5c66afc580b9b85ebc550db266143f98e
Dierk Landmann <d.landmann@impericon.com>
2020-02-13 16:59:00 +0100

    correction  id

--- src/themes/icmaa-imp/test/e2e/integration/Search/searchAlias.spec.ts
+++ src/themes/icmaa-imp/test/e2e/integration/Search/searchAlias.spec.ts
@@ -2,13 +2,13 @@ describe('SearchAlias', () => {
   it('Search Pants', () => {
     cy.visitAsRecurringUser('/')
     cy.get('[data-test-id="SearchInput"]').click()
-    cy.get('[data-test-id="searchSidebar"]').find('input').type('Pants')
-    cy.get('[data-test-id="searchSidebar"] [data-test-id="ProductTile"]').log('search')
+    cy.get('[data-test-id="SearchPanel"]').find('input').type('Pants')
+    cy.get('[data-test-id="SearchPanel"] [data-test-id="ProductTile"]').log('search')
   })
   it('Search Hosen', () => {
     cy.visitAsRecurringUser('/')
     cy.get('[data-test-id="SearchInput"]').click()
-    cy.get('[data-test-id="searchSidebar"]').find('input').type('Hosen')
-    cy.get('[data-test-id="searchSidebar"] [data-test-id="ProductTile"]').log('searchAlias')
+    cy.get('[data-test-id="SearchPanel"]').find('input').type('Hosen')
+    cy.get('[data-test-id="SearchPanel"] [data-test-id="ProductTile"]').log('searchAlias')
   })
 })
