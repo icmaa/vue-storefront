87b84c48d51580b309ff406eb952721dff996789
schill <s.chill@impericon.com>
2020-02-14 11:49:44 +0100

    Improvements

--- src/themes/icmaa-imp/test/e2e/integration/Marketing/recommendations.spec.ts
+++ src/themes/icmaa-imp/test/e2e/integration/Marketing/recommendations.spec.ts
@@ -1,22 +1,24 @@
 describe('Recommendations', () => {
   it('Recommendations should be visible', () => {
     // Click random product
-    cy.visitAsRecurringUser('/new', { storeCode: 'de' })
-      .getByTestId('productLink')
-      .random()
-      .click()
+    cy.visitCategoryPage()
+      .getByTestId('ProductTile')
+      .findByTestId('productLink')
+      .clickRandomElement()
 
     // Product has recommendations
     cy.getByTestId('Recommendations')
       .as('recommendations')
       .findByTestId('ProductTile')
+      .random()
       .find('img')
       .each(e => cy.wrap(e).checkImage())
 
-    // Recommendation have link element
+    // Recommendation has link
     cy.get('@recommendations')
       .findByTestId('productLink')
       .random()
       .should('have.attr', 'href')
+      .and('have.length.gt', 0)
   })
 })
