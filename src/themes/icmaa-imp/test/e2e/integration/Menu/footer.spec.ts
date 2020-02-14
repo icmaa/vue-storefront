427283bfdf639916342755c85125ba9007b1ece1
schill <s.chill@impericon.com>
2020-02-14 14:34:43 +0100

    Packed tests

--- src/themes/icmaa-imp/test/e2e/integration/Menu/footer.spec.ts
+++ src/themes/icmaa-imp/test/e2e/integration/Menu/footer.spec.ts
@@ -2,26 +2,17 @@ describe('Footer', () => {
   it('Footer should be visible', () => {
     cy.visitAsRecurringUser('/')
     cy.get('footer').should('be.visible')
-  })
 
-  it('Social media links should be visible in footer', () => {
-    cy.visitAsRecurringUser('/')
     cy.get('footer .social-media').should('be.visible')
     cy.get('footer .social-media a')
       .should('have.length', 6)
-  })
 
-  it('Service carrier should be visible in footer', () => {
-    cy.visitAsRecurringUser('/')
     cy.get('footer .service-carrier')
       .should('be.visible')
-  })
 
-  it('Navigation should be visible in footer', () => {
-    cy.visitAsRecurringUser('/')
     cy.get('footer .footer-navigation')
       .should('be.visible')
       .find('a')
-      .should('have.length', 7)
+      .should('have.length.gt', 0)
   })
 })
