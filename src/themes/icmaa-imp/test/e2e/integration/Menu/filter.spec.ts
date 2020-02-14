b7c395fd6963bf9f192c1b4cb5711203800f0a84
schill <s.chill@impericon.com>
2020-02-14 14:04:45 +0100

    Compare product totals before and after filter

--- src/themes/icmaa-imp/test/e2e/integration/Menu/filter.spec.ts
+++ src/themes/icmaa-imp/test/e2e/integration/Menu/filter.spec.ts
@@ -3,34 +3,20 @@ describe('Filter', () => {
     cy.visitAsRecurringUser('/girls.html')
     cy.openFilterSidebar()
 
-    cy.get('.sidebar-content').find('[data-attribute-key="department"] button')
-      .clickRandomElement()
+    // compare product totals before and after a filter is selected
+    cy.getByTestId('productsTotal').then((element) => {
+      const productsTotalNumber = parseInt(element.text())
 
-    cy.url().should('include', '?department=')
-  })
+      // Select deparment filter
+      cy.get('@sidebar').find('[data-attribute-key="department"] button')
+        .clickRandomElement()
 
-  it('Select gender filter', () => {
-    cy.visitAsRecurringUser('/girls.html')
-    cy.openFilterSidebar()
+      cy.getByTestId('productsTotal').should((element) => {
+        const productsTotalFiltered = parseInt(element.text())
+        expect(productsTotalNumber).gt(productsTotalFiltered)
+      })
+    })
 
-    cy.get('.sidebar-content').find('[data-attribute-key="gender"] button')
-      .clickRandomElement()
-
-    cy.url().should('include', '?gender=')
-  })
-
-  it('Select multiple colors', () => {
-    cy.visitAsRecurringUser('/girls.html')
-    cy.openFilterSidebar()
-
-    cy.get('.sidebar-content').find('[data-attribute-key="color"] button')
-      .first()
-      .click()
-
-    cy.get('.sidebar-content').find('[data-attribute-key="color"] button')
-      .last()
-      .click()
-
-    cy.url().should('include', '?color=').and('include', '&color=')
+    cy.url().should('include', `?department=`)
   })
 })
