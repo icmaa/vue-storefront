9d0ad982a4ea0930b29e2130ad8c5ece5fa705da
dlandmann <d.landmann@impericon.com>
2020-02-14 16:40:54 +0100

    compare search-results (length) if used aliases

--- src/themes/icmaa-imp/test/e2e/integration/Search/search.spec.ts
+++ src/themes/icmaa-imp/test/e2e/integration/Search/search.spec.ts
@@ -1,14 +1,24 @@
-describe('SearchAlias', () => {
-  it('Search Pants', () => {
-    cy.visitAsRecurringUser('/')
+describe('Search and SearchAliases', () => {
+  it('Search Pants and his Alias Hosen', () => {
+    cy.visitAsRecurringUser('/', { storeCode: 'de' })
     cy.getByTestId('SearchInput').click()
     cy.getByTestId('Sidebar').find('input').type('Pants')
-    cy.getByTestId('Sidebar').findByTestId('ProductTile').log('search')
-  })
-  it('Search Hosen', () => {
-    cy.visitAsRecurringUser('/')
-    cy.getByTestId('SearchInput').click()
-    cy.getByTestId('Sidebar').find('input').type('Hosen')
-    cy.getByTestId('Sidebar').findByTestId('ProductTile').log('search')
+    cy.getByTestId('Sidebar').findByTestId('ProductTile').as('target')
+
+    cy.get('@target')
+      .then(listing => {
+        const countTarget = Cypress.$(listing).length
+
+        cy.visitAsRecurringUser('/', { storeCode: 'de' })
+        cy.getByTestId('SearchInput').click()
+        cy.getByTestId('Sidebar').find('input').type('Hosen')
+        cy.getByTestId('Sidebar').findByTestId('ProductTile').as('alias')
+
+        cy.get('@alias')
+          .then(listing => {
+            const countAlias = Cypress.$(listing).length
+            expect(countAlias).to.eql(countTarget)
+          })
+      })
   })
 })
