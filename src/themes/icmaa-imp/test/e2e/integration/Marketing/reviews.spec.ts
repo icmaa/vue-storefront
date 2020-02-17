b7faa9b674a8297b1ba8c41ed590aa0c0f84fdf9
schill <s.chill@impericon.com>
2020-02-17 09:24:25 +0100

    Fix missing parameter in selectRandomOption

--- src/themes/icmaa-imp/test/e2e/integration/Marketing/reviews.spec.ts
+++ src/themes/icmaa-imp/test/e2e/integration/Marketing/reviews.spec.ts
@@ -15,7 +15,7 @@ describe('Reviews', () => {
     cy.getFaker().then(faker => {
       cy.get('input[name="name"]').type(faker.name.firstName())
       cy.get('input[name="email"]').type(faker.internet.email())
-      cy.get('#rating').selectRandomOption()
+      cy.get('#rating').selectRandomOption(true)
       cy.get('input[name="summary"]').type('Lorem ipsum')
       cy.get('textarea[name="review"]').type('Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod')
       cy.get('form button').click()
