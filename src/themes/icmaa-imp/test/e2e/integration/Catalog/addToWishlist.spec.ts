696a301ad4157af380a68dfd90bbca3c1dcd6c36
Dierk Landmann <d.landmann@impericon.com>
2020-02-17 16:17:31 +0100

    finetuning addtowishist

--- src/themes/icmaa-imp/test/e2e/integration/Catalog/addToWishlist.spec.ts
+++ src/themes/icmaa-imp/test/e2e/integration/Catalog/addToWishlist.spec.ts
@@ -1,18 +1,20 @@
 describe('Add to And Remove from Wishlist ...', () => {
-  it('... from ProductListing', () => {
-    cy.visitCategoryPage()
-    cy.getByTestId('ProductTile').random().findByTestId('AddToWishlist').click()
-    cy.checkNotification('success')
-    cy.openNavigationSidebar('[data-test-id="HeaderButtonWishlist"]')
-    cy.get('@sidebar').findByTestId('AddToWishlistButton').clickRandomElement()
-    cy.checkNotification('success')
-  })
-
   it('... from ProductDetailPage', () => {
     cy.visitCategoryPage()
     cy.getByTestId('ProductTile').random().findByTestId('productLink').click()
     cy.getByTestId('AddToWishlistButton').click()
     cy.checkNotification('success')
+    cy.scrollTo('top')
+    cy.openNavigationSidebar('[data-test-id="HeaderButtonWishlist"]')
+    cy.get('@sidebar').findByTestId('AddToWishlistButton').clickRandomElement()
+    cy.checkNotification('success')
+  })
+  it('... from ProductListing', () => {
+    cy.visitCategoryPage()
+    cy.wait(1000)
+    cy.getByTestId('ProductTile').random().findByTestId('AddToWishlist').click()
+    cy.checkNotification('success')
+    cy.scrollTo('top')
     cy.openNavigationSidebar('[data-test-id="HeaderButtonWishlist"]')
     cy.get('@sidebar').findByTestId('AddToWishlistButton').clickRandomElement()
     cy.checkNotification('success')
