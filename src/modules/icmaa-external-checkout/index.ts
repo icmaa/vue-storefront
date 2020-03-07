21f7a4cf6343be34f2d738c3a2691258bfaec437
cewald <c.ewald@impericon.com>
2020-03-07 09:45:18 +0000

    Add `vue-cookies` and init it via `icmaa-external-checkout` module

--- src/modules/icmaa-external-checkout/index.ts
+++ src/modules/icmaa-external-checkout/index.ts
@@ -1,5 +1,13 @@
 import config from 'config'
 
+import Vue from 'vue'
+import VueCookies from 'vue-cookies'
+
+import { once } from '@vue-storefront/core/helpers'
+import { coreHooks } from '@vue-storefront/core/hooks'
+import { isServer } from '@vue-storefront/core/helpers'
+import { Logger } from '@vue-storefront/core/lib/logger'
+
 import { StorefrontModule } from '@vue-storefront/core/lib/modules'
 import { setupMultistoreRoutes } from '@vue-storefront/core/lib/multistore'
 
@@ -9,6 +17,16 @@ import moduleRoutes from './routes'
 export const KEY = 'external-checkout'
 
 export const IcmaaExternalCheckoutModule: StorefrontModule = function ({ router }) {
+  if (!isServer) {
+    once('__VUE_EXTEND_COOKIES__', () => {
+      Vue.use(VueCookies)
+    })
+
+    coreHooks.afterAppInit(async () => {
+      Logger.error('All keys: ', 'icmaa-external-checkout', Vue.$cookies.keys())()
+    })
+  }
+
   router.beforeEach(beforeEachGuard)
   setupMultistoreRoutes(config, router, moduleRoutes, 10)
 }
