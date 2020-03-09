65550689da9500a858f3f1330f4e659de442caf1
cewald <c.ewald@impericon.com>
2020-03-09 07:08:44 +0000

    Add quote-token reconnect

--- src/modules/icmaa-external-checkout/index.ts
+++ src/modules/icmaa-external-checkout/index.ts
@@ -24,11 +24,22 @@ export const IcmaaExternalCheckoutModule: StorefrontModule = function ({ router,
 
     EventBus.$on('session-after-nonauthorized', async () => {
       const customerToken = Vue.$cookies.get('vsf_token_customer')
-      if (!store.getters['user/isLoggedIn'] && customerToken) {
-        Logger.info('Customer token found in cookie – try to login:', 'external-checkout', customerToken)()
-        store.dispatch('user/startSessionWithToken', customerToken).then(() => {
-          Vue.$cookies.remove('vsf_token_customer')
-        })
+      const quoteToken = Vue.$cookies.get('vsf_token_quote')
+
+      if (!store.getters['user/isLoggedIn'] && (customerToken || quoteToken)) {
+        if (customerToken) {
+          Logger.info('Customer token found in cookie – try to login:', 'external-checkout', customerToken)()
+          store.dispatch('user/startSessionWithToken', customerToken).then(() => {
+            Vue.$cookies.remove('vsf_token_customer')
+          })
+        }
+
+        if (quoteToken) {
+          Logger.info('Quote token found in cookie – try to reconnect with:', 'external-checkout', quoteToken)()
+          store.dispatch('cart/reconnect', { token: quoteToken }).then(() => {
+            Vue.$cookies.remove('vsf_token_quote')
+          })
+        }
       }
     })
 
