a1c2ecb7b535c2b8938ab20a1808bf2095182951
cewald <c.ewald@impericon.com>
2020-03-27 13:10:28 +0000

    Bugfix for init load bug

--- src/modules/icmaa-google-tag-manager/hooks/afterEach.ts
+++ src/modules/icmaa-google-tag-manager/hooks/afterEach.ts
@@ -2,18 +2,18 @@ import { currentStoreView } from '@vue-storefront/core/lib/multistore'
 
 export interface AfterEachHookOptions {
   store: any,
-  path: string,
-  viewName: string
+  to: any,
+  from: any
 }
 
-export default ({ store, path, viewName }: AfterEachHookOptions): void => {
+export default ({ store, to, from }: AfterEachHookOptions): void => {
   let dataLayer = (window['dataLayer'] = window['dataLayer'] || [])
   const { storeCode } = currentStoreView()
 
   dataLayer.push({
     'event': 'icmaa-content-view',
-    'content-name': path,
-    'content-view-name': viewName,
+    'content-name': to.fullPath,
+    'content-view-name': to.meta.gtm || to.name,
     'store_code': storeCode,
     'customerLoggedIn': store.getters['user/isLoggedIn'],
     'customerEmail': store.getters['user/getUserEmail']
