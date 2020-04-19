2ff4a427fb1fc7df1f9a9272d9969abf6e0fa3ad
cewald <c.ewald@impericon.com>
2020-04-19 09:06:52 +0000

    Remove custom mutator – don't need it

--- src/modules/icmaa-catalog/store/product/index.ts
+++ src/modules/icmaa-catalog/store/product/index.ts
@@ -1,9 +1,7 @@
 import { Module } from 'vuex'
 import actions from './actions'
-import mutations from './mutations'
 import ProductState from '@vue-storefront/core/modules/catalog/types/ProductState'
 
 export const IcmaaExtendedProductStore: Module<ProductState, any> = {
-  actions,
-  mutations
+  actions
 }
