b035f00001b8c4326c8dc96f210d540cdf2112f2
schill <s.chill@impericon.com>
2020-04-23 12:19:46 +0200

    Added alternative text if there's no release date

--- src/themes/icmaa-imp/components/core/blocks/Product/ProductPreorder.vue
+++ src/themes/icmaa-imp/components/core/blocks/Product/ProductPreorder.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="t--mb-8 t--mx-8 t-bg-base-tone t-px-8 t-py-4 t-text-sm t-text-white">
-    <material-icon icon="alarm" size="sm" class="t--ml-6 t-absolute " />
+    <material-icon icon="alarm" size="sm" class="t--ml-6 t-absolute" />
     <span class="t-font-bold">{{ $t('Notice') }}: </span>
     <span class="description" v-text="preorderText" />
   </div>
@@ -25,12 +25,12 @@ export default {
   },
   computed: {
     preorderText () {
-      if (this.hasReleaseDate) {
+      if (this.hasMediaReleaseDate) {
         return i18n.t('Delivery of your complete order not before {date}. The preorder date is the release date for Germany, as we were told by the record label or distributor. There is no guarantee for a delivery on that date. In exceptional cases, especially for imported products, there might be delays. As soon as we receive the article, we will ship it.', { date: this.mediaReleaseDate })
       }
-      return i18n.t('No release date')
+      return i18n.t('Delivery of your complete order not before official release. The preorder date is the release or delivery date for Germany, as we were told by the record label,the producer or the shipping company. There is no guarantee for a delivery on that date. In exceptional cases, especially for US imported products, there might be delays. As soon as we receive the article, we will ship it.')
     },
-    hasReleaseDate () {
+    hasMediaReleaseDate () {
       return this.product.media_release || false
     },
     mediaReleaseDate () {
