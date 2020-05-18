4081a43c04a0fae4c4d49e37fd3fb1bcc422e2c3
schill <s.chill@impericon.com>
2020-05-18 11:59:57 +0200

    Removed materialicon

--- src/themes/icmaa-imp/components/core/blocks/MyAccount/Headline.vue
+++ src/themes/icmaa-imp/components/core/blocks/MyAccount/Headline.vue
@@ -1,18 +1,14 @@
 <template>
   <h1 class="t-flex t-flex-wrap t-items-center t-mt-2 t-mb-6 t-text-2xl t-leading-tight">
-    <material-icon :icon="icon" size="md" class="t-mr-2" />
     <slot />
   </h1>
 </template>
 
 <script>
 
-import MaterialIcon from 'theme/components/core/blocks/MaterialIcon'
-
 export default {
   name: 'ProfileHeadline',
   components: {
-    MaterialIcon
   },
   props: {
     icon: {
