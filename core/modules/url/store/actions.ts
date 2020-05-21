84f732401c11741d6c66f3533de0a6a6a0ad3c28
cewald <c.ewald@impericon.com>
2020-05-21 07:54:24 +0000

    Revert "Revert "Add core events to server and logger + consolidate use of logger""
    This reverts commit c90b6145455c7bee209df356b3327bb4d5e82e6f.

--- core/modules/url/store/actions.ts
+++ core/modules/url/store/actions.ts
@@ -78,7 +78,7 @@ export const actions: ActionTree<UrlState, any> = {
    * This method could be overriden in custom module to provide custom URL mapping logic
    */
   async mappingFallback ({ dispatch }, { url, params }: { url: string, params: any}) {
-    console.warn(`
+    Logger.warn(`
       Deprecated action mappingFallback - use mapFallbackUrl instead.
       You can enable mapFallbackUrl by changing 'config.urlModule.enableMapFallbackUrl' to true
     `)()
@@ -88,7 +88,7 @@ export const actions: ActionTree<UrlState, any> = {
     productQuery.applyFilter({key: 'url_path', value: {'eq': url}}) // Tees category
     const products = await dispatch('product/list', { query: productQuery }, { root: true })
     if (products && products.items && products.items.length) {
-      console.log(url, productQuery)
+      Logger.log(url, 'search', productQuery)()
       const product = products.items[0]
       return {
         name: localizedDispatcherRouteName(product.type_id + '-product', storeCode, appendStoreCode),
