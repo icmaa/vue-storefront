3bdacbcb1864e0f69aba43ae553cc7d888eceab5
cewald <c.ewald@impericon.com>
2020-06-02 06:14:37 +0000

    Add local storage quota limit to core modules

--- core/modules/url/index.ts
+++ core/modules/url/index.ts
@@ -3,7 +3,7 @@ import { StorefrontModule } from '@vue-storefront/core/lib/modules'
 import { beforeEachGuard } from './router/beforeEach'
 import { StorageManager } from '@vue-storefront/core/lib/storage-manager'
 
-export const cacheStorage = StorageManager.init('url')
+export const cacheStorage = StorageManager.init('url', undefined, 2048)
 
 export const UrlModule: StorefrontModule = function ({ store, router }) {
   store.registerModule('url', urlStore)
