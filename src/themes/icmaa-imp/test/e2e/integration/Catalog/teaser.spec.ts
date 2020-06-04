08656a8b4465b865b4e71303aa5ad2d38ae65943
Christian Ewald <c.ewald@impericon.com>
2020-06-04 13:23:59 +0200

    Improve test to also test whitelist

--- src/themes/icmaa-imp/test/e2e/integration/Catalog/teaser.spec.ts
+++ src/themes/icmaa-imp/test/e2e/integration/Catalog/teaser.spec.ts
@@ -1,10 +1,12 @@
 describe('Teaser', () => {
-  it('Show teaser', () => {
+  it('should be visible on category page', () => {
     cy.visit('/sales.html')
     cy.getByTestId('Teaser').should('be.visible')
   })
-  it('Hide teaser', () => {
+  it('should be toggled on category page when GET parameter filter is set', () => {
     cy.visit('/sales.html?gender=9')
     cy.getByTestId('Teaser').should('be.not.visible')
+    cy.visit('/sales.html?utm_source=CUSTOMSOURCE')
+    cy.getByTestId('Teaser').should('be.visible')
   })
 })
