32d26f869073582a1a3fefac3a56d0737dd78c4b
schill <s.chill@impericon.com>
2020-06-08 12:50:34 +0200

    Consolidated div elements. Added LazyHydrate

--- src/themes/icmaa-imp/components/core/blocks/ICMAA/Cms/Pages/DigitalEvents.vue
+++ src/themes/icmaa-imp/components/core/blocks/ICMAA/Cms/Pages/DigitalEvents.vue
@@ -1,22 +1,18 @@
 <template :class="content.backgroundcolor">
   <div class="t-container">
     <!-- Video -->
-    <div class="t-container t-w-full t-mb-4">
-      <div class="t-relative t-w-full t-bg-white" style="padding-top: 56.25%">
-        <iframe class="t-absolute t-top-0" width="100%" height="100%" :src="videoUrl" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen/>
-      </div>
+    <div class="t-relative t-w-full t-bg-white t-mb-4" style="padding-top: 56.25%">
+      <iframe class="t-absolute t-top-0" width="100%" height="100%" :src="videoLink" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen/>
     </div>
 
     <!-- Button -->
-    <template v-if="buttonLink">
-      <div class="t-flex t-justify-center t-mb-4">
-        <a :href="buttonLink">
-          <button-component type="primary" size="md" class="t-w-full  t-block t-text-white t-px-4 t-py-2">
-            {{ buttonText }}
-          </button-component>
-        </a>
-      </div>
-    </template>
+    <div v-if="buttonLink" class="t-flex t-justify-center t-mb-4">
+      <a :href="buttonLink">
+        <button-component type="primary" size="md" class="t-w-full t-block t-text-white t-px-4 t-py-2">
+          {{ buttonText }}
+        </button-component>
+      </a>
+    </div>
 
     <!-- SEO text-->
     <div class="t-w-full t-px-3 lg:t-px-4 t-mb-4">
@@ -31,9 +27,9 @@
 </template>
 
 <script>
-import Page from 'icmaa-cms/mixins/Page'
-import LazyHydrate from 'vue-lazy-hydration'
 import ButtonComponent from 'theme/components/core/blocks/Button'
+import LazyHydrate from 'vue-lazy-hydration'
+import Page from 'icmaa-cms/mixins/Page'
 import ProductListingWidget from 'icmaa-category/components/core/ProductListingWidget'
 
 import { mapGetters } from 'vuex'
@@ -43,7 +39,8 @@ export default {
   mixins: [ Page ],
   components: {
     ButtonComponent,
-    ProductListingWidget
+    ProductListingWidget,
+    LazyHydrate
   },
   data () {
     return {
@@ -51,17 +48,17 @@ export default {
     }
   },
   computed: {
-    videoUrl () {
+    videoLink () {
       return this.content.video.link
+    },
+    seoText () {
+      return this.content.seo_text || ''
     },
     buttonText () {
       return this.content.button.text || ''
     },
     buttonLink () {
       return this.content.button.link || ''
-    },
-    seoText () {
-      return this.content.seo_text || ''
     }
   }
 }
