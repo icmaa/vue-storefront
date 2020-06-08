da9c6a131c5bbde71a4ec9601afbc2df55369fda
simonchill <s.chill@impericon.com>
2020-06-08 13:44:03 +0200

    #202341 Added digital-events page (#353)

---
+++ src/themes/icmaa-imp/components/core/blocks/ICMAA/Cms/Pages/DigitalEvents.vue
@@ -0,0 +1,65 @@
+<template :class="content.backgroundcolor">
+  <div class="t-container">
+    <!-- Video -->
+    <div class="t-relative t-w-full t-bg-white t-mb-4" style="padding-top: 56.25%">
+      <iframe class="t-absolute t-top-0" width="100%" height="100%" :src="videoLink" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen/>
+    </div>
+
+    <!-- Button -->
+    <div v-if="buttonLink" class="t-flex t-justify-center t-mb-4">
+      <a :href="buttonLink">
+        <button-component type="primary" size="md" class="t-w-full t-block t-text-white t-px-4 t-py-2">
+          {{ buttonText }}
+        </button-component>
+      </a>
+    </div>
+
+    <!-- SEO text-->
+    <div class="t-w-full t-px-4 t-mb-4">
+      <p>{{ seoText }}</p>
+    </div>
+
+    <!-- Products -->
+    <lazy-hydrate when-visible>
+      <product-listing-widget :category-id="content.products.category_id" :limit="content.products.limit" />
+    </lazy-hydrate>
+  </div>
+</template>
+
+<script>
+import ButtonComponent from 'theme/components/core/blocks/Button'
+import LazyHydrate from 'vue-lazy-hydration'
+import Page from 'icmaa-cms/mixins/Page'
+import ProductListingWidget from 'icmaa-category/components/core/ProductListingWidget'
+
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'DigitalEvent',
+  mixins: [ Page ],
+  components: {
+    ButtonComponent,
+    ProductListingWidget,
+    LazyHydrate
+  },
+  data () {
+    return {
+      dataType: 'yaml'
+    }
+  },
+  computed: {
+    videoLink () {
+      return this.content.video.link
+    },
+    seoText () {
+      return this.content.seo_text || ''
+    },
+    buttonText () {
+      return this.content.button.text || ''
+    },
+    buttonLink () {
+      return this.content.button.link || ''
+    }
+  }
+}
+</script>
--- src/themes/icmaa-imp/router/icmaa-cms/router.ts
+++ src/themes/icmaa-imp/router/icmaa-cms/router.ts
@@ -9,6 +9,7 @@ const AffiliateComponent = () => import(/* webpackChunkName: "vsf-icmaa-cms-page
 const TicketsComponent = () => import(/* webpackChunkName: "vsf-icmaa-cms-page-custom-tickets" */ 'theme/pages/Tickets.vue')
 const FestivalComponent = () => import(/* webpackChunkName: "vsf-icmaa-cms-page-custom-festival" */ 'theme/components/core/blocks/ICMAA/Cms/Pages/Festival.vue')
 const INSDComponent = () => import(/* webpackChunkName: "vsf-icmaa-cms-page-custom-impericon-never-say-die" */ 'theme/components/core/blocks/ICMAA/Cms/Pages/ImpericonNeverSayDie.vue')
+const DigitalEventsComponent = () => import(/* webpackChunkName: "vsf-icmaa-cms-page-custom-digital-events" */ 'theme/components/core/blocks/ICMAA/Cms/Pages/DigitalEvents.vue')
 
 export const routes: any = [
   // Custom cms routes, like /service or /festivals
@@ -21,5 +22,6 @@ export const routes: any = [
   { name: 'affiliate', path: '/:identifier', component: AffiliateComponent },
   { name: 'tickets', path: '/:identifier', component: TicketsComponent },
   { name: 'festival', path: '/:identifier', component: FestivalComponent },
+  { name: 'digital-events', path: '/:identifier', component: DigitalEventsComponent },
   { name: 'impericon-never-say-die', path: '/:identifier', component: INSDComponent }
 ]
