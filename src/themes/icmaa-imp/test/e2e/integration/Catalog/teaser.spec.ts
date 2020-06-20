797db03d9a3eb16b33a61d0ffe7d21265cdcf695
cewald <c.ewald@impericon.com>
2020-06-20 08:45:04 +0000

    Improve teaser test

--- src/themes/icmaa-imp/test/e2e/integration/Catalog/teaser.spec.ts
+++ src/themes/icmaa-imp/test/e2e/integration/Catalog/teaser.spec.ts
@@ -1,10 +1,10 @@
 describe('Teaser', () => {
   it('should be visible on category page', () => {
-    cy.visit('/sales.html')
+    cy.visitAsRecurringUser('/sales.html')
     cy.getByTestId('Teaser').should('be.visible')
   })
   it('should be toggled on category page when GET parameter filter is set', () => {
-    cy.visit('/sales.html?gender=9')
+    cy.visitAsRecurringUser('/sales.html?gender=9')
     cy.getByTestId('Teaser').should('be.not.visible')
     cy.visit('/sales.html?utm_source=CUSTOMSOURCE')
     cy.getByTestId('Teaser').should('be.visible')
