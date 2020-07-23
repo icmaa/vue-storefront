56e91e1d5042a630536027905069c4d5abc7e320
cewald <c.ewald@impericon.com>
2020-07-23 11:53:26 +0200

    Make `storeSwitchNotification` test conditional

--- src/themes/icmaa-imp/test/e2e/integration/Misc/storeSwitchNotification.spec.ts
+++ src/themes/icmaa-imp/test/e2e/integration/Misc/storeSwitchNotification.spec.ts
@@ -2,29 +2,39 @@ import Settings from '../../support/utils/Settings'
 
 const { _ } = Cypress
 
+const getInitialState = () => cy.window().its('__INITIAL_STATE__')
+
 describe('Notification for Store switch', () => {
   it('shows up after switching stores', () => {
     cy.visitAsRecurringUser('/')
 
-    cy.get<string>('@storeCode').then(storeCode => {
-      const storeViews: any = Settings.availableStoreViews.filter(l => l !== storeCode)
-      storeCode = _.sample(storeViews)
+    getInitialState()
+      .its<string>('config.icmaa.storeViewSwitch.showAdvice')
+      .then(enabled => {
+        cy.log('The store-view-switch notification is ' + (enabled ? 'enabled' : 'disabled'))
 
-      cy.setStoreCode(storeCode)
+        // Only run test if this feature is enabled
+        if (enabled) {
+          cy.get<string>('@storeCode').then(storeCode => {
+            const storeViews: any = Settings.availableStoreViews.filter(l => l !== storeCode)
+            storeCode = _.sample(storeViews)
 
-      cy.visit('/', { storeCode })
-    })
+            cy.setStoreCode(storeCode)
 
-    cy.getByTestId('ModalStoreViewAdvice').should('be.visible')
-    cy.getByTestId('ModalClose').click()
-    cy.getByTestId('Modal').should('not.be.visible')
+            cy.visit('/', { storeCode })
+          })
 
-    cy.getFromLocalStorage('shop/uniClaims/languageAccepted')
-      .then(claim => {
-        // { "code": "languageAccepted", "created_at": "2020-07-23T09:17:34.003Z", "value": { "accepted": true, "storeCode": "es" } }
-        cy.get<string>('@storeCode').then(storeCode => {
-          expect(claim).to.contain(`"storeCode":"${storeCode}"`, 'New store code is in localstorag true')
-        })
+          cy.getByTestId('ModalStoreViewAdvice').scrollIntoView().should('be.visible')
+          cy.getByTestId('ModalClose').click()
+          cy.getByTestId('Modal').should('not.be.visible')
+
+          cy.getFromLocalStorage('shop/uniClaims/languageAccepted')
+            .then(claim => {
+              cy.get<string>('@storeCode').then(storeCode => {
+                expect(claim).to.contain(`"storeCode":"${storeCode}"`, 'New store code is in localstorag true')
+              })
+            })
+        }
       })
   })
 })
