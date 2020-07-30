573d943aa8a1e8366f8b7ec6ec7aec93db029da7
Christian Ewald <c.ewald@impericon.com>
2020-07-30 07:15:51 +0200

    #206324 Trigger events in `icmaa-google-tag-manager` for login/logout (#408)

--- src/modules/icmaa-google-tag-manager/hooks/afterRegistration.ts
+++ src/modules/icmaa-google-tag-manager/hooks/afterRegistration.ts
@@ -3,6 +3,7 @@ import VueGtm from 'vue-gtm'
 import { Store } from 'vuex'
 import { currentStoreView } from '@vue-storefront/core/lib/multistore'
 import { isServer } from '@vue-storefront/core/helpers'
+import { userHooks } from '@vue-storefront/core/modules/user/hooks'
 
 import { IcmaaGoogleTagManager as EventHooks } from './'
 import { claimCollection } from 'theme/store/claims'
@@ -62,6 +63,18 @@ export async function afterRegistration (config, store: Store<any>) {
     GTM.trackEvent({
       event: 'icmaa-search',
       searchTerm
+    })
+  })
+
+  userHooks.afterUserAuthorize(() => {
+    GTM.trackEvent({
+      event: 'icmaa-login'
+    })
+  })
+
+  userHooks.afterUserUnauthorize(() => {
+    GTM.trackEvent({
+      event: 'icmaa-logout'
     })
   })
 }
