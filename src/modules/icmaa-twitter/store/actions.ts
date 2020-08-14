4063fb5918de95c841fd1e363d3521a46f0f8cdd
cewald <c.ewald@impericon.com>
2020-08-14 06:52:31 +0000

    Remove custom localstorage caching for `icmaa-spotify` & `icmaa-twitter`

--- src/modules/icmaa-twitter/store/actions.ts
+++ src/modules/icmaa-twitter/store/actions.ts
@@ -2,13 +2,10 @@ import { ActionTree } from 'vuex'
 import RootState from '@vue-storefront/core/types/RootState'
 import TwitterState from '../types/TwitterState'
 import * as mutationTypes from './mutation-types'
-import { cacheStorage as cache } from '../'
 
 import Axios from 'axios'
 import config from 'config'
 import { processURLAddress } from '@vue-storefront/core/helpers'
-
-import { Logger } from '@vue-storefront/core/lib/logger'
 
 const actions: ActionTree<TwitterState, RootState> = {
   async fetchStatusFeed (context, screenName: string) {
@@ -19,26 +16,15 @@ const actions: ActionTree<TwitterState, RootState> = {
       .catch(() => [])
   },
   async loadStatusFeed ({ dispatch, state, commit }, screenName: string): Promise<any> {
-    const cacheKey = 'status/' + screenName
     if (!state.status || Object.keys(state.status).length === 0 || !state.status.find(s => s.screenName === screenName)) {
-      if (await cache.getItem(cacheKey).then(item => item !== null)) {
-        return cache.getItem(cacheKey).then(status => {
-          commit(mutationTypes.SN_ICMAA_TWITTER_ADD_STATUS, { screenName, status })
-          return status
-        })
-      }
-
       const status = await dispatch('fetchStatusFeed', screenName)
 
       commit(mutationTypes.SN_ICMAA_TWITTER_ADD_STATUS, { screenName, status })
-      return cache.setItem(cacheKey, status)
-        .catch(error => Logger.error(error, 'icmaa-twitter'))
+      return status
     } else {
       return new Promise((resolve, reject) => {
         let result = state.status.find(s => s.screenName === screenName)
         if (result) {
-          cache.setItem(cacheKey, result.status)
-            .catch(error => Logger.error(error, 'icmaa-twitter'))
           resolve(result)
         } else {
           reject('Error while fetching state for ' + name)
