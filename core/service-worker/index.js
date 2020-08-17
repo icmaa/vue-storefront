0da6dd14bc3ae6b8f61589bbac95662f2de0a250
cewald <c.ewald@impericon.com>
2020-08-17 13:09:15 +0200

    Remove `clientsClaim` and `skipWaiting` as we don't need it

--- core/service-worker/index.js
+++ core/service-worker/index.js
@@ -1,8 +1,3 @@
 import '@vue-storefront/core/service-worker/core-service-worker'
 import '@vue-storefront/core/modules/offline-order/extends/service-worker'
 import 'theme/service-worker/index'
-
-import { skipWaiting, clientsClaim } from 'workbox-core'
-
-skipWaiting()
-clientsClaim()
