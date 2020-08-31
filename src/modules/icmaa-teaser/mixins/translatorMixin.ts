d6f374b5c8a6cf19e68c8eb25c8e2bbcaf7775dd
cewald <c.ewald@impericon.com>
2020-08-31 11:03:38 +0000

    Change prefix of teaser default translations to save file-size by string-length in bundled JSON files

--- src/modules/icmaa-teaser/mixins/translatorMixin.ts
+++ src/modules/icmaa-teaser/mixins/translatorMixin.ts
@@ -3,7 +3,7 @@ import Translation from '../helpers/defaultTranslator'
 export default {
   filters: {
     translate (text) {
-      const t = new Translation({ text })
+      const t = new Translation({ prefix: 'TS', text })
       return t.translate()
     }
   }
