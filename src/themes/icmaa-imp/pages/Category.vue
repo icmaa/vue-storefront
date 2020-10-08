b0f139a3b9fafab20ddf128ed123ff149223c85d
Dierk Landmann <d.landmann@impericon.com>
2020-10-08 21:19:25 +0200

    #212232 Change category headline (#463)
    * Change headline from `title` to `category.name` for correct translation
    * Clean-up and improve components

--- src/themes/icmaa-imp/pages/Category.vue
+++ src/themes/icmaa-imp/pages/Category.vue
@@ -7,10 +7,12 @@
         <block-wrapper :components="contentHeader" v-if="contentHeader" />
         <div class="t-w-full">
           <div class="t-flex t-flex-wrap t-items-center t--mx-1 lg:t--mx-2">
-            <h1 class="category-title t-hidden lg:t-block t-w-3/4 t-px-1 lg:t-px-2 t-mb-4 t-font-light t-text-2xl t-text-base-dark" v-text="title" />
+            <h1 class="category-title t-hidden lg:t-block t-w-3/4 t-px-1 lg:t-px-2 t-mb-4 t-font-light t-text-2xl t-text-base-dark" v-text="category.name" />
             <div class="t-hidden lg:t-block t-w-1/4 t-px-1 lg:t-px-2 t-text-sm t-text-base-dark t-text-right">
-              <span class="t-font-bold" data-test-id="productsTotal">{{ getCategoryProductsTotal }}</span> {{ $t('items') }}
-              <span class="t-mx-2 t-text-base-lighter">|</span>
+              <span class="t-hidden xl:t-inline">
+                <span class="t-font-bold" data-test-id="productsTotal">{{ getCategoryProductsTotal }}</span> {{ $t('items') }}
+              </span>
+              <span class="t-mx-2 t-text-base-lighter t-hidden xl:t-inline">|</span>
               <dropdown @change="changePageSize" :options="pageSizeOptions" :current="parseInt(pageSize)" position="right" name="pagesize" class="t-inline-block" :dropdown-class="{ 't-w-32 t-mt-2': true }">
                 {{ pageSize }} {{ $t('items per page') }}
                 <material-icon icon="keyboard_arrow_down" size="xs" class="t-align-middle t-text-primary" />
