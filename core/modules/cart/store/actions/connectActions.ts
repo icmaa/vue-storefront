f4f4b5451af9cbb4c0fe8e94586649f9dbdc64ee
cewald <c.ewald@impericon.com>
2020-11-17 15:20:55 +0000

    Bugfix for type

--- core/modules/cart/store/actions/connectActions.ts
+++ core/modules/cart/store/actions/connectActions.ts
@@ -34,7 +34,7 @@ const connectActions = {
       await dispatch('removeCoupon', { sync: false })
     }
 
-    await dispatch('connect', { guestCart: false, mergeQty: false })
+    await dispatch('connect', { guestCart: false, mergeQty: true })
 
     if (coupon) {
       await dispatch('applyCoupon', coupon)
