17c117b4699d38eb7486bdae4cb2d26c807f8db6
cewald <c.ewald@impericon.com>
2020-12-02 09:48:58 +0000

    Hotfix for fetch-request payload

--- src/modules/icmaa-user/data-resolver/UserService.ts
+++ src/modules/icmaa-user/data-resolver/UserService.ts
@@ -4,21 +4,11 @@ import Task from '@vue-storefront/core/lib/sync/types/Task'
 import config from 'config'
 import getApiEndpointUrl from '@vue-storefront/core/helpers/getApiEndpointUrl'
 
-const headers = {
-  'Accept': 'application/json, text/plain, */*',
-  'Content-Type': 'application/json'
-}
-
 const getLastOrder = async (token: string): Promise<Task> =>
   TaskQueue.execute({
     url: processLocalizedURLAddress(
       getApiEndpointUrl(config.users, 'last_order').replace('{{token}}', token)
-    ),
-    payload: {
-      method: 'GET',
-      mode: 'cors',
-      headers
-    }
+    )
   })
 
 export const UserService: any = {
