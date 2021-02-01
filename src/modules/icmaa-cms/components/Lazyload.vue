923b97a316ca15eaa62e12764f83de34f6b2853c
cewald <c.ewald@impericon.com>
2021-02-01 12:03:42 +0000

    #220686 Add `lazyload` component and mixin * Implement it on QA page for teaser

---
+++ src/modules/icmaa-cms/components/Lazyload.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-if="inViewport">
+    <slot />
+  </div>
+  <div class="loader" v-else>
+    <slot name="loading" />
+  </div>
+</template>
+
+<script>
+
+import isVisibleMixin from 'icmaa-cms/mixins/isVisibleMixin'
+
+export default {
+  name: 'LazyloadComponent',
+  mixins: [ isVisibleMixin ]
+}
+</script>
