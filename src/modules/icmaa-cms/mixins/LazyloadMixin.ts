c0073363f71fe30f901faa43f665546df4e843c8
cewald <c.ewald@impericon.com>
2021-02-05 09:33:42 +0000

    Improve lazyload component

--- src/modules/icmaa-cms/mixins/LazyloadMixin.ts
+++ src/modules/icmaa-cms/mixins/LazyloadMixin.ts
@@ -1,10 +1,25 @@
 import { isServer } from '@vue-storefront/core/helpers'
 
+/**
+ * Everything you need to know about IntersectionObserver:
+ * @see https://www.smashingmagazine.com/2018/01/deferring-lazy-loading-intersection-observer-api/
+ *
+ * @todo Load components only when needed
+ */
+
 export default {
   props: {
     serverRendering: {
       type: Boolean,
       default: false
+    },
+    intersectOptions: {
+      type: Object,
+      default: () => {}
+    },
+    timeout: {
+      type: Number,
+      default: 0
     }
   },
   data () {
@@ -22,18 +37,30 @@ export default {
       this.inViewport = true
       return
     } else if (isServer && !this.serverRendering) {
-      return false
+      return
     }
 
-    const observer = new IntersectionObserver(entries => {
-      if (entries[0].intersectionRatio <= 0) return
+    let observerCallback = this.observerCallback
+    if (this.timeout > 0) {
+      let timeout
+      observerCallback = (e, o) => {
+        clearTimeout(timeout)
+        timeout = setTimeout(this.observerCallback.bind(this, e, o), this.timeout)
+      }
+    }
 
-      observer.unobserve(this.$el)
+    const observerOptions = Object.assign({ rootMargin: '200px 0px' }, this.intersectOptions)
+    const observer = new IntersectionObserver(observerCallback, observerOptions)
+    observer.observe(this.$el)
+  },
+  methods: {
+    observerCallback (entries, observer) {
+      if (!entries[0].isIntersecting) return
 
       this.inViewport = true
       this.$emit('visible')
-    })
 
-    observer.observe(this.$el)
+      observer.unobserve(this.$el)
+    }
   }
 }
