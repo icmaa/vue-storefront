79e98f534af199f2e95fa50ea46f9cc24093595d
cewald <c.ewald@impericon.com>
2021-02-17 11:06:42 +0000

    Clean-up code and finalize

--- src/modules/icmaa-category/index.ts
+++ src/modules/icmaa-category/index.ts
@@ -1,9 +1,6 @@
 import { StorefrontModule } from '@vue-storefront/core/lib/modules'
-import { setupMultistoreRoutes } from '@vue-storefront/core/lib/multistore'
 import { CategoryStore } from './store'
-import moduleRoutes from './routes'
 
 export const IcmaaCategoryModule: StorefrontModule = function ({ store, router, appConfig }) {
   store.registerModule('icmaaCategory', CategoryStore)
-  setupMultistoreRoutes(appConfig, router, moduleRoutes, 10)
 }
