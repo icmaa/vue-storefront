9c37e487c93588aba7db4cf262e763521082c647
cewald <c.ewald@impericon.com>
2021-02-25 13:36:35 +0000

    Workaround [WIP]

--- src/modules/icmaa-user/index.ts
+++ src/modules/icmaa-user/index.ts
@@ -3,6 +3,7 @@ import { StorefrontModule } from '@vue-storefront/core/lib/modules'
 import { extendStore } from '@vue-storefront/core/helpers'
 import { StorageManager } from '@vue-storefront/core/lib/storage-manager'
 import { isServer } from '@vue-storefront/core/helpers'
+import { localizedRoute } from '@vue-storefront/core/lib/multistore'
 
 import { ExtendedUserStore } from './store'
 import * as types from './store/mutation-types'
@@ -10,14 +11,21 @@ import * as types from './store/mutation-types'
 export const IcmaaExtendedUserModule: StorefrontModule = async function ({ store, router }) {
   extendStore('user', ExtendedUserStore)
 
-  router.beforeEach((to, from, next) => {
-    if (!isServer && to.meta.isSecure === true) {
-      console.error('TEST', to, store.getters['user/isLoggedIn'])
+  if (!isServer) {
+    router.beforeEach(async (to, from, next) => {
+      if (to.meta.isSecure === true && !store.getters['user/isLoggedIn']) {
+        EventBus.$once('session-after-started', () => {
+          if (store.getters['user/isLoggedIn']) {
+            next()
+          } else {
+            next(localizedRoute('/'))
+          }
+        })
+        return
+      }
       next()
-    }
-
-    next()
-  })
+    })
+  }
 
   store.subscribe((mutation, state) => {
     if (mutation.type.endsWith(types.USER_ADD_SESSION_DATA) || mutation.type.endsWith(types.USER_RMV_SESSION_DATA)) {
