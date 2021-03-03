67eaa90cdcc11e347d95c1bd0aab381fd4b1b9c2
Christian Ewald <c.ewald@impericon.com>
2021-03-03 11:03:50 +0100

    #218992 Improve Next-Generation Landingpage (#576)
    * Add picture component for responsive and preloaded images

--- src/themes/icmaa-imp/components/core/blocks/ICMAA/Cms/Pages/NextGeneration.vue
+++ src/themes/icmaa-imp/components/core/blocks/ICMAA/Cms/Pages/NextGeneration.vue
@@ -2,7 +2,7 @@
   <div class="t-bg-white">
     <!-- Header -->
     <div>
-      <img :src="getMediaThumbnail(imageUrlHeader, 0, 0)" :alt="content.header.name" :title="content.header.name">
+      <picture-component :src="headerImage.src" :width="headerImage.width" :height="headerImage.height" :sizes="headerImage.sizes" :placeholder="true" :ratio="`${headerImage.width}:${headerImage.height}`" :alt="headerImage.alt | htmlDecode" />
     </div>
     <!-- Text -->
     <div class="t-container t-text-center t-w-3/4 md:t-w-1/2">
@@ -11,29 +11,28 @@
     </div>
     <!-- Categories -->
     <div class="t-container t-flex t-flex-wrap">
-      <div v-for="category in content.categories" :key="category.name" class="t-w-full t-text-center md:t-w-1/2 t-p-10 t-self-center">
-        <img :src="getMediaThumbnail(category.img, 0, 0)" :alt="category.name" :title="category.name" class="t-w-full">
+      <div v-for="category in categories" :key="category.name" class="t-w-full t-text-center md:t-w-1/2 t-p-10 t-self-center">
+        <picture-component :src="category.img.src" :width="category.img.width" :height="category.img.height" :sizes="category.img.sizes" :placeholder="true" :ratio="`${category.img.width}:${category.img.height}`" :alt="category.img.alt | htmlDecode" class="t-w-full" />
         <div v-if="category.text">
-          <p class="t-font-bold t-my-4" v-html="category.text" />
-          <p class="t-font-bold" v-html="content.voting_price" /><span v-html="category.price_voting" />
-          <p class="t-font-bold" v-html="content.jury_price" /><span v-html="category.price_jury" />
-          <a :href="category.link">
-            <button-component type="primary" size="md" class="t-w-1/2 t-text-center t-bg-base-dark t-container t-text-white t-px-4 t-my-4">
-              {{ content.button_text }}
-            </button-component>
-          </a>
+          <p class="t-font-bold t-my-4" v-text="category.text" />
+          <p class="t-font-bold" v-text="content.voting_price" /><span v-text="category.price_voting" />
+          <p class="t-font-bold" v-text="content.jury_price" /><span v-text="category.price_jury" />
+          <button-component type="primary" size="md" class="t-w-1/2 t-text-center t-bg-base-dark t-container t-text-white t-px-4 t-my-4" @click="$router.push(localizedRoute(category.link))">
+            {{ content.button_text }}
+          </button-component>
         </div>
       </div>
     </div>
     <!-- Footer -->
     <div>
-      <img :src="getMediaThumbnail(imageUrlFooter, 0, 0)" :alt="content.footer.name" :title="content.footer.name">
+      <picture-component :src="footerImage.src" :width="footerImage.width" :height="footerImage.height" :sizes="footerImage.sizes" :placeholder="true" :ratio="`${footerImage.width}:${footerImage.height}`" :alt="footerImage.alt | htmlDecode" />
     </div>
   </div>
 </template>
 
 <script>
 import Page from 'icmaa-cms/mixins/Page'
+import PictureComponent from 'theme/components/core/blocks/Picture'
 import ButtonComponent from 'theme/components/core/blocks/Button'
 import { mapGetters } from 'vuex'
 
@@ -41,6 +40,7 @@ export default {
   name: 'NextGeneration',
   mixins: [ Page ],
   components: {
+    PictureComponent,
     ButtonComponent
   },
   data () {
@@ -49,17 +49,27 @@ export default {
     }
   },
   computed: {
-    ...mapGetters({
-      viewport: 'ui/getViewport'
-    }),
-    isMobile () {
-      return ['xs', 'sm'].includes(this.viewport)
+    headerImage () {
+      return this.content.header.img
     },
-    imageUrlHeader () {
-      return this.isMobile ? this.content.header.img_mobil : this.content.header.img
+    footerImage () {
+      return this.content.footer.img
     },
-    imageUrlFooter () {
-      return this.isMobile ? this.content.footer.img_mobil : this.content.footer.img
+    categories () {
+      return this.content.categories.map(c => {
+        const imageDefaults = {
+          alt: c.name,
+          width: 610,
+          height: 460,
+          sizes: [
+            { media: '(min-width: 415px)', width: 560 },
+            { media: '(max-width: 414px)', width: 415 }
+          ]
+        }
+
+        c.img = Object.assign(imageDefaults, c.img)
+        return c
+      })
     }
   }
 }
