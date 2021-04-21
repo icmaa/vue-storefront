2fef7cee7feb1d71f8781bf81c9c1c33df87e3df
Christian Ewald <c.ewald@impericon.com>
2021-04-21 11:30:24 +0200

    #226973 Sort reviews on PDP by `created_at:desc` (#632)

--- src/modules/icmaa-review/store/index.ts
+++ src/modules/icmaa-review/store/index.ts
@@ -1,7 +1,9 @@
 import { Module } from 'vuex'
 import getters from './getters'
+import actions from './actions'
 import ReviewState from '../type/ReviewState'
 
 export const ExtendedReviewStore: Module<ReviewState, any> = {
+  actions,
   getters
 }
