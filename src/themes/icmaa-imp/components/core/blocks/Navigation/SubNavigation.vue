cedf2faff5c88f84da93c7f00c0a7375000043d7
cewald <c.ewald@impericon.com>
2021-05-07 14:09:01 +0000

    Add navigation

--- src/themes/icmaa-imp/components/core/blocks/Navigation/SubNavigation.vue
+++ src/themes/icmaa-imp/components/core/blocks/Navigation/SubNavigation.vue
@@ -1,7 +1,20 @@
 <template>
-  <div>
+  <div v-if="navigation">
     <gender-navigation :items="genderNavigationItems" class="t--mx-4 t--mt-4 t-mb-4" />
-    {{ subNavigation }}
+    <div
+      v-if="navigation.navigation && navigation.navigation.length"
+      class="t-flex t-flex-wrap t-mb-6"
+    >
+      <router-link
+        v-for="(item, i) in navigation.navigation"
+        :key="`nav-${i}`"
+        :to="localizedRoute(item.route)"
+        class="t-w-full t-py-2 t-px-4 t-border-b t-border-base-lightest t-text-sm"
+        :class="{ 't-font-bold': item.bold }"
+        v-text="item.name"
+      />
+    </div>
+    {{ navigation }}
   </div>
 </template>
 
@@ -24,14 +37,14 @@ export default {
     ...mapGetters({
       'getJsonBlockByIdentifier': 'icmaaCmsBlock/getJsonBlockByIdentifier'
     }),
-    navigation () {
+    mainNavigation () {
       return this.getJsonBlockByIdentifier('navigation-main')
     },
     genderNavigationItems () {
-      return this.navigation.genderNavigation
+      return this.mainNavigation.genderNavigation
     },
-    subNavigation () {
-      return this.navigation.subNavigation[this.subNavigationKey] || false
+    navigation () {
+      return this.mainNavigation.subNavigation[this.subNavigationKey] || false
     }
   }
 }
