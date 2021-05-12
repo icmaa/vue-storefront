cddeecde45f363f6910bb3162ea36a7e9f06a7f0
cewald <c.ewald@impericon.com>
2021-05-12 06:18:47 +0000

    Add optional `gcloud/tracer` export

--- src/modules/icmaa-logs/lib/gcloud/index.ts
+++ src/modules/icmaa-logs/lib/gcloud/index.ts
@@ -1,11 +1,25 @@
 import { isEnabled } from './helper'
 
-/**
- * Add gcloud trace-agent, debugger and profiler – see `icmaa-logs` module
- */
+let tracer: any = {
+  createChildSpan: () => ({ endSpan: () => {} })
+}
+
 if (isEnabled) {
-  require('@google-cloud/trace-agent').start()
+  tracer = require('@google-cloud/trace-agent').start({
+    /**
+     * We could create a custom trace plugin for redis/minify
+     * @see https://github.com/googleapis/cloud-trace-nodejs/blob/master/doc/plugin-guide.md
+     */
+    // projectId: 'icmaa-shop-imp-test-01',
+    // keyFilename: 'key.json'
+    // plugins: {
+    //   'html-minifier': path.join(__dirname, 'tracer/plugins/html-minifier.js'),
+    //   'redis-tag-cache': path.join(__dirname, 'tracer/plugins/redis-tag-cache.js')
+    // }
+  })
+
   require('@google-cloud/profiler').start()
+
   // We don't have working source-maps yet and it wont work
   // require('@google-cloud/debug-agent').start({
   //   appPathRelativeToRepository: '../../',
@@ -14,3 +28,5 @@ if (isEnabled) {
 
   console.log('Enable Google Cloud Operations libraries')
 }
+
+export default tracer
