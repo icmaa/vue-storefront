95df9a9fa128438ab9ca1838a2850716cce9074f
cewald <c.ewald@impericon.com>
2021-05-21 06:58:13 +0000

    Prevent SSR hydration bug for cart

--- src/themes/icmaa-imp/components/core/blocks/Checkout/Cart.vue
+++ src/themes/icmaa-imp/components/core/blocks/Checkout/Cart.vue
@@ -3,23 +3,25 @@
     <h2 class="t-font-light t-text-xl t-mb-4">
       {{ $t('Shopping cart') }}
     </h2>
-    <div v-if="productsInCart.length">
-      <ul class="t-mb-4">
-        <product
-          v-for="product in productsInCart"
-          :key="product.sku"
-          :product="product"
-        />
-      </ul>
-      <totals />
-      <button-component
-        class="t-mt-6"
-        size="sm"
-        @click="editCart"
-      >
-        {{ $t('Edit your cart') }}
-      </button-component>
-    </div>
+    <no-ssr>
+      <div v-if="productsInCart.length">
+        <ul class="t-mb-4">
+          <product
+            v-for="product in productsInCart"
+            :key="product.sku"
+            :product="product"
+          />
+        </ul>
+        <totals />
+        <button-component
+          class="t-mt-6"
+          size="sm"
+          @click="editCart"
+        >
+          {{ $t('Edit your cart') }}
+        </button-component>
+      </div>
+    </no-ssr>
   </div>
 </template>
 
@@ -29,13 +31,15 @@ import { mapGetters } from 'vuex'
 import Product from 'theme/components/core/blocks/Checkout/Cart/Product'
 import Totals from 'theme/components/core/blocks/Microcart/Totals'
 import ButtonComponent from 'theme/components/core/blocks/Button'
+import NoSsr from 'vue-no-ssr'
 import { localizedRoute } from '@vue-storefront/core/lib/multistore'
 
 export default {
   components: {
     Product,
     Totals,
-    ButtonComponent
+    ButtonComponent,
+    NoSsr
   },
   computed: {
     ...mapGetters({
