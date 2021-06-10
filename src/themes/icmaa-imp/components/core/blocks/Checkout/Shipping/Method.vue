046d80d5108d2d3f161b2f737cf29c0ad48b5799
cewald <c.ewald@impericon.com>
2021-06-10 08:57:14 +0000

    Small CSS bugfix for shipping-method

--- src/themes/icmaa-imp/components/core/blocks/Checkout/Shipping/Method.vue
+++ src/themes/icmaa-imp/components/core/blocks/Checkout/Shipping/Method.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="t-flex t-items-center t-flex-wrap t-cursor-pointer"
+    class="t-flex t-items-center t-cursor-pointer"
     :class="{ 't-opacity-50': !active }"
     @click="onClick"
   >
