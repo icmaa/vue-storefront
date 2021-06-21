6e68db36755cab4217c7ca67a116b643e56fc061
cewald <c.ewald@impericon.com>
2021-06-21 10:32:09 +0000

    Improve getters to  not iterate through regexs if key is found directly

--- src/modules/icmaa-payment/store/payment/getters.ts
+++ src/modules/icmaa-payment/store/payment/getters.ts
@@ -3,13 +3,15 @@ import RootState from '@vue-storefront/core/types/RootState'
 import PaymentState from '../../types/PaymentState'
 
 const getters: GetterTree<PaymentState, RootState> = {
-  getFactoryKeyByCode: (state) => (code: string): string =>
+  getFactoryKeys: state => Object.keys(state.methodsFactories),
+  getFactoryKeyByCode: state => (code: string): string =>
     state.methodsFactoryKeyMap.find(i => i.regExp.test(code))?.mapTo || code,
   isMethod: (state, getters) => (code: string): boolean =>
-    Object.keys(state.methodsFactories).includes(getters.getFactoryKeyByCode(code)),
+    getters.getFactoryKeys.includes(code) ||
+    getters.getFactoryKeys.includes(getters.getFactoryKeyByCode(code)),
   getMethodFactoryByCode: (state, getters) => (code: string): any | boolean =>
-    state.methodsFactories[getters.getFactoryKeyByCode(code)] || false,
-  isRegistered: (state, getters) => (code: string): boolean =>
+    state.methodsFactories[code] || state.methodsFactories[getters.getFactoryKeyByCode(code)] || false,
+  isRegistered: (state) => (code: string): boolean =>
     state.registeredMethods.includes(code),
   getInfoComponentByCode: (state, getters, rootState, rootGetters) => (code: string): any => {
     if (!getters.isMethod(code)) return false
