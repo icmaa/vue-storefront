ef7e19ce33b9c0c15fd4c941544e5e93e131769b
cewald <c.ewald@impericon.com>
2021-06-24 11:19:51 +0000

    Klarna widget implementation [WIP]

--- src/modules/icmaa-checkout-com/components/methods/apm/Klarna.vue
+++ src/modules/icmaa-checkout-com/components/methods/apm/Klarna.vue
@@ -2,7 +2,8 @@
   <div class="t-text-sm">
     <div v-text="error" v-if="error" />
     <div id="klarna_container" />
-    <div v-text="'SDK is initialized'" v-if="sdkInit" />
+    <div v-text="'SDK is loaded and initialized'" v-if="sdkLoaded" />
+    <div v-text="'Widget is loaded'" v-if="widgetLoaded" />
     <pre>{{ info }}</pre>
   </div>
 </template>
@@ -16,14 +17,14 @@ export default {
   mixins: [ ApmMethod ],
   data () {
     return {
-      sdkInit: false,
+      sdkLoaded: false,
+      widgetLoaded: false,
       error: null
     }
   },
   async mounted () {
     window.klarnaAsyncCallback = this.klarnaAsyncCallback
     await this.loadSdkScript()
-    this.sdkInit = true
   },
   methods: {
     loadSdkScript () {
@@ -36,12 +37,24 @@ export default {
       })
     },
     klarnaAsyncCallback () {
-      console.error('klarnaAsyncCallback')
+      this.sdkLoaded = true
 
       try {
-        window.Klarna.Payments.init({
+        const k = window.Klarna.Payments.init({
           client_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIifQ.dtxWM6MIcgoeMgH87tGvsNDY6cH'
         });
+
+        k.load(
+          {
+            container: '#klarna_container',
+            payment_method_categories: [],
+            instance_id: 'klarna-payments-instance'
+          },
+          {},
+          () => {
+            this.widgetLoaded = true
+          }
+        )
       } catch (e) {
         this.error = e.message
         console.error(e)
