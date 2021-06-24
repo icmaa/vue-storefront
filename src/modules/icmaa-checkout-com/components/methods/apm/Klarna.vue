12bea38787d75d7d613c0f0e0036799a225aab5e
cewald <c.ewald@impericon.com>
2021-06-24 11:12:42 +0000

    Klarna widget implementation [WIP]

--- src/modules/icmaa-checkout-com/components/methods/apm/Klarna.vue
+++ src/modules/icmaa-checkout-com/components/methods/apm/Klarna.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="klarna-widget t-text-sm">
+  <div class="t-text-sm">
+    <div v-text="error" v-if="error" />
+    <div id="klarna_container" />
     <div v-text="'SDK is initialized'" v-if="sdkInit" />
     <pre>{{ info }}</pre>
   </div>
@@ -14,19 +16,37 @@ export default {
   mixins: [ ApmMethod ],
   data () {
     return {
-      sdkInit: false
+      sdkInit: false,
+      error: null
     }
   },
   async mounted () {
-    await new Promise(resolve => {
-      const script = document.createElement('script')
-      script.async = true
-      script.src = 'https://x.klarnacdn.net/kp/lib/v1/api.js'
-      script.onload = () => { resolve() }
-      document.body.appendChild(script)
-    })
-
+    window.klarnaAsyncCallback = this.klarnaAsyncCallback
+    await this.loadSdkScript()
     this.sdkInit = true
+  },
+  methods: {
+    loadSdkScript () {
+      return new Promise(resolve => {
+        const script = document.createElement('script')
+        script.async = true
+        script.src = 'https://x.klarnacdn.net/kp/lib/v1/api.js'
+        script.onload = () => { resolve() }
+        document.body.appendChild(script)
+      })
+    },
+    klarnaAsyncCallback () {
+      console.error('klarnaAsyncCallback')
+
+      try {
+        window.Klarna.Payments.init({
+          client_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIifQ.dtxWM6MIcgoeMgH87tGvsNDY6cH'
+        });
+      } catch (e) {
+        this.error = e.message
+        console.error(e)
+      }
+    }
   }
 }
 </script>
