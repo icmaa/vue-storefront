7adb5404dadd5420f0d517ffb5dc3e75c1852069
cewald <c.ewald@impericon.com>
2021-07-02 16:19:03 +0200

    Bugfix for `checkoutFillPersonalDetails` cmd

---
+++ src/themes/icmaa-imp/test/e2e/integration/Checkout/address.spec.ts
@@ -0,0 +1,34 @@
+describe('Checkout', () => {
+  it('as guest and use a poststation address.', () => {
+    cy.visitAsRecurringUser('/', { storeCode: 'de' })
+
+    cy.addRandomProductToCart({ enterCheckout: true })
+
+    cy.createCustomerWithFaker()
+
+    cy.checkoutFillPersonalDetails()
+
+    /**
+     * @todo Add post-station address and proceed
+     */
+
+    cy.getCustomer().then(customer => {
+      cy.get('#checkout .step-addresses .addresses').as('addresses')
+      cy.get('@addresses').get('.address-shipping').as('shipping-address')
+
+      cy.get('@shipping-address').focusInput('street[0]').type(customer.address.streetAddress())
+      cy.get('@shipping-address').focusInput('postcode').type(customer.address.zipCode())
+      cy.get('@shipping-address').focusInput('city').type(customer.address.city())
+
+      cy.get('@addresses').findByTestId('NextStepButton').click()
+      cy.waitForLoader(1000)
+    })
+
+    cy.checkoutFillShipping()
+
+    cy.checkoutFillPayment('Bankpayment')
+
+    cy.checkoutPlaceOrder()
+    cy.isLoggedIn(false)
+  })
+})
