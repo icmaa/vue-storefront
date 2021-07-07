e8028e128bf1ea6b2bdaa7d5e58fc3584ea44317
cewald <c.ewald@impericon.com>
2021-07-07 08:15:02 +0200

    Remove redundant code

--- src/themes/icmaa-imp/test/e2e/integration/Checkout/existingCustomerKlarna.spec.ts
+++ src/themes/icmaa-imp/test/e2e/integration/Checkout/existingCustomerKlarna.spec.ts
@@ -1,7 +1,7 @@
 describe('Checkout', () => {
   it('as existing customer and use Klarna as payment-method.', () => {
     cy.registerCustomerWithAddress('de')
-    cy.createCartAndGoToCheckout('de')
+    cy.createCartAndGoToCheckout()
 
     cy.checkoutFillAddress(true)
 
