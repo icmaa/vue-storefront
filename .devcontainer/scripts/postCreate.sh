43f474d1ddf055e3181517fa17253f1f168cc4ef
Christian Ewald <c.ewald@impericon.com>
2021-09-03 09:20:17 +0000

    Use NodeJS 16 and add `yarn install` for GitHub Codespace

--- .devcontainer/scripts/postCreate.sh
+++ .devcontainer/scripts/postCreate.sh
@@ -4,3 +4,5 @@ git clone https://github.com/icmaa/shop-workspace.git -b develop
 yarn --cwd shop-workspace
 yarn --cwd shop-workspace dev:sync --no-api --no-magento --sync-dest ../
 sudo rm -r shop-workspace
+
+yarn install
