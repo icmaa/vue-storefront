e5464dcdd58e7e3457132030b889cc4d7ad894ab
Christian Ewald <c.ewald@impericon.com>
2021-09-03 08:49:20 +0000

    Clone configs into`devcontainer.json`

---
+++ .devcontainer/scripts/postCreate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://github.com/icmaa/shop-workspace.git -b develop
+yarn --cwd shop-workspace
+yarn --cwd shop-workspace dev:sync --no-api --no-magento --sync-dest ../
+sudo rm -r shop-workspace
