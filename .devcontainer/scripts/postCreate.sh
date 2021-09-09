5a9e8a63d4d35271c9a05fd65da2b9d431ac3e19
Christian Ewald <c.ewald@impericon.com>
2021-09-09 07:31:02 +0000

    Update specs

--- .devcontainer/scripts/postCreate.sh
+++ .devcontainer/scripts/postCreate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Sync credentials
 git clone https://github.com/icmaa/shop-workspace.git -b develop
 yarn --cwd shop-workspace
 yarn --cwd shop-workspace dev:sync --no-api --no-magento --sync-dest ../
