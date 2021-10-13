700a98bfa72bf3e1e0647472240fc6ec972ba26e
Christian Ewald <c.ewald@impericon.com>
2021-10-13 05:17:54 +0000

    Import script using the component rendering process

--- src/themes/icmaa-imp/components/core/blocks/ICMAA/Cms/Pages/ServiceJobs.vue
+++ src/themes/icmaa-imp/components/core/blocks/ICMAA/Cms/Pages/ServiceJobs.vue
@@ -4,14 +4,7 @@
     <div class="t-mt-4 t-mb-8">
       {{ content.description }}
     </div>
-    <div id="join-widget">
-      <script
-        defer
-        type="text/javascript"
-        data-mount-in="#join-widget"
-        :src="content.joinlink"
-      />
-    </div>
+    <div id="join-widget" />
   </layout>
 </template>
 
@@ -32,6 +25,17 @@ export default {
   },
   asyncData ({ store }) {
     return store.dispatch('icmaaCmsBlock/single', { value: 'service-navigation' })
+  },
+  async mounted () {
+    await new Promise(resolve => {
+      const script = document.createElement('script')
+      script.defer = true
+      script.src = this.content.joinlink
+      script.setAttribute('data-mount-in', '#join-widget')
+      script.onload = () => resolve(true)
+      script.onerror = () => resolve(false)
+      document.body.appendChild(script)
+    })
   }
 }
 </script>
