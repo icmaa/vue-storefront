6e3d8ce0915c10a30a07d177dc278720cdf0c2c9
Christian Ewald <c.ewald@impericon.com>
2022-01-14 18:29:53 +0100

    #246148 Remove account-data for prepayment method in checkout (#744)

--- src/modules/icmaa-payment/components/methods/bank-payment/Info.vue
+++ src/modules/icmaa-payment/components/methods/bank-payment/Info.vue
@@ -1,30 +1,6 @@
 <template>
   <div class="t-text-sm">
-    <p class="t-mb-4">
-      {{ info.description }}:
-    </p>
-    <div
-      v-for="(account, i) in info.accounts"
-      :key="`account-${i}`"
-      class="t-border t-border-base-lightest t-p-4 t-rounded"
-    >
-      <div class="t-flex t-flex-wrap">
-        <div v-text="account.account_holder.label" class="t-w-full t-text-xxs t-text-base-light t-uppercase t-font-light" />
-        <div v-text="account.account_holder.value" class="t-mb-1" />
-      </div>
-      <div class="t-flex t-flex-wrap">
-        <div v-text="account.iban.label" class="t-w-full t-text-xxs t-text-base-light t-uppercase t-font-light" />
-        <div v-text="account.iban.value" class="t-mb-1" />
-      </div>
-      <div class="t-flex t-flex-wrap">
-        <div v-text="account.bank_name.label" class="t-w-full t-text-xxs t-text-base-light t-uppercase t-font-light" />
-        <div v-text="account.bank_name.value" class="t-mb-1" />
-      </div>
-      <div class="t-flex t-flex-wrap">
-        <div v-text="account.bic.label" class="t-w-full t-text-xxs t-text-base-light t-uppercase t-font-light" />
-        <div v-text="account.bic.value" />
-      </div>
-    </div>
+    {{ info.description }}
   </div>
 </template>
 
