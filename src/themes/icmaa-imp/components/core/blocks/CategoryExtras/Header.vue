63f11c676b64ce87c199ee9a2cc4470401c89179
Christian Ewald <c.ewald@impericon.com>
2022-04-05 10:39:18 +0000

    Prevent layout-shift in PLP for Spotify logoline

--- src/themes/icmaa-imp/components/core/blocks/CategoryExtras/Header.vue
+++ src/themes/icmaa-imp/components/core/blocks/CategoryExtras/Header.vue
@@ -6,8 +6,9 @@
         <slot />
       </div>
     </div>
-    <div class="t-px-4 t-py-2 t-flex t-justify-between" v-if="spotifyLogoItems && spotifyLogoItems.length > 0">
+    <div class="t-px-4 t-py-2 t-flex t-justify-between" v-if="isInSpotifyCategoryAllowList && (!isSpotifyLoaded || spotifyLogoItems.length > 0)">
       <span class="t-flex-fix t-hidden xl:t-inline-block t-flex t-self-center t-text-base-light t-text-sm t-mr-8">{{ $t('Similar bands:') }}</span>
+      <div class="t-flex-fix" style="height: 38px;" v-if="!isSpotifyLoaded" />
       <department-logo v-for="(logo, index) in spotifyLogoItems" :key="index" v-bind="logo.data()" class="t-flex-fix t-opacity-60 hover:t-opacity-100" :class="{ 't-mr-4': isLast(index, spotifyLogoItems)}" />
     </div>
   </div>
@@ -66,6 +67,8 @@ export default {
       category: 'icmaaCategoryExtras/getCurrentCategory',
       categoryExtras: 'icmaaCategoryExtras/getCategoryExtrasByCurrentCategory',
       getSpotifyLogoItems: 'icmaaCategoryExtras/getSpotifyLogolineItemsByCurrentCategory',
+      isInSpotifyCategoryAllowList: 'icmaaSpotify/isInCategoryAllowList',
+      isSpotifyLoaded: 'icmaaSpotify/areCurrentRelatedCategoriesLoaded',
       viewport: 'ui/getViewport'
     }),
     isVisible () {
@@ -74,7 +77,7 @@ export default {
       }
 
       const { bannerShowFrom, bannerShowTo, active } = this.categoryExtras
-      return active && (this.banner || this.spotifyLogoItems.length > 0) &&
+      return active && (this.banner || this.isInSpotifyCategoryAllowList || this.spotifyLogoItems.length > 0) &&
         isDatetimeInBetween(bannerShowFrom, bannerShowTo)
     },
     banner () {
