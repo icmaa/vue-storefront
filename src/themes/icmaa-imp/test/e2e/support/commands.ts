02a00cd76eb4ff9e3aa422cc422c18c8692d09de
cewald <hello@christianewald.de>
2022-04-11 10:59:41 +0200

    Use `denyAllConsents` instead  `acceptAllConsents` to prevent unneeded payload

--- src/themes/icmaa-imp/test/e2e/support/commands.ts
+++ src/themes/icmaa-imp/test/e2e/support/commands.ts
@@ -313,10 +313,12 @@ Cypress.Commands.add('acceptCookieNotice', () => {
           clearInterval(myInterval)
 
           /**
-           * This method sets the `uc_user_interaction` local-storage value to true and hides the modal
+           * This method sets the `uc_user_interaction` local-storage value to true and hides the modal.
+           * Originally UC recommends to use the `UC_UI_SUPPRESS_CMP_DISPLAY` window var but this needs
+           * to be set on each request and is hard to implement that way (unless you change the application-code).
            * @see https://docs.usercentrics.com/#/cmp-v2-ui-api?id=suppress-the-cmp
            * */
-          window.UC_UI.acceptAllConsents().then(resolve)
+          window.UC_UI.denyAllConsents().then(resolve)
         }
       }, 100)
     })
--- src/themes/icmaa-imp/test/e2e/typings/window.d.ts
+++ src/themes/icmaa-imp/test/e2e/typings/window.d.ts
@@ -2,8 +2,10 @@ export {}
 
 declare global {
   interface Window {
+    UC_UI_SUPPRESS_CMP_DISPLAY: boolean,
     UC_UI: {
-      acceptAllConsents(): Promise<any>
+      acceptAllConsents(): Promise<any>,
+      denyAllConsents(): Promise<any>
     }
   }
 }
