11071d6e212137d42c920ad28479178e57dc787f
Christian Ewald <c.ewald@impericon.com>
2022-04-20 09:36:33 +0000

    #252761 Add static logoline to homepage

--- src/themes/icmaa-imp/components/core/blocks/CategoryExtras/LogoLineBlock.vue
+++ src/themes/icmaa-imp/components/core/blocks/CategoryExtras/LogoLineBlock.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="t-w-full lg:t-w-1/2 t-px-4" data-test-id="LogoLineBlock">
+  <div class="t-w-full lg:t-w-1/3 t-px-4" data-test-id="LogoLineBlock">
     <h4 class="t-flex t-justify-between t-items-center t-text-xl t-text-base-dark t-mb-4">
       {{ $t('Our {department}', { department: $t(title) }) }}
       <router-link :to="localizedRoute(path)" class="t-inline-block t-l t-text-primary t-text-xs t-leading-loose">
         {{ $t('View all') }}
       </router-link>
     </h4>
-    <logo-line :parent-id="parentId" :limit="12" :placeholder="true" column-class="t-w-1/3 md:t-w-1/6 lg:t-w-1/4 t-py-2" class="t-justify-between" />
+    <logo-line :static-items="items" :limit="9" :placeholder="true" column-class="t-w-1/3 lg:t-w-1/3 t-py-2" class="t-justify-between" />
   </div>
 </template>
 
@@ -19,8 +19,8 @@ export default {
     LogoLine
   },
   props: {
-    parentId: {
-      type: Number,
+    items: {
+      type: Array,
       required: true
     },
     title: {
