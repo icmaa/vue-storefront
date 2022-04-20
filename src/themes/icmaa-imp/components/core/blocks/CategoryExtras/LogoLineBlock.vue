30e41f4d30f071343137763d241aa236df3e867c
Christian Ewald <c.ewald@impericon.com>
2022-04-20 14:31:49 +0200

    #252761 Remove title from logoline on homepage (#802)

--- src/themes/icmaa-imp/components/core/blocks/CategoryExtras/LogoLineBlock.vue
+++ src/themes/icmaa-imp/components/core/blocks/CategoryExtras/LogoLineBlock.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="t-w-full lg:t-w-1/3 t-px-4" data-test-id="LogoLineBlock">
     <h4 class="t-flex t-justify-between t-items-center t-text-xl t-text-base-dark t-mb-4">
-      {{ $t('Our {department}', { department: $t(title) }) }}
+      {{ title }}
       <router-link :to="localizedRoute(path)" class="t-inline-block t-l t-text-primary t-text-xs t-leading-loose">
         {{ $t('View all') }}
       </router-link>
