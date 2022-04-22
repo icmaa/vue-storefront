aad922d9b4b6208645d12ddae19987a676b2f567
Christian Ewald <c.ewald@impericon.com>
2022-04-22 09:16:13 +0000

    Bugfixes for flacky tests

--- src/themes/icmaa-imp/test/e2e/support/commands.ts
+++ src/themes/icmaa-imp/test/e2e/support/commands.ts
@@ -450,7 +450,7 @@ Cypress.Commands.add('addRandomProductToCart', (options?: { categoryUrl?: string
   cy.get<boolean>('@availability')
     .then(available => {
       if (!available) {
-        cy.addRandomProductToCart({ tries, enterCheckout }, count + 1)
+        cy.addRandomProductToCart(options, count + 1)
       } else {
         cy.addCurrentProductToCart(false, enterCheckout)
       }
