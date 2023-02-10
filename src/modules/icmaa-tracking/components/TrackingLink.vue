7563fe6a4da18559beedffe9b6c1729b10bae167
Christian Ewald <c.ewald@impericon.com>
2023-02-10 11:02:51 +0100

    #268886 Add Paqato tracking to customer-account and add custom `/order-tracking` route (#895)

--- src/modules/icmaa-tracking/components/TrackingLink.vue
+++ src/modules/icmaa-tracking/components/TrackingLink.vue
@@ -24,12 +24,21 @@ export default {
     }
   },
   computed: {
-    ...mapGetters({ getTracking: 'icmaaTracking/getTrackingByOrderId' }),
+    ...mapGetters({
+      getTracking: 'icmaaTracking/getTrackingByOrderId',
+      ordersHistory: 'user/getOrdersHistory',
+      store: 'icmaaConfig/getCurrentStore'
+    }),
     tracking () {
       return this.getTracking(this.orderId)
     },
     validStatus () {
       return config.icmaa_tracking.orderStatusWhitelist.includes(this.status)
+    },
+    order () {
+      return this.ordersHistory.find(order =>
+        parseInt(order.entity_id) === parseInt(this.orderId)
+      )
     }
   },
   methods: {
@@ -37,7 +46,23 @@ export default {
       this.$store.dispatch('ui/loader', { message: i18n.t('Please wait') })
       await this.$store.dispatch('icmaaTracking/fetchTracking', this.orderId)
       if (this.tracking) {
-        window.open(this.tracking.url, '_blank')
+        const languageCode = this.store.storeCode
+        const incrementId = this.order.increment_id + '-1'
+        const address = this.order?.extension_attributes?.shipping_assignments[0]?.shipping.address ||
+          this.order.billing_address
+        const postcode = address.postcode
+
+        this.$router.push({
+          name: 'order-tracking',
+          query: {
+            'tracking-code': [
+              languageCode,
+              this.tracking.id,
+              postcode,
+              incrementId
+            ].join('~')
+          }
+        })
       }
       this.$store.dispatch('ui/loader', false)
     }
---
+++ src/modules/icmaa-tracking/pages/Tracking.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="t-container">
+    <div class="t-py-8 t-px-4">
+      <div v-if="!checkTrackingParam">
+        {{ $t('Sorry, but we couldn\'t find a valid tracking-code in this tracking URL.') }}
+      </div>
+      <div
+        id="pqt-tracking"
+        :class="{ 't-bg-white t-p-4 md:t-p-8': sdKLoaded }"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import config from 'config'
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'OrderTracking',
+  data () {
+    return {
+      sdKLoaded: false
+    }
+  },
+  computed: {
+    ...mapGetters({
+      store: 'icmaaConfig/getCurrentStore'
+    }),
+    trackingCode () {
+      return this.$route.query['tracking-code'] || false
+    },
+    checkTrackingParam () {
+      /**
+       * The original Paqato tracking-code has this format:
+       * https://tracking.paqato.com/[LANGUAGE-CODE]~[PAQATO-CUSTOMER-ID]~[TRACKING-ID]~[ZIP-CODE]~[ORDER-ID]
+       *
+       * The tracking-code for the GET param of the Paqato script misses the `PAQATO-CUSTOMER-ID`,
+       * because we set it on initial function call, so it results in:
+       * https://domain.com/de/order-tracking?tracking-code=[LANGUAGE-CODE]~~[TRACKING-ID]~[ZIP-CODE]~[ORDER-ID]
+       */
+      return !!this.customerId && !!this.trackingCode && this.trackingCode.split('~').length === 4
+    },
+    customerId () {
+      return config.icmaa_tracking?.paqato?.customerId || false
+    }
+  },
+  methods: {
+    loadSdkScript () {
+      return new Promise(resolve => {
+        const script = document.createElement('script')
+        script.async = true
+        script.src = '//tracking.paqato.com/scripts/pqt-tracking.min.js'
+        script.onload = () => { resolve() }
+        document.body.appendChild(script)
+      })
+    }
+  },
+  mounted () {
+    if (!this.trackingCode || !this.checkTrackingParam) {
+      this.$router.push(this.localizedHomeRoute)
+      this.$store.dispatch('notification/spawnNotification', {
+        type: 'error',
+        message: this.$t('Sorry, but we couldn\'t find a valid tracking-code in this tracking URL.'),
+        action1: { label: this.$t('OK') }
+      })
+    }
+
+    this.loadSdkScript().then(() => {
+      this.sdKLoaded = true
+
+      // eslint-disable-next-line
+      pqtTracking(this.customerId, this.store.storeCode)
+    })
+  }
+}
+</script>
+
+<style type="scss">
+.pqt-trail,
+.pqt-done {
+  @apply t-z-0;
+}
+</style>
