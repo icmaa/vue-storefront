1a7b98e1d36750dc723c8a0646b9cbae20ee66a5
Christian Ewald <c.ewald@impericon.com>
2023-05-24 06:09:21 +0000

    Bugfix for ESLint errors

--- src/themes/icmaa-imp/components/core/blocks/Header/MetaNavigation.vue
+++ src/themes/icmaa-imp/components/core/blocks/Header/MetaNavigation.vue
@@ -1,13 +1,34 @@
 <template>
-  <nav role="meta-navigation" class="t-flex t-pr-10 t-hidden md:t-flex t-items-center">
+  <nav
+    role="meta-navigation"
+    class="t-hidden t-items-center t-pr-10 md:t-flex"
+  >
     <template v-for="(link, index) in navigation">
-      <router-link :to="localizedRoute(link.route)" class="t-flex t-text-sm t-text-base-dark" :title="link.name | htmlDecode" :key="index" v-if="link.isRoute === true">
+      <router-link
+        v-if="link.isRoute === true"
+        :key="index"
+        :to="localizedRoute(link.route)"
+        class="t-flex t-text-sm t-text-base-dark"
+        :title="link.name | htmlDecode"
+      >
         {{ link.name }}
       </router-link>
-      <a :href="link.route" class="t-flex t-text-sm t-text-base-dark" target="_blank" rel="noopener noreferrer" :title="link.name | htmlDecode" :key="index" v-else>
+      <a
+        v-else
+        :key="index"
+        :href="link.route"
+        class="t-flex t-text-sm t-text-base-dark"
+        target="_blank"
+        rel="noopener noreferrer"
+        :title="link.name | htmlDecode"
+      >
         {{ link.name }}
       </a>
-      <span class="t-flex t-px-3 t-text-sm t-text-base-lighter" v-if="index !== navigation.length - 1" :key="'bullet-' + index">&bull;</span>
+      <span
+        v-if="index !== navigation.length - 1"
+        :key="'bullet-' + index"
+        class="t-flex t-px-3 t-text-sm t-text-base-lighter"
+      >&bull;</span>
     </template>
   </nav>
 </template>
