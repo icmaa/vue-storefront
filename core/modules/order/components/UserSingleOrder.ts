07c29f816a3b938d715f51c6dceb88b6cbfc8c17
Christian Ewald <c.ewald@impericon.com>
2023-06-20 08:55:50 +0000

    #279501 Bugfix for configurable options on `/my-account/orders` page

--- core/modules/order/components/UserSingleOrder.ts
+++ core/modules/order/components/UserSingleOrder.ts
@@ -30,25 +30,5 @@ export const UserSingleOrder = {
         return !item.parent_item_id
       })
     }
-  },
-  methods: {
-    async remakeOrder (products) {
-      this.$bus.$emit('notification-progress-start', this.$t('Please wait ...'))
-      const productsToAdd = []
-      for (const item of products) {
-        const product = await this.$store.dispatch('product/single', { options: { sku: item.sku } })
-        product.qty = item.qty_ordered
-        productsToAdd.push(product)
-      }
-      await this.$store.dispatch('cart/addItems', { productsToAdd })
-      this.$bus.$emit('notification-progress-stop', {})
-      // Redirect to the cart straight away.
-      this.$router.push(this.localizedRoute('/checkout'))
-    },
-    skipGrouped (items) {
-      return items.filter((item) => {
-        return !item.parent_item_id
-      })
-    }
   }
 }
