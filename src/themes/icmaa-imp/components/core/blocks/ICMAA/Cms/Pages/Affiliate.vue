1a914cf82cbab3990efff33e755fcc1797e547ae
Dierk Landmann <d.landmann@impericon.com>
2023-06-28 11:15:03 +0200

    #281383 New affiliatepage (#934)

--- src/themes/icmaa-imp/components/core/blocks/ICMAA/Cms/Pages/Affiliate.vue
+++ src/themes/icmaa-imp/components/core/blocks/ICMAA/Cms/Pages/Affiliate.vue
@@ -1,123 +1,228 @@
 <template>
-  <div class="t-container t-bg-base-dark t-p-4 t-text-center t-uppercase t-text-white md:t-p-8 lg:t-p-16">
+  <div class="t-container t-bg-base-dark t-text-center t-text-white">
     <!-- banner -->
     <div class="t-flex t-justify-center">
       <img
-        :src="getMediaThumbnail(content.header.img, 0, 0)"
+        :src="getMediaThumbnail(imageUrlHeader, 0, 0)"
         :alt="content.header.alt"
         :title="content.header.alt"
-        class="t-mb-4 t-mt-8 t-w-2/3 t-border-b t-border-base-lightest t-pb-8"
+        class="t-mb-4 t-border-b t-border-base-lightest"
       >
     </div>
 
-    <!-- description -->
-    <div class="t-mb-8 t-mt-4">
-      {{ content.description }}
-    </div>
-    <div class="t-mb-8 t-flex t-w-full t-justify-center">
-      <MaterialIcon
-        icon="arrow_drop_down_circle"
-        size="xl"
-        class=""
-      />
-    </div>
-    <div class="t-mb-8 t-font-semibold">
-      {{ content.subheading }}
-    </div>
-
-    <!-- gallery -->
-    <div class="t-mb-16 t-flex t-flex-wrap t-items-center t-justify-center">
+    <div class="t-p-4 md:t-p-8 lg:t-p-16">
       <div
-        v-for="img in content.img"
-        :key="img.link"
-        class="t-mb-6 t-w-full md:t-mr-6 md:t-w-2/5"
+        v-for="about in content.aboutus"
+        :key="about.text"
       >
-        <img
-          :src="getMediaThumbnail(img.link, 0, 0)"
-          :alt="img.alt"
-          :title="img.alt"
-          class="t-w-full"
-        >
-      </div>
-    </div>
-
-    <div class="t-mb-16 t-flex t-justify-center t-font-semibold t-leading-snug">
-      <div class="t-w-3/4">
-        {{ content.headline2 }}
-      </div>
-    </div>
-
-    <!-- list -->
-    <div class="t-mb-8 t-flex t-flex-wrap t-justify-center">
-      <div
-        v-for="(block, index) in content.table"
-        :key="index"
-        class="t-mb-4 t-w-full md:t-mr-6 md:t-w-2/5"
-      >
-        <div class="t-mb-4 t-border t-p-1">
-          {{ block.headline }}
+        <div class="t-mb-8 t-mt-4">
+          {{ about.text }}
         </div>
-        <template v-if="!hasImage(block)">
-          <ul class="t-list-disc t-pl-16 t-text-left t-text-xs">
-            <li
-              v-for="(entry, row) in block.list"
-              :key="row"
-              class="t-mb-2"
-            >
-              {{ entry.text }}
-            </li>
-          </ul>
-        </template>
-        <template v-else>
-          <div class="t-flex t-justify-center t-px-4">
-            <img :src="getMediaThumbnail(block.img.link, 0, 0)">
-          </div>
-        </template>
       </div>
-    </div>
 
-    <!-- affilinet -->
-    <div class="t-mb-8 t-flex t-flex-wrap t-justify-center">
-      <a
-        :href="content.affilinet.link"
-        class="t-cursor-pointer lg:t-w-2/3"
+      <LinkComponent
+        :to="content.applybutton.link"
+        :title="content.applybutton.link"
+        class="t-mb-5 t-flex t-justify-center t-pb-5 t-text-sm t-leading-tight t-text-primary"
       >
-        <img
-          :src="getMediaThumbnail(content.affilinet.img, 0, 0)"
-          :alt="content.affilinet.alt"
-          :title="content.affilinet.alt"
-          class="t-w-full"
+        <ButtonComponent
+          type="primary"
+          size="sm"
+          class="t-mt-2"
         >
-      </a>
-    </div>
+          {{ content.applybutton.text }}
+        </ButtonComponent>
+      </LinkComponent>
 
-    <!-- footer -->
-    <div class="t-mb-8">
-      <div class="t-mb-8">
-        {{ content.footer.description }}
+      <hr class="t-mx-16 md:t-mx-24 lg:t-mx-96">
+
+      <!-- Creator-Programm Images -->
+      <div class="t-mb-16 t-mt-5 t-flex t-flex-wrap t-items-center t-justify-center t-pt-5">
+        <div
+          v-for="img in content.creatorprogrammimg"
+          :key="img.link"
+          class="t-w-1/2 t-p-4 md:t-w-1/4"
+        >
+          <img
+            :src="getMediaThumbnail(img.link, 0, 0)"
+            :alt="img.alt"
+            :title="img.alt"
+            class="t-w-full"
+          >
+        </div>
       </div>
-      <a
-        class="t-font-semibold t-text-white"
-        :href="content.footer.link"
-        :title="content.footer.title"
-      >{{ content.footer.title }}</a>
+
+      <hr class="t-mx-16 md:t-mx-24 lg:t-mx-96">
+
+      <div class="t-mb-10 t-mt-5 t-flex t-justify-center t-pt-5 t-font-bold t-leading-snug">
+        <div class="t-w-3/4">
+          {{ content.creator.headline }}
+        </div>
+      </div>
+
+      <div class="t-mb-10 t-flex t-justify-center t-font-normal t-leading-snug">
+        <div class="t-w-3/4">
+          {{ content.creator.text }}
+        </div>
+      </div>
+
+      <LinkComponent
+        :to="content.creator.button.link"
+        :title="content.creator.button.text"
+        class="t-mb-5 t-flex t-justify-center t-pb-5 t-text-sm t-leading-tight t-text-primary"
+      >
+        <ButtonComponent
+          type="primary"
+          size="sm"
+          class="t-mt-2"
+        >
+          {{ content.creator.button.text }}
+        </ButtonComponent>
+      </LinkComponent>
+
+      <hr class="t-mx-16 md:t-mx-24 lg:t-mx-96">
+
+      <div class="t-mb-16 t-mt-5 t-flex t-flex-wrap t-items-center t-justify-center t-pt-5">
+        <div
+          v-for="img in content.processimg"
+          :key="img.link"
+          class="t-w-1/5 md:t-w-1/5"
+        >
+          <img
+            :src="getMediaThumbnail(img.link, 0, 0)"
+            :alt="img.alt"
+            :title="img.alt"
+            class="t-w-full"
+          >
+        </div>
+      </div>
+
+      <div class="t-mb-8 t-flex t-justify-center t-font-normal t-leading-snug">
+        <div class="t-w-3/4">
+          {{ content.processtext }}
+        </div>
+      </div>
+
+      <LinkComponent
+        :to="content.awinhelp.link"
+        :title="content.awinhelp.text"
+        class="t-mb-5 t-flex t-justify-center t-pb-5 t-text-sm t-leading-tight t-text-primary"
+      >
+        <ButtonComponent
+          type="primary"
+          size="sm"
+          class="t-mt-2"
+        >
+          {{ content.awinhelp.text }}
+        </ButtonComponent>
+      </LinkComponent>
+
+      <hr class="t-mx-16 md:t-mx-24 lg:t-mx-96">
+
+      <!--List of advantages-->
+      <div class="t-mb-8 t-mt-5 t-flex t-flex-wrap t-justify-center t-pt-5">
+        <div
+          v-for="(block, index) in content.advantages"
+          :key="index"
+          class="t-mb-4 t-w-full t-text-left md:t-mr-6 md:t-w-2/5"
+        >
+          <div class="t-mb-4 t-p-1 t-text-xl t-font-bold t-uppercase">
+            {{ block.headline }}
+          </div>
+          <div>
+            <ul class="t-list-disc t-pl-6">
+              <li
+                v-for="(entry, row) in block.list"
+                :key="row"
+                class="t-mb-2"
+              >
+                {{ entry.text }}
+              </li>
+            </ul>
+            <div
+              v-if="(block.img)"
+              class=" t-mx-16 t-mt-10"
+            >
+              <img :src="getMediaThumbnail(block.img.link, 0, 0)">
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <hr class="t-mx-16 md:t-mx-24 lg:t-mx-96">
+
+      <!-- AWIN -->
+      <div class="t-mb-8 t-mt-5 t-flex t-flex-wrap t-justify-center t-pt-5">
+        <a
+          :href="content.awin.link"
+          class="t-cursor-pointer lg:t-w-2/3"
+        >
+          <img
+            :src="getMediaThumbnail(content.awin.img, 0, 0)"
+            :alt="content.awin.alt"
+            :title="content.awin.alt"
+            class="t-w-full"
+          >
+        </a>
+      </div>
+      <div class="t-mb-10 t-flex t-justify-center t-font-normal t-leading-snug">
+        <div class="t-w-3/4 t-text-[9px]">
+          {{ content.awin.text }}
+        </div>
+      </div>
+
+      <hr class="t-mx-16 md:t-mx-24 lg:t-mx-96">
+
+      <div class="t-mb-16 t-mt-5 t-flex t-justify-center t-pt-5 t-font-bold t-leading-snug">
+        <div class="t-w-3/4 t-text-xl">
+          {{ content.questionheadline }}
+        </div>
+      </div>
+
+      <!-- footer -->
+      <div class="t-mb-8">
+        <div class="t-mb-8">
+          {{ content.footer.description }}
+        </div>
+        <a
+          class="t-font-semibold t-text-white"
+          :href="content.footer.link"
+          :title="content.footer.linktext"
+        >
+          {{ content.footer.linktext }}
+        </a>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Page from 'icmaa-cms/mixins/Page'
-import MaterialIcon from 'theme/components/core/blocks/MaterialIcon'
+import ButtonComponent from 'theme/components/core/blocks/Button';
+import LinkComponent from 'theme/components/core/blocks/Link';
+import { mapGetters } from 'vuex'
 
 export default {
   name: 'Affiliates',
   components: {
-    MaterialIcon
+    ButtonComponent,
+    LinkComponent
   },
   mixins: [ Page ],
   data () {
     return {
       dataType: 'yaml'
+    }
+  },
+  computed: {
+    ...mapGetters({
+      viewport: 'ui/getViewport'
+    }),
+    isMobile () {
+      return ['xs', 'sm'].includes(this.viewport)
+    },
+    imageUrlHeader () {
+      return this.isMobile ? this.content.header.img_mob : this.content.header.img
     }
   },
   methods: {
