4e27e02a36ba6751c4b5efa221767a42bf1663ec
Christian Ewald <c.ewald@impericon.com>
2023-06-30 11:59:50 +0200

    #282446 Bugfix for sale-prices in CLP (#937)

--- src/themes/icmaa-imp/mixins/product/priceMixin.ts
+++ src/themes/icmaa-imp/mixins/product/priceMixin.ts
@@ -14,7 +14,7 @@ export default {
             return false
           }
 
-          const cPrice = c.price_incl_tax || c.original_price_incl_tax
+          const cPrice = c.price_incl_tax || product.price_incl_tax
           if (price === 0) {
             price = cPrice
             return false
@@ -27,6 +27,8 @@ export default {
     },
     lowestPriceInclTax () {
       const product = this.product
+      let key = 'price_incl_tax'
+
       if (product.type_id === 'configurable' &&
         product.configurable_children &&
         product.configurable_children.length > 0
@@ -35,16 +37,17 @@ export default {
           if (!a.stock || !a.stock.is_in_stock || a.stock.qty === 0) {
             return b
           }
-          const aPrice = a.price_incl_tax || a.original_price_incl_tax
-          const bPrice = b.price_incl_tax || b.original_price_incl_tax
+
+          const aPrice = a[key] || product[key]
+          const bPrice = b[key] || product[key]
 
           return aPrice <= bPrice ? a : b
         }, {})
 
-        return cheapestProduct.price_incl_tax || cheapestProduct.original_price_incl_tax || 0
+        return cheapestProduct[key] || product[key] || 0
       }
 
-      return product.price_incl_tax
+      return product[key]
     }
   },
   methods: {
