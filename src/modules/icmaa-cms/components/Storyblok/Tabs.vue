7aa2d65c4b3c790f3f4589ce9db6d02d1411aeff
Dierk Landmann <d.landmann@impericon.com>
2023-09-14 08:37:10 +0000

    test

--- src/modules/icmaa-cms/components/Storyblok/Tabs.vue
+++ src/modules/icmaa-cms/components/Storyblok/Tabs.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
     {{ title }}
+
+    {{ block[0].headline }}
+    {{ block[2].headline }}
+    {{ block[4].headline }}
+    {{ block[6].headline }}
+    <br>
+    <img :src="block[1].asset.filename">
+    {{ block[3].asset.filename }}
+    {{ block[5].asset.filename }}
+    {{ block[7].asset.filename }}
+
+    <!--
+    - for schleife (i)  durch den block
+    - gerade -> headline
+    - ungreade -> imagecomponent !!
+    -->
   </div>
 </template>
 
@@ -12,6 +28,10 @@ export default {
   props: {
     title: {
       type: String,
+      required: true
+    },
+    block: {
+      type: Array,
       required: true
     }
   }
