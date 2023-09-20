114c1bcaa756214b3bb31206bfa682285076e3a5
Christian Ewald <c.ewald@impericon.com>
2023-09-20 08:39:18 +0000

    #275142 Use correct GA id for recommendations

--- src/modules/icmaa-recommendations/store/getters.ts
+++ src/modules/icmaa-recommendations/store/getters.ts
@@ -4,7 +4,13 @@ import RootState from '@vue-storefront/core/types/RootState'
 import { getCookie } from 'icmaa-recommendations/helpers/Cookies'
 
 const getters: GetterTree<RecommendationsState, RootState> = {
-  getGAVisitorId: (): string | null => getCookie('_ga'),
+  getGAVisitorId: (): string | null => {
+    const cookie = getCookie('_ga')
+    if (cookie) {
+      const regex = /^GA\d\.\d\./g
+      return cookie.replace(regex, '')
+    }
+  },
   getList: (state): Recommendations[] => state.list,
   getByTypeAndProductId: (state, getters) => (productId: string | null, eventType: string, servingConfigs: string): Recommendations =>
     getters.getList
